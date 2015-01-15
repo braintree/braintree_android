9341b57a6f94e67933bbebf6c168a87bda856329
lkorth <luke@lukekorth.com>
2015-01-15 11:03:21 -0800

    Show title and loading indicator for 3D Secure
    https://trello.com/c/VAE2wwYd/19-android-back-button-close-button-webview-chrome

--- BraintreeApi/src/main/java/com/braintreepayments/api/ThreeDSecureWebViewActivity.java
+++ BraintreeApi/src/main/java/com/braintreepayments/api/ThreeDSecureWebViewActivity.java
@@ -11,6 +11,8 @@ import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.Window;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -35,12 +37,14 @@ public class ThreeDSecureWebViewActivity extends Activity {
     public static final String EXTRA_THREE_D_SECURE_LOOKUP = "com.braintreepayments.api.EXTRA_THREE_D_SECURE_LOOKUP";
     public static final String EXTRA_THREE_D_SECURE_RESULT = "com.braintreepayments.api.EXTRA_THREE_D_SECURE_RESULT";
 
+    private ActionBar mActionBar;
     private WebView mThreeDSecureWebView;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_PROGRESS);
 
         ThreeDSecureLookup threeDSecureLookup =
                 getIntent().getParcelableExtra(EXTRA_THREE_D_SECURE_LOOKUP);
@@ -53,13 +57,14 @@ public class ThreeDSecureWebViewActivity extends Activity {
 
         mThreeDSecureWebView = new WebView(this);
         mThreeDSecureWebView.setId(android.R.id.widget_frame);
-        mThreeDSecureWebView.setWebViewClient(threeDSecureWebViewClient());
-        ((FrameLayout) findViewById(android.R.id.content)).addView(mThreeDSecureWebView);
-
+        mThreeDSecureWebView.setWebChromeClient(mThreeDSecureWebChromeClient);
+        mThreeDSecureWebView.setWebViewClient(mThreeDSecureWebViewClient);
         mThreeDSecureWebView.getSettings().setJavaScriptEnabled(true);
         mThreeDSecureWebView.getSettings().setBuiltInZoomControls(true);
         mThreeDSecureWebView.getSettings().setUserAgentString(HttpRequest.USER_AGENT);
         mThreeDSecureWebView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+
+        ((FrameLayout) findViewById(android.R.id.content)).addView(mThreeDSecureWebView);
 
         List<NameValuePair> params = new LinkedList<NameValuePair>();
         params.add(new BasicNameValuePair("PaReq", threeDSecureLookup.getPareq()));
@@ -69,41 +74,65 @@ public class ThreeDSecureWebViewActivity extends Activity {
         try {
             new UrlEncodedFormEntity(params, HTTP.UTF_8).writeTo(encodedParams);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            finish();
         }
         mThreeDSecureWebView.postUrl(threeDSecureLookup.getAcsUrl(), encodedParams.toByteArray());
     }
 
+    private WebViewClient mThreeDSecureWebViewClient = new WebViewClient() {
+        public void onPageStarted(WebView view, String url, Bitmap icon) {
+            if (url.contains("html/authentication_complete_frame")) {
+                view.stopLoading();
+
+                String authResponseJson = (Uri.parse(url).getQueryParameter("auth_response"));
+
+                ThreeDSecureAuthenticationResponse authResponse =
+                        ThreeDSecureAuthenticationResponse.fromJson(authResponseJson);
+                setResult(Activity.RESULT_OK, new Intent()
+                        .putExtra(ThreeDSecureWebViewActivity.EXTRA_THREE_D_SECURE_RESULT,
+                                authResponse));
+                finish();
+            } else {
+                super.onPageStarted(view, url, icon);
+            }
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            setActionBarTitle(view.getTitle());
+        }
+    };
+
+    private WebChromeClient mThreeDSecureWebChromeClient = new WebChromeClient() {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+            if (newProgress < 100) {
+                setProgress(newProgress);
+                setProgressBarVisibility(true);
+            } else {
+                setProgressBarVisibility(false);
+            }
+        }
+    };
+
     @TargetApi(VERSION_CODES.HONEYCOMB)
     private void setupActionBar() {
         if (VERSION.SDK_INT >= VERSION_CODES.HONEYCOMB) {
-            ActionBar actionBar = getActionBar();
-            if (actionBar != null) {
-                actionBar.setTitle("");
-                actionBar.setDisplayHomeAsUpEnabled(true);
+            mActionBar = getActionBar();
+            if (mActionBar != null) {
+                setActionBarTitle("");
+                mActionBar.setDisplayHomeAsUpEnabled(true);
             }
         }
     }
 
-    private WebViewClient threeDSecureWebViewClient() {
-        return new WebViewClient() {
-            public void onPageStarted(WebView view, String url, Bitmap icon) {
-                if (url.contains("html/authentication_complete_frame")) {
-                    view.stopLoading();
-
-                    String authResponseJson = (Uri.parse(url).getQueryParameter("auth_response"));
-
-                    ThreeDSecureAuthenticationResponse authResponse =
-                            ThreeDSecureAuthenticationResponse.fromJson(authResponseJson);
-                    setResult(Activity.RESULT_OK, new Intent()
-                            .putExtra(ThreeDSecureWebViewActivity.EXTRA_THREE_D_SECURE_RESULT,
-                                    authResponse));
-                    finish();
-                } else {
-                    super.onPageStarted(view, url, icon);
-                }
-            }
-        };
+    @TargetApi(VERSION_CODES.HONEYCOMB)
+    private void setActionBarTitle(String title) {
+        if (mActionBar != null) {
+            mActionBar.setTitle(title);
+        }
     }
 
     @Override
@@ -124,5 +153,4 @@ public class ThreeDSecureWebViewActivity extends Activity {
             super.onBackPressed();
         }
     }
-
 }
