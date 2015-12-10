0c579a8ec0e896865bd75b7259b3b2c7688ff094
lkorth <luke@lukekorth.com>
2015-12-10 15:57:25 -0500

    Use class to click environment spinner in test

--- Demo/src/androidTest/java/com/braintreepayments/demo/test/utilities/TestHelper.java
+++ Demo/src/androidTest/java/com/braintreepayments/demo/test/utilities/TestHelper.java
@@ -5,15 +5,16 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.widget.Spinner;
 
 import static android.support.test.InstrumentationRegistry.getTargetContext;
 import static com.lukekorth.deviceautomator.AutomatorAction.click;
 import static com.lukekorth.deviceautomator.AutomatorAssertion.text;
 import static com.lukekorth.deviceautomator.DeviceAutomator.onDevice;
+import static com.lukekorth.deviceautomator.UiObjectMatcher.withClass;
 import static com.lukekorth.deviceautomator.UiObjectMatcher.withText;
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
-import static junit.framework.Assert.fail;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class TestHelper {
@@ -21,44 +22,19 @@ public class TestHelper {
     private static final String PAYPAL_WALLET_PACKAGE_NAME = "com.paypal.android.p2pmobile";
 
     public void setup() {
-        clearPreferences();
-        launchDemoApp();
-        ensureEnvironmentIsSandbox();
-    }
-
-    private void clearPreferences() {
         PreferenceManager.getDefaultSharedPreferences(getTargetContext())
                 .edit()
                 .clear()
                 .commit();
-    }
-
-    private void launchDemoApp() {
         onDevice().onHomeScreen().launchApp("com.braintreepayments.demo");
-    }
-
-    private void ensureEnvironmentIsSandbox() {
         ensureEnvironmentIs("Sandbox");
     }
 
     private static void ensureEnvironmentIs(String environment) {
-        String otherEnvironment;
-        switch (environment) {
-            case "Sandbox":
-                otherEnvironment = "Production";
-                break;
-            case "Production":
-                otherEnvironment = "Sandbox";
-                break;
-            default:
-                fail("Environment was invalid");
-                return;
-        }
-
         try {
             onDevice(withText(environment)).check(text(equalTo(environment)));
         } catch (RuntimeException e) {
-            onDevice(withText(otherEnvironment)).perform(click());
+            onDevice(withClass(Spinner.class)).perform(click());
             onDevice(withText(environment)).perform(click());
             onDevice(withText(environment)).check(text(equalTo(environment)));
         }
