f03a1dd7b14138756d064d5f4507764a8eb5b5c3
lkorth <luke@lukekorth.com>
2015-12-16 10:55:23 -0500

    Removed unneeded 'this'

--- OneTouchCore/src/main/java/com/paypal/android/sdk/onetouch/core/config/ConfigEndpoint.java
+++ OneTouchCore/src/main/java/com/paypal/android/sdk/onetouch/core/config/ConfigEndpoint.java
@@ -7,17 +7,17 @@ public class ConfigEndpoint {
     private String mCertificate;
 
     public ConfigEndpoint name(String name) {
-        this.mName = name;
+        mName = name;
         return this;
     }
 
     public ConfigEndpoint url(String url) {
-        this.mUrl = url;
+        mUrl = url;
         return this;
     }
 
     public ConfigEndpoint certificate(String certificate) {
-        this.mCertificate = certificate;
+        mCertificate = certificate;
         return this;
     }
 
