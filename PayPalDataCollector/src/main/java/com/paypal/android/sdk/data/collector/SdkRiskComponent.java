67da9fb120ef803f8cf300bd3cfa48c601e4340d
lkorth <luke@lukekorth.com>
2016-01-12 14:15:11 -0500

    Remove unnecessary try/catch

--- PayPalDataCollector/src/main/java/com/paypal/android/sdk/data/collector/SdkRiskComponent.java
+++ PayPalDataCollector/src/main/java/com/paypal/android/sdk/data/collector/SdkRiskComponent.java
@@ -2,7 +2,6 @@ package com.paypal.android.sdk.data.collector;
 
 import android.content.Context;
 import android.support.annotation.MainThread;
-import android.util.Log;
 
 import com.paypal.android.sdk.onetouch.core.metadata.MetadataIdProvider;
 import com.paypal.android.sdk.onetouch.core.metadata.MetadataIdProviderImpl;
@@ -14,47 +13,37 @@ import java.util.concurrent.Executors;
 
 public final class SdkRiskComponent {
 
-    private static final String PUBLIC_TAG = "paypal.sdk";
-
     private static MetadataIdProvider sMetadataIdProvider;
 
     /**
-     * Starts the risk component if it hasn't been initialized yet.  Otherwise, just generate a
-     * clientMetadataId.
+     * Starts the risk component if it hasn't been initialized yet.  Otherwise, just generate a clientMetadataId.
      *
      * @param context
      * @return the clientMetadataId
      */
     @MainThread
-    public static String getClientMetadataId(Context context, String applicationGuid,
-            String pairingId) {
+    public static String getClientMetadataId(Context context, String applicationGuid, String pairingId) {
         if (sMetadataIdProvider == null) {
-            try {
-                sMetadataIdProvider = new MetadataIdProviderImpl();
+            sMetadataIdProvider = new MetadataIdProviderImpl();
 
-                Map<String, Object> params;
-                if (pairingId != null) {
-                    params = new HashMap<>();
-                    params.put(MetadataIdProvider.PAIRING_ID, pairingId);
-                } else {
-                    params = Collections.emptyMap();
-                }
-
-                String clientMetadataId = sMetadataIdProvider.init(context.getApplicationContext(),
-                        applicationGuid, params);
-
-                Executors.newSingleThreadExecutor().submit(new Runnable() {
-                    @Override
-                    public void run() {
-                        sMetadataIdProvider.flush();
-                    }
-                });
-
-                return clientMetadataId;
-            } catch (Throwable t) {
-                Log.e(PUBLIC_TAG, "An internal component failed to initialize: " + t.getMessage());
-                return "";
+            Map<String, Object> params;
+            if (pairingId != null) {
+                params = new HashMap<>();
+                params.put(MetadataIdProvider.PAIRING_ID, pairingId);
+            } else {
+                params = Collections.emptyMap();
             }
+
+            String clientMetadataId = sMetadataIdProvider.init(context.getApplicationContext(), applicationGuid, params);
+
+            Executors.newSingleThreadExecutor().submit(new Runnable() {
+                @Override
+                public void run() {
+                    sMetadataIdProvider.flush();
+                }
+            });
+
+            return clientMetadataId;
         } else {
             String clientMetadataId;
             if (null != pairingId) {
