3410b371359ba7041a8780990c70fbd70ad61bff
lkorth <luke@lukekorth.com>
2016-02-01 13:30:23 -0500

    Convert ErrorsWithResponseTest to a unit test

--- Braintree/src/test/java/com/braintreepayments/api/exceptions/ErrorsWithResponseTest.java
+++ Braintree/src/test/java/com/braintreepayments/api/exceptions/ErrorsWithResponseTest.java
@@ -1,21 +1,17 @@
 package com.braintreepayments.api.exceptions;
 
-import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.SmallTest;
-
-import static com.braintreepayments.testutils.FixturesHelper.stringFromFixture;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
 
+import static com.braintreepayments.testutils.FixturesHelper.stringFromFixture;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNull;
 
-@RunWith(AndroidJUnit4.class)
+@RunWith(RobolectricGradleTestRunner.class)
 public class ErrorsWithResponseTest {
 
-    @Test(timeout = 1000)
-    @SmallTest
+    @Test
     public void parsesErrorsCorrectly() {
         String response = stringFromFixture("errors/credit_card_error_response.json");
 
@@ -23,9 +19,7 @@ public class ErrorsWithResponseTest {
 
         assertEquals("Credit card is invalid", errorWithResponse.getMessage());
         assertEquals(422, errorWithResponse.getStatusCode());
-
         assertNull(errorWithResponse.errorFor("creditCard").errorFor("postalCode"));
-
         assertEquals("Credit card must include number, payment_method_nonce, or venmo_sdk_payment_method_code",
                 errorWithResponse.errorFor("creditCard").errorFor("base").getMessage());
         assertEquals("Credit card number is required",
@@ -34,8 +28,7 @@ public class ErrorsWithResponseTest {
                 errorWithResponse.errorFor("creditCard").errorFor("expirationYear").getMessage());
     }
 
-    @Test(timeout = 1000)
-    @SmallTest
+    @Test
     public void handlesTopLevelErrors() {
         String topLevelError = stringFromFixture("errors/auth_fingerprint_error.json");
 
@@ -45,21 +38,18 @@ public class ErrorsWithResponseTest {
         assertEquals(1, errorWithResponse.getFieldErrors().size());
     }
 
-    @Test(timeout = 1000)
-    @SmallTest
+    @Test
     public void canHandleMultipleCategories() {
         String errors = stringFromFixture("errors/complex_error_response.json");
 
         ErrorWithResponse errorWithResponse = new ErrorWithResponse(422, errors);
 
         assertEquals(3, errorWithResponse.errorFor("creditCard").getFieldErrors().size());
-
         assertEquals("is invalid", errorWithResponse.errorFor("customer").getMessage());
         assertEquals(0, errorWithResponse.errorFor("customer").getFieldErrors().size());
     }
 
-    @Test(timeout = 1000)
-    @SmallTest
+    @Test
     public void doesNotBlowUpParsingBadJson() {
         String badJson = stringFromFixture("random_json.json");
 
