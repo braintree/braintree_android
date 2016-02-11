c8ee45ad05d4c22f06b03de2cf375be414860368
lkorth <luke@lukekorth.com>
2016-02-11 11:08:47 -0500

    Convert some CardTests to CardUnitTests
    Introduce PowerMock for unit tests

--- Braintree/src/androidTest/java/com/braintreepayments/api/CardTest.java
+++ Braintree/src/androidTest/java/com/braintreepayments/api/CardTest.java
@@ -1,21 +1,15 @@
 package com.braintreepayments.api;
 
-import android.app.Activity;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.MediumTest;
 
-import com.braintreepayments.api.exceptions.ErrorWithResponse;
 import com.braintreepayments.api.exceptions.InvalidArgumentException;
-import com.braintreepayments.api.interfaces.BraintreeErrorListener;
 import com.braintreepayments.api.interfaces.PaymentMethodNonceCreatedListener;
-import com.braintreepayments.api.internal.BraintreeHttpClient;
-import com.braintreepayments.api.models.AnalyticsConfiguration;
 import com.braintreepayments.api.models.CardBuilder;
 import com.braintreepayments.api.models.CardNonce;
-import com.braintreepayments.api.models.Configuration;
 import com.braintreepayments.api.models.PaymentMethodNonce;
-import com.braintreepayments.testutils.BraintreeActivityTestRule;
 import com.braintreepayments.api.test.TestActivity;
+import com.braintreepayments.testutils.BraintreeActivityTestRule;
 import com.braintreepayments.testutils.TestClientTokenBuilder;
 
 import org.junit.Before;
@@ -23,44 +17,32 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
-import static com.braintreepayments.api.BraintreeFragmentTestUtils.getMockFragment;
-import static com.braintreepayments.api.BraintreeFragmentTestUtils.verifyAnalyticsEvent;
-import static com.braintreepayments.api.internal.BraintreeHttpClientTestUtils.clientWithExpectedResponse;
 import static com.braintreepayments.testutils.CardNumber.VISA;
-import static com.braintreepayments.testutils.FixturesHelper.stringFromFixture;
 import static com.braintreepayments.testutils.TestTokenizationKey.TOKENIZATION_KEY;
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @RunWith(AndroidJUnit4.class)
+@MediumTest
 public class CardTest {
 
     @Rule
     public final BraintreeActivityTestRule<TestActivity> mActivityTestRule =
             new BraintreeActivityTestRule<>(TestActivity.class);
 
-    private Activity mActivity;
-    private BraintreeFragment mBraintreeFragment;
     private CountDownLatch mCountDownLatch;
 
     @Before
     public void setUp() throws InvalidArgumentException {
-        mActivity = mActivityTestRule.getActivity();
         mCountDownLatch = new CountDownLatch(1);
     }
 
     @Test(timeout = 10000)
-    @MediumTest
     public void tokenize_tokenizesACard() throws InvalidArgumentException, InterruptedException {
-        setupFragment();
-        mBraintreeFragment.addListener(new PaymentMethodNonceCreatedListener() {
+        BraintreeFragment fragment = BraintreeFragment.newInstance(mActivityTestRule.getActivity(),
+                new TestClientTokenBuilder().build());
+        fragment.addListener(new PaymentMethodNonceCreatedListener() {
             @Override
             public void onPaymentMethodNonceCreated(PaymentMethodNonce paymentMethodNonce) {
                 assertEquals("11", ((CardNonce) paymentMethodNonce).getLastTwo());
@@ -71,46 +53,15 @@ public class CardTest {
                 .cardNumber(VISA)
                 .expirationDate("08/20");
 
-        Card.tokenize(mBraintreeFragment, cardBuilder);
+        Card.tokenize(fragment, cardBuilder);
 
         mCountDownLatch.await();
     }
 
     @Test(timeout = 10000)
-    @MediumTest
-    public void tokenize_sendsAnalyticsEventOnSuccess()
-            throws InvalidArgumentException, InterruptedException, IOException, ErrorWithResponse {
-        AnalyticsConfiguration analyticsConfiguration = mock(AnalyticsConfiguration.class);
-        when(analyticsConfiguration.isEnabled()).thenReturn(true);
-        Configuration configuration = mock(Configuration.class);
-        when(configuration.getAnalytics()).thenReturn(analyticsConfiguration);
-        mBraintreeFragment = getMockFragment(mActivity, configuration);
-        BraintreeHttpClient httpClient = clientWithExpectedResponse(201,
-                stringFromFixture("payment_methods/visa_credit_card_response.json"));
-        when(mBraintreeFragment.getHttpClient()).thenReturn(httpClient);
-        mBraintreeFragment.addListener(new PaymentMethodNonceCreatedListener() {
-            @Override
-            public void onPaymentMethodNonceCreated(PaymentMethodNonce paymentMethodNonce) {
-                mCountDownLatch.countDown();
-            }
-        });
-        CardBuilder cardBuilder = new CardBuilder()
-                .cardNumber(VISA)
-                .expirationDate("08/20");
-
-        Card.tokenize(mBraintreeFragment, cardBuilder);
-
-        mCountDownLatch.await();
-        verifyAnalyticsEvent(mBraintreeFragment, "card.nonce-received");
-    }
-
-    @Test(timeout = 10000)
-    @MediumTest
     public void tokenize_tokenizesACardWithATokenizationKey() throws InvalidArgumentException, InterruptedException {
-        setupFragment();
-        mBraintreeFragment = BraintreeFragment.newInstance(mActivityTestRule.getActivity(),
-                TOKENIZATION_KEY);
-        mBraintreeFragment.addListener(new PaymentMethodNonceCreatedListener() {
+        BraintreeFragment fragment = BraintreeFragment.newInstance(mActivityTestRule.getActivity(), TOKENIZATION_KEY);
+        fragment.addListener(new PaymentMethodNonceCreatedListener() {
             @Override
             public void onPaymentMethodNonceCreated(PaymentMethodNonce paymentMethodNonce) {
                 assertEquals("11", ((CardNonce) paymentMethodNonce).getLastTwo());
@@ -121,70 +72,8 @@ public class CardTest {
                 .cardNumber(VISA)
                 .expirationDate("08/20");
 
-        Card.tokenize(mBraintreeFragment, cardBuilder);
+        Card.tokenize(fragment, cardBuilder);
 
         mCountDownLatch.await();
-    }
-
-    @Test(timeout = 10000)
-    @MediumTest
-    public void tokenize_callsListenerWithErrorOnFailure()
-            throws InterruptedException, InvalidArgumentException {
-        setupFragment();
-        mBraintreeFragment.addListener(new BraintreeErrorListener() {
-            @Override
-            public void onError(Exception exception) {
-                ErrorWithResponse error = (ErrorWithResponse) exception;
-
-                assertEquals(422, error.getStatusCode());
-                assertNotNull(error.getFieldErrors());
-                assertEquals("Credit card is invalid", error.getMessage());
-                assertEquals(1, error.getFieldErrors().size());
-                assertEquals(3, error.errorFor("creditCard").getFieldErrors().size());
-                assertEquals(
-                        "Credit card must include number, payment_method_nonce, or venmo_sdk_payment_method_code",
-                        error.errorFor("creditCard").errorFor("base").getMessage());
-                assertEquals("Expiration year is invalid",
-                        error.errorFor("creditCard").errorFor("expirationYear").getMessage());
-                assertEquals("Credit card number is required",
-                        error.errorFor("creditCard").errorFor("number").getMessage());
-                assertNull(error.errorFor("creditCard").errorFor("expirationMonth"));
-                mCountDownLatch.countDown();
-            }
-        });
-
-        Card.tokenize(mBraintreeFragment, new CardBuilder().expirationMonth("01"));
-
-        mCountDownLatch.await();
-    }
-
-    @Test(timeout = 10000)
-    @MediumTest
-    public void tokenize_sendsAnalyticsEventOnFailure()
-            throws InterruptedException, InvalidArgumentException, IOException, ErrorWithResponse {
-        AnalyticsConfiguration analyticsConfiguration = mock(AnalyticsConfiguration.class);
-        when(analyticsConfiguration.isEnabled()).thenReturn(true);
-        Configuration configuration = mock(Configuration.class);
-        when(configuration.getAnalytics()).thenReturn(analyticsConfiguration);
-        mBraintreeFragment = getMockFragment(mActivity, configuration);
-        BraintreeHttpClient httpClient = clientWithExpectedResponse(422, "");
-        when(mBraintreeFragment.getHttpClient()).thenReturn(httpClient);
-        mBraintreeFragment.addListener(new BraintreeErrorListener() {
-            @Override
-            public void onError(Exception error) {
-                mCountDownLatch.countDown();
-            }
-        });
-
-        Card.tokenize(mBraintreeFragment, new CardBuilder().expirationMonth("01"));
-
-        mCountDownLatch.await();
-        verifyAnalyticsEvent(mBraintreeFragment, "card.nonce-failed");
-    }
-
-    /* helpers */
-    private void setupFragment() throws InvalidArgumentException {
-        mBraintreeFragment = BraintreeFragment.newInstance(mActivity, new TestClientTokenBuilder().build());
-        getInstrumentation().waitForIdleSync();
     }
 }
---
+++ Braintree/src/test/java/com/braintreepayments/api/CardUnitTest.java
@@ -0,0 +1,103 @@
+package com.braintreepayments.api;
+
+import com.braintreepayments.api.exceptions.ErrorWithResponse;
+import com.braintreepayments.api.interfaces.PaymentMethodNonceCallback;
+import com.braintreepayments.api.models.CardBuilder;
+import com.braintreepayments.api.models.CardNonce;
+import com.braintreepayments.api.models.PaymentMethodBuilder;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.rule.PowerMockRule;
+import org.robolectric.RobolectricGradleTestRunner;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.doAnswer;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@PowerMockIgnore({ "org.mockito.*", "org.robolectric.*", "android.*" })
+@PrepareForTest(TokenizationClient.class)
+public class CardUnitTest {
+
+    @Rule
+    public PowerMockRule mPowerMockRule = new PowerMockRule();
+
+    private BraintreeFragment mBraintreeFragment;
+
+    @Before
+    public void setup() {
+        mBraintreeFragment = mock(BraintreeFragment.class);
+    }
+
+    @Test
+    public void tokenize_callsListenerWithNonceOnSuccess() {
+        mockSuccessCallback();
+
+        Card.tokenize(mBraintreeFragment, null);
+
+        verify(mBraintreeFragment).postCallback(any(CardNonce.class));
+    }
+
+    @Test
+    public void tokenize_sendsAnalyticsEventOnSuccess() {
+        mockSuccessCallback();
+
+        Card.tokenize(mBraintreeFragment, new CardBuilder());
+
+        verify(mBraintreeFragment).sendAnalyticsEvent("card.nonce-received");
+    }
+
+    @Test
+    public void tokenize_callsListenerWithErrorOnFailure() {
+        mockFailureCallback();
+
+        Card.tokenize(mBraintreeFragment, null);
+
+        verify(mBraintreeFragment).postCallback(any(ErrorWithResponse.class));
+    }
+
+    @Test
+    public void tokenize_sendsAnalyticsEventOnFailure() {
+        mockFailureCallback();
+
+        Card.tokenize(mBraintreeFragment, null);
+
+        verify(mBraintreeFragment).sendAnalyticsEvent("card.nonce-failed");
+    }
+
+    /* helpers */
+    private void mockSuccessCallback() {
+        mockStatic(TokenizationClient.class);
+        doAnswer(new Answer<Void>() {
+            @Override
+            public Void answer(InvocationOnMock invocation) throws Throwable {
+                ((PaymentMethodNonceCallback) invocation.getArguments()[2]).success(new CardNonce());
+                return null;
+            }
+        }).when(TokenizationClient.class);
+        TokenizationClient.tokenize(any(BraintreeFragment.class), any(PaymentMethodBuilder.class),
+                any(PaymentMethodNonceCallback.class));
+    }
+
+    private void mockFailureCallback() {
+        mockStatic(TokenizationClient.class);
+        doAnswer(new Answer<Void>() {
+            @Override
+            public Void answer(InvocationOnMock invocation) throws Throwable {
+                ((PaymentMethodNonceCallback) invocation.getArguments()[2]).failure(new ErrorWithResponse(422, ""));
+                return null;
+            }
+        }).when(TokenizationClient.class);
+        TokenizationClient.tokenize(any(BraintreeFragment.class), any(PaymentMethodBuilder.class),
+                any(PaymentMethodNonceCallback.class));
+    }
+}
