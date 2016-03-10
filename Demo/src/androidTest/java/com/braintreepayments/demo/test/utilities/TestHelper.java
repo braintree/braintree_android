624c3545aa346e7f9025d90ce580e8e722cb8fff
lkorth <luke@lukekorth.com>
2016-03-10 13:14:53 -0500

    Clear all preferences before demo app tests

--- Demo/src/androidTest/java/com/braintreepayments/demo/test/utilities/TestHelper.java
+++ Demo/src/androidTest/java/com/braintreepayments/demo/test/utilities/TestHelper.java
@@ -1,6 +1,7 @@
 package com.braintreepayments.demo.test.utilities;
 
 import android.Manifest.permission;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
 import android.support.v4.content.ContextCompat;
@@ -17,13 +18,24 @@ import static org.hamcrest.CoreMatchers.equalTo;
 public class TestHelper {
 
     public void setup() {
+        clearPreference("BraintreeApi");
+        clearPreference("PayPalOTC");
+
         PreferenceManager.getDefaultSharedPreferences(getTargetContext())
                 .edit()
                 .clear()
                 .commit();
+
         onDevice().onHomeScreen().launchApp("com.braintreepayments.demo");
         enableStoragePermission();
         ensureEnvironmentIs("Sandbox");
+    }
+
+    private void clearPreference(String preference) {
+        getTargetContext().getSharedPreferences(preference, Context.MODE_PRIVATE)
+                .edit()
+                .clear()
+                .commit();
     }
 
     private static void ensureEnvironmentIs(String environment) {
