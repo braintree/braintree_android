7dd92a8491164c2a9dc6ecd1e88592b2f6fdb31c
quinnjn <quinn.neumiiller@getbraintree.com>
2016-07-25 13:37:39 -0700

    Removing extra 0

--- TestUtils/src/main/java/com/braintreepayments/testutils/CardNumber.java
+++ TestUtils/src/main/java/com/braintreepayments/testutils/CardNumber.java
@@ -26,5 +26,5 @@ public class CardNumber {
     public static final String UNIONPAY_DEBIT = "6212345678901265";
     public static final String UNIONPAY_SINGLE_STEP_SALE = "6212345678900093";
     public static final String UNIONPAY_SMS_NOT_REQUIRED = "6212345678900085";
-    public static final String UNIONPAY_NOT_ACCEPTED = "62123456789000028";
+    public static final String UNIONPAY_NOT_ACCEPTED = "6212345678900028";
 }
