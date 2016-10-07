41183f22812f9b11a3806e320a3d60f5d500689d
lkorth <luke@lukekorth.com>
2016-10-07 10:37:09 -0400

    Convert DataCollector tests from unit tests to instrumentation tests

---
+++ Braintree/src/androidTest/java/com/braintreepayments/api/DataCollectorTest.java
@@ -0,0 +1,123 @@
+package com.braintreepayments.api;
+
+import android.app.Activity;
+import android.support.test.runner.AndroidJUnit4;
+import android.text.TextUtils;
+
+import com.braintreepayments.api.interfaces.BraintreeResponseListener;
+import com.braintreepayments.api.test.BraintreeActivityTestRule;
+import com.braintreepayments.api.test.TestActivity;
+import com.braintreepayments.testutils.TestConfigurationBuilder;
+import com.braintreepayments.testutils.TestConfigurationBuilder.TestKountConfigurationBuilder;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.CountDownLatch;
+
+import static com.braintreepayments.api.BraintreeFragmentTestUtils.getFragmentWithConfiguration;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.fail;
+
+@RunWith(AndroidJUnit4.class)
+public class DataCollectorTest {
+
+    @Rule
+    public final BraintreeActivityTestRule<TestActivity> mActivityTestRule =
+            new BraintreeActivityTestRule<>(TestActivity.class);
+
+    private Activity mActivity;
+    private CountDownLatch mCountDownLatch;
+
+    @Before
+    public void setup() {
+        mActivity = mActivityTestRule.getActivity();
+        mCountDownLatch = new CountDownLatch(1);
+    }
+
+    @Test(timeout = 10000)
+    public void collectDeviceData_withListener() throws InterruptedException {
+        String configuration = new TestConfigurationBuilder()
+                .kount(new TestKountConfigurationBuilder()
+                        .enabled(true)
+                        .kountMerchantId("500000"))
+                .build();
+        BraintreeFragment fragment = getFragmentWithConfiguration(mActivity, configuration);
+
+        DataCollector.collectDeviceData(fragment, new BraintreeResponseListener<String>() {
+            @Override
+            public void onResponse(String deviceData) {
+                try {
+                    JSONObject json = new JSONObject(deviceData);
+                    assertFalse(TextUtils.isEmpty(json.getString("device_session_id")));
+                    assertEquals("500000", json.getString("fraud_merchant_id"));
+                    assertNotNull(json.getString("correlation_id"));
+                } catch (JSONException e) {
+                    fail(e.getMessage());
+                }
+
+                mCountDownLatch.countDown();
+            }
+        });
+
+        mCountDownLatch.await();
+    }
+
+    @Test(timeout = 10000)
+    public void collectDeviceData_withListener_usesDirectMerchantId() throws InterruptedException {
+        String configuration = new TestConfigurationBuilder()
+                .kount(new TestKountConfigurationBuilder()
+                        .enabled(true)
+                        .kountMerchantId("600000"))
+                .build();
+        BraintreeFragment fragment = getFragmentWithConfiguration(mActivity, configuration);
+
+        DataCollector.collectDeviceData(fragment, "600001", new BraintreeResponseListener<String>() {
+            @Override
+            public void onResponse(String deviceData) {
+                try {
+                    JSONObject json = new JSONObject(deviceData);
+                    assertFalse(TextUtils.isEmpty(json.getString("device_session_id")));
+                    assertEquals("600001", json.getString("fraud_merchant_id"));
+                    assertNotNull(json.getString("correlation_id"));
+                } catch (JSONException e) {
+                    fail(e.getMessage());
+                }
+
+                mCountDownLatch.countDown();
+            }
+        });
+
+        mCountDownLatch.await();
+    }
+
+    @Test(timeout = 1000)
+    public void collectDeviceData_doesNotCollectKountDataIfKountDisabledInConfiguration() throws InterruptedException {
+        BraintreeFragment fragment = getFragmentWithConfiguration(mActivity, new TestConfigurationBuilder().build());
+
+        DataCollector.collectDeviceData(fragment, new BraintreeResponseListener<String>() {
+            @Override
+            public void onResponse(String deviceData) {
+                try {
+                    JSONObject json = new JSONObject(deviceData);
+                    assertNull(Json.optString(json, "device_session_id", null));
+                    assertNull(Json.optString(json, "fraud_merchant_id", null));
+                    assertNotNull(json.getString("correlation_id"));
+                } catch (JSONException e) {
+                    fail(e.getMessage());
+                }
+
+                mCountDownLatch.countDown();
+            }
+        });
+
+        mCountDownLatch.await();
+    }
+}
--- Braintree/src/test/java/com/braintreepayments/api/DataCollectorUnitTest.java
+++ Braintree/src/test/java/com/braintreepayments/api/DataCollectorUnitTest.java
@@ -2,12 +2,6 @@ package com.braintreepayments.api;
 
 import android.text.TextUtils;
 
-import com.braintreepayments.api.interfaces.BraintreeResponseListener;
-import com.braintreepayments.api.models.Configuration;
-import com.braintreepayments.api.test.RobolectricCountDownLatch;
-import com.braintreepayments.testutils.TestConfigurationBuilder;
-import com.braintreepayments.testutils.TestConfigurationBuilder.TestKountConfigurationBuilder;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Test;
@@ -15,18 +9,12 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.RuntimeEnvironment;
 
-import java.util.concurrent.CountDownLatch;
-
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertNull;
-import static junit.framework.Assert.fail;
 
 @RunWith(RobolectricGradleTestRunner.class)
 public class DataCollectorUnitTest {
-
-    private static BraintreeResponseListener<String> sListener;
 
     @Test
     public void getDeviceCollectorEnvironment_returnsCorrectEnvironment() {
@@ -63,93 +51,5 @@ public class DataCollectorUnitTest {
     public void getPayPalClientMetadataId_returnsClientMetadataId() {
         String clientMetadataId = DataCollector.getPayPalClientMetadataId(RuntimeEnvironment.application);
         assertFalse(TextUtils.isEmpty(clientMetadataId));
-    }
-
-    @Test(timeout = 20000)
-    public void collectDeviceData_withListener() {
-        Configuration configuration = new TestConfigurationBuilder()
-                .kount(new TestKountConfigurationBuilder()
-                        .enabled(true)
-                        .kountMerchantId("500000"))
-                .buildConfiguration();
-        BraintreeFragment fragment = new MockFragmentBuilder()
-                .configuration(configuration)
-                .build();
-        final RobolectricCountDownLatch latch = new RobolectricCountDownLatch(1);
-        sListener = new BraintreeResponseListener<String>() {
-            @Override
-            public void onResponse(String deviceData) {
-                try {
-                    JSONObject json = new JSONObject(deviceData);
-                    assertFalse(TextUtils.isEmpty(json.getString("device_session_id")));
-                    assertEquals("500000", json.getString("fraud_merchant_id"));
-                    assertNotNull(json.getString("correlation_id"));
-                } catch (JSONException e) {
-                    fail(e.getMessage());
-                }
-
-                latch.countDown();
-            }
-        };
-
-        DataCollector.collectDeviceData(fragment, sListener);
-
-        latch.await();
-    }
-
-    @Test(timeout = 20000)
-    public void collectDeviceData_withListener_usesDirectMerchantId() {
-        Configuration configuration = new TestConfigurationBuilder()
-                .kount(new TestKountConfigurationBuilder()
-                        .enabled(true)
-                        .kountMerchantId("600000"))
-                .buildConfiguration();
-        BraintreeFragment fragment = new MockFragmentBuilder()
-                .configuration(configuration)
-                .build();
-        final RobolectricCountDownLatch latch = new RobolectricCountDownLatch(1);
-        sListener = new BraintreeResponseListener<String>() {
-            @Override
-            public void onResponse(String deviceData) {
-                try {
-                    JSONObject json = new JSONObject(deviceData);
-                    assertFalse(TextUtils.isEmpty(json.getString("device_session_id")));
-                    assertEquals("600001", json.getString("fraud_merchant_id"));
-                    assertNotNull(json.getString("correlation_id"));
-                } catch (JSONException e) {
-                    fail(e.getMessage());
-                }
-
-                latch.countDown();
-            }
-        };
-
-        DataCollector.collectDeviceData(fragment, "600001", sListener);
-
-        latch.await();
-    }
-
-    @Test(timeout = 1000)
-    public void collectDeviceData_doesNotCollectKountDataIfKountDisabledInConfiguration() throws InterruptedException {
-        BraintreeFragment fragment = new MockFragmentBuilder().build();
-        final CountDownLatch latch = new CountDownLatch(1);
-
-        DataCollector.collectDeviceData(fragment, new BraintreeResponseListener<String>() {
-            @Override
-            public void onResponse(String deviceData) {
-                try {
-                    JSONObject json = new JSONObject(deviceData);
-                    assertNull(Json.optString(json, "device_session_id", null));
-                    assertNull(Json.optString(json, "fraud_merchant_id", null));
-                    assertNotNull(json.getString("correlation_id"));
-                } catch (JSONException e) {
-                    fail(e.getMessage());
-                }
-
-                latch.countDown();
-            }
-        });
-
-        latch.await();
     }
 }
