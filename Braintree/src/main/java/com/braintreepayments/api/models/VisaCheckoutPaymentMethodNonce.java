0a992f58226e9a3a8f644dcf1e7e8677b85d6a2d
mrak and quinnjn <code@getbraintree.com>
2016-10-07 16:31:49 -0700

    dxjs 6FmIhPfx - Add properties and tests for VisaCheckoutPaymentMethodNonce

--- Braintree/src/main/java/com/braintreepayments/api/models/VisaCheckoutPaymentMethodNonce.java
+++ Braintree/src/main/java/com/braintreepayments/api/models/VisaCheckoutPaymentMethodNonce.java
@@ -4,23 +4,60 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
 public class VisaCheckoutPaymentMethodNonce extends PaymentMethodNonce implements Parcelable {
     public static final String TYPE = "VisaCheckoutCard";
     protected static final String API_RESOURCE_KEY = "visaCheckoutCards";
 
-    protected VisaCheckoutPaymentMethodNonce(Parcel in) {
-        super(in);
+    private static final String CARD_DETAILS_KEY = "details";
+    private static final String CARD_TYPE_KEY = "cardType";
+    private static final String LAST_TWO_KEY = "lastTwo";
+
+    private String mLastTwo;
+    private String mCardType;
+
+    public String getLastTwo() {
+        return mLastTwo;
     }
+
+    public String getCardType() {
+        return mCardType;
+    }
+
+    @Override
+    public String getTypeLabel() {
+        return "Visa Checkout";
+    }
+
+    public static VisaCheckoutPaymentMethodNonce fromJson(String json) throws JSONException {
+        VisaCheckoutPaymentMethodNonce visaCheckoutPaymentMethodNonce = new VisaCheckoutPaymentMethodNonce();
+        visaCheckoutPaymentMethodNonce.fromJson(PaymentMethodNonce.getJsonObjectForType(API_RESOURCE_KEY, json));
+        return visaCheckoutPaymentMethodNonce;
+    }
+
+    @Override
+    protected void fromJson(JSONObject json) throws JSONException {
+        super.fromJson(json);
+
+        JSONObject details = json.getJSONObject(CARD_DETAILS_KEY);
+        mLastTwo = details.getString(LAST_TWO_KEY);
+        mCardType = details.getString(CARD_TYPE_KEY);
+    }
+
+    private VisaCheckoutPaymentMethodNonce() {}
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         super.writeToParcel(dest, flags);
+        dest.writeString(mLastTwo);
+        dest.writeString(mCardType);
     }
 
-    @Override
-    public int describeContents() {
-        return 0;
+    protected VisaCheckoutPaymentMethodNonce(Parcel in) {
+        super(in);
+        mLastTwo = in.readString();
+        mCardType = in.readString();
     }
 
     public static final Creator<VisaCheckoutPaymentMethodNonce> CREATOR =
@@ -36,17 +73,4 @@ public class VisaCheckoutPaymentMethodNonce extends PaymentMethodNonce implement
                 }
             };
 
-    @Override
-    public String getTypeLabel() {
-        return "Visa Checkout";
-    }
-
-    public static PaymentMethodNonce fromJson(String json) throws JSONException {
-        VisaCheckoutPaymentMethodNonce visaCheckoutPaymentMethodNonce = new VisaCheckoutPaymentMethodNonce();
-        visaCheckoutPaymentMethodNonce.fromJson(PaymentMethodNonce.getJsonObjectForType(API_RESOURCE_KEY, json));
-        return visaCheckoutPaymentMethodNonce;
-    }
-
-    private VisaCheckoutPaymentMethodNonce() {
-    }
 }
---
+++ Braintree/src/test/java/com/braintreepayments/api/models/VisaCheckoutPaymentMethodNonceUnitTest.java
@@ -0,0 +1,47 @@
+package com.braintreepayments.api.models;
+
+import android.os.Parcel;
+
+import org.json.JSONException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+
+import static com.braintreepayments.testutils.FixturesHelper.stringFromFixture;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+
+@RunWith(RobolectricGradleTestRunner.class)
+public class VisaCheckoutPaymentMethodNonceUnitTest {
+
+    @Test
+    public void fromJson_createsVisaCheckoutPaymentMethodNonce() throws JSONException {
+        VisaCheckoutPaymentMethodNonce paymentMethodNonce = VisaCheckoutPaymentMethodNonce.fromJson(
+                stringFromFixture("payment_methods/visa_checkout_response.json"));
+
+        assertEquals("11", paymentMethodNonce.getLastTwo());
+        assertEquals("Visa", paymentMethodNonce.getCardType());
+        assertEquals("123456-12345-12345-a-adfa", paymentMethodNonce.getNonce());
+        assertEquals("ending in ••11", paymentMethodNonce.getDescription());
+        assertFalse(paymentMethodNonce.isDefault());
+        assertEquals("Visa Checkout", paymentMethodNonce.getTypeLabel());
+    }
+
+    @Test
+    public void parcelsCorrectly() throws JSONException {
+        VisaCheckoutPaymentMethodNonce expected = VisaCheckoutPaymentMethodNonce.fromJson(
+                stringFromFixture("payment_methods/visa_checkout_response.json"));
+
+        Parcel parcel = Parcel.obtain();
+        expected.writeToParcel(parcel, 0);
+        parcel.setDataPosition(0);
+        VisaCheckoutPaymentMethodNonce actual = VisaCheckoutPaymentMethodNonce.CREATOR.createFromParcel(parcel);
+
+        assertEquals(expected.getNonce(), actual.getNonce());
+        assertEquals(expected.getDescription(), actual.getDescription());
+        assertEquals(expected.isDefault(), actual.isDefault());
+
+        assertEquals(expected.getLastTwo(), actual.getLastTwo());
+        assertEquals(expected.getCardType(), actual.getCardType());
+    }
+}
