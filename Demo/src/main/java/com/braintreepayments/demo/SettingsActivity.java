63d31e48a14fdf5faef7355cc00c4f96bd266ab2
lkorth <luke@lukekorth.com>
2016-10-24 13:44:45 -0400

    Fix crash in demo app settings

--- Demo/src/main/java/com/braintreepayments/demo/SettingsActivity.java
+++ Demo/src/main/java/com/braintreepayments/demo/SettingsActivity.java
@@ -1,19 +1,19 @@
 package com.braintreepayments.demo;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 import com.braintreepayments.demo.fragments.SettingsFragment;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends AppCompatActivity {
 
     @SuppressWarnings("ConstantConditions")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
