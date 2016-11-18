d32f27763dd89126708b9230a42722293c40f068
lkorth <luke@lukekorth.com>
2016-11-18 16:26:03 -0500

    Remove Espresso Intents

--- Braintree/src/androidTest/java/com/braintreepayments/api/test/BraintreeActivityTestRule.java
+++ Braintree/src/androidTest/java/com/braintreepayments/api/test/BraintreeActivityTestRule.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.app.KeyguardManager;
 import android.app.KeyguardManager.KeyguardLock;
 import android.content.Context;
-import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 
 import static android.support.test.InstrumentationRegistry.getTargetContext;
@@ -36,20 +35,10 @@ public class BraintreeActivityTestRule<T extends Activity> extends ActivityTestR
         mKeyguardLock.disableKeyguard();
     }
 
-    @Override
-    protected void afterActivityLaunched() {
-        Intents.init();
-        super.afterActivityLaunched();
-    }
-
     @SuppressLint("MissingPermission")
     @Override
     protected void afterActivityFinished() {
         super.afterActivityFinished();
-
-        try {
-            Intents.release();
-        } catch (IllegalStateException ignored) {}
 
         getSharedPreferences(getTargetContext()).edit().clear().commit();
 
