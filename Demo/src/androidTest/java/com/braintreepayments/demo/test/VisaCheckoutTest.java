141433a0aaf5a55da2900e90eeca779cc349f4b0
quinnjn <quinn@getbraintree.com>
2017-01-04 09:00:06 -0800

    Tokenization UI test

--- Demo/src/androidTest/java/com/braintreepayments/demo/test/VisaCheckoutTest.java
+++ Demo/src/androidTest/java/com/braintreepayments/demo/test/VisaCheckoutTest.java
@@ -5,14 +5,17 @@ import android.support.test.runner.AndroidJUnit4;
 import com.braintreepayments.demo.test.utilities.TestHelper;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static com.braintreepayments.api.testutils.BuildConfig.VISA_CHECKOUT_PASSWORD;
+import static com.braintreepayments.api.testutils.BuildConfig.VISA_CHECKOUT_USERNAME;
 import static com.lukekorth.deviceautomator.AutomatorAction.click;
+import static com.lukekorth.deviceautomator.AutomatorAssertion.text;
 import static com.lukekorth.deviceautomator.DeviceAutomator.onDevice;
 import static com.lukekorth.deviceautomator.UiObjectMatcher.withContentDescription;
 import static com.lukekorth.deviceautomator.UiObjectMatcher.withText;
+import static org.hamcrest.core.StringContains.containsString;
 
 @RunWith(AndroidJUnit4.class)
 public class VisaCheckoutTest extends TestHelper {
@@ -43,7 +46,17 @@ public class VisaCheckoutTest extends TestHelper {
     }
 
     @Test(timeout = 60000)
-    @Ignore("unimplemented")
     public void tokenizesVisaCheckout() {
+        onDevice(withContentDescription("Visa Checkout")).perform(click());
+        onDevice(withContentDescription("Email or Mobile Number")).perform(click());
+        for (int i=0; i<VISA_CHECKOUT_USERNAME.length(); i++) {
+            onDevice(withContentDescription("Email or Mobile Number")).pressDelete();
+        }
+        onDevice().typeText(VISA_CHECKOUT_USERNAME);
+        onDevice(withContentDescription("Password")).perform(click());
+        onDevice().typeText(VISA_CHECKOUT_PASSWORD);
+        onDevice(withText("Sign In")).perform(click());
+        onDevice(withText("Continue")).perform(click());
+        getNonceDetails().check(text(containsString("Nonce:")));
     }
 }
