32f35dea80ef4930c697ed95d5320a9113a0895c
Brian Guertin <contact@brianguertin.com>
2017-04-10 17:16:47 -0700

    Include cause in ConfigurationException

--- Braintree/src/main/java/com/braintreepayments/api/exceptions/BraintreeException.java
+++ Braintree/src/main/java/com/braintreepayments/api/exceptions/BraintreeException.java
@@ -7,6 +7,10 @@ import java.io.IOException;
  */
 public class BraintreeException extends IOException {
 
+    public BraintreeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
     public BraintreeException(String message) {
         super(message);
     }
