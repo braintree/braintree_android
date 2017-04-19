f757d95f28e7ca5dca575e10fb2d9cd9109fbaf0
lkorth <luke@lukekorth.com>
2017-04-19 12:33:05 -0400

    Improve Visa Checkout tests in demo app

--- Demo/src/androidTest/java/com/braintreepayments/demo/test/VisaCheckoutTest.java
+++ Demo/src/androidTest/java/com/braintreepayments/demo/test/VisaCheckoutTest.java
@@ -1,9 +1,7 @@
 package com.braintreepayments.demo.test;
 
 import android.support.test.runner.AndroidJUnit4;
-import android.support.test.uiautomator.UiObject;
 import android.support.test.uiautomator.UiObjectNotFoundException;
-import android.support.test.uiautomator.UiSelector;
 
 import com.braintreepayments.demo.test.utilities.TestHelper;
 
@@ -11,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static com.lukekorth.deviceautomator.AutomatorAction.check;
 import static com.lukekorth.deviceautomator.AutomatorAction.clearTextField;
 import static com.lukekorth.deviceautomator.AutomatorAction.click;
 import static com.lukekorth.deviceautomator.AutomatorAssertion.text;
@@ -18,7 +17,9 @@ import static com.lukekorth.deviceautomator.DeviceAutomator.onDevice;
 import static com.lukekorth.deviceautomator.UiObjectMatcher.withContentDescription;
 import static com.lukekorth.deviceautomator.UiObjectMatcher.withResourceId;
 import static com.lukekorth.deviceautomator.UiObjectMatcher.withText;
+import static com.lukekorth.deviceautomator.UiObjectMatcher.withTextStartingWith;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.core.StringEndsWith.endsWith;
 
 @RunWith(AndroidJUnit4.class)
 public class VisaCheckoutTest extends TestHelper {
@@ -48,19 +49,16 @@ public class VisaCheckoutTest extends TestHelper {
         onDevice(withText("Reset")).waitForExists();
     }
 
-    @Test(timeout = 60000)
+    @Test(timeout = 120000)
     public void tokenizesVisaCheckout() throws UiObjectNotFoundException {
-        String rememberMeResourceId = "com.braintreepayments.demo:id/com_visa_checkout_cbSignInUsernamePreferences";
-
-        if (new UiObject(new UiSelector().text("New To Visa Checkout?")).exists()) {
-            // If we land in the sign up page, move to the login page.
+        if (onDevice(withText("New to Visa Checkout?")).exists()) {
             onDevice(withText("Sign In")).perform(click());
         }
 
-        // TODO Device Automator doesn't check for checked inputs
-        if (new UiObject(new UiSelector().resourceId(rememberMeResourceId)).isChecked()) {
-            onDevice(withResourceId(rememberMeResourceId)).perform(click());
-        }
+        try {
+            onDevice(withResourceId("com.braintreepayments.demo:id/com_visa_checkout_cbSignInUsernamePreferences"))
+                    .perform(check(false));
+        } catch (RuntimeException ignored) {}
 
         onDevice(withContentDescription("Email or Mobile Number")).perform(click(), clearTextField());
         onDevice(withContentDescription("Email or Mobile Number")).typeText(VISA_CHECKOUT_USERNAME);
@@ -78,5 +76,8 @@ public class VisaCheckoutTest extends TestHelper {
                 text(containsString("Email: no-reply-visa-checkout@getbraintree.com")),
                 text(containsString("Billing Address: NoReply VisaCheckout 123 Townsend Street  San Francisco 94107 CA US")),
                 text(containsString("Shipping Address: NoReply VisaCheckout 123 Townsend Street  San Francisco 94107 CA US")));
+
+        onDevice(withText("Create a Transaction")).perform(click());
+        onDevice(withTextStartingWith("created")).check(text(endsWith("authorized")));
     }
 }
