54f3b5896e8596a2a30477ecea2a83f45ccc7d8f
Steve Coffey <steve.coffey@getbraintree.com>
2018-02-06 11:38:35 -0800

    BTDXCLTS-1015 Ensure GooglePaymentException is fully parcelable (#325)
    * BTDXCLTS-1015 Ensure GooglePaymentException is fully parcelable
    
    * Remove extra line in GooglePaymentExceptionTest

--- Braintree/src/main/java/com/braintreepayments/api/exceptions/GooglePaymentException.java
+++ Braintree/src/main/java/com/braintreepayments/api/exceptions/GooglePaymentException.java
@@ -1,11 +1,14 @@
 package com.braintreepayments.api.exceptions;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.android.gms.common.api.Status;
 
 /**
  * Error class thrown when a Google Payment exception is encountered.
  */
-public class GooglePaymentException extends BraintreeException {
+public class GooglePaymentException extends BraintreeException implements Parcelable {
 
     private Status mStatus;
 
@@ -22,4 +25,32 @@ public class GooglePaymentException extends BraintreeException {
     public Status getStatus() {
         return mStatus;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(getMessage());
+        dest.writeParcelable(mStatus, 0);
+    }
+
+    protected GooglePaymentException(Parcel in) {
+        super(in.readString());
+        mStatus = in.readParcelable(Status.class.getClassLoader());
+    }
+
+    public static final Creator<GooglePaymentException> CREATOR = new Creator<GooglePaymentException>() {
+        @Override
+        public GooglePaymentException createFromParcel(Parcel in) {
+            return new GooglePaymentException(in);
+        }
+
+        @Override
+        public GooglePaymentException[] newArray(int size) {
+            return new GooglePaymentException[size];
+        }
+    };
 }
