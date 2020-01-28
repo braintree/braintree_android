660df4e295f2fb4df559ed6ad93b4d241367306c
sestevens <51723734+sestevens@users.noreply.github.com>
2020-01-28 16:06:22 -0600

    Update GooglePayment/src/main/java/com/braintreepayments/api/models/ReadyForGooglePayRequest.java

--- GooglePayment/src/main/java/com/braintreepayments/api/models/ReadyForGooglePayRequest.java
+++ GooglePayment/src/main/java/com/braintreepayments/api/models/ReadyForGooglePayRequest.java
@@ -14,7 +14,7 @@ public class ReadyForGooglePayRequest {
     /**
      * If set to true, then the {@link GooglePayment#isReadyToPay(BraintreeFragment, ReadyForGooglePayRequest, BraintreeResponseListener)}
      * method will call the listener with true if the customer is ready to pay with one or more of your
-     * allowed card networks.
+     * supported card networks.
      *
      * @param value Indicates whether the customer must have one or more payment methods from your allowed card networks in order
      *              to be considered ready to pay with Google Pay
