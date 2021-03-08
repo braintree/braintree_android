cf02a968240b4589e846a3016804e3fb1ca29716
Steven Shropshire <steven.shropshire@getbraintree.com>
2021-03-08 15:52:53 -0600

    Add VenmoRequest java docs
    Signed-off-by: Sarah Koop <skoop@paypal.com>

--- Venmo/src/main/java/com/braintreepayments/api/VenmoRequest.java
+++ Venmo/src/main/java/com/braintreepayments/api/VenmoRequest.java
@@ -1,24 +1,47 @@
 package com.braintreepayments.api;
 
+/**
+ A VenmoRequest specifies options that contribute to the Venmo flow
+ */
 public class VenmoRequest {
 
     private boolean shouldVault;
     private String profileID;
 
+    /**
+     * @param shouldVault Optional - Whether or not to automatically vault the Venmo Account.
+     *                    Vaulting will only occur if a client token with a customer_id is being used.
+     *                    Defaults to false.
+     * @return {@link VenmoRequest}
+     */
     public VenmoRequest shouldVault(boolean shouldVault) {
         this.shouldVault = shouldVault;
         return this;
     }
 
+    /**
+     * @param profileID Optional - The Venmo profile ID to be used during payment authorization.
+     *                  Customers will see the business name and logo associated with this Venmo
+     *                  profile, and it will show up in the Venmo app as a "Connected Merchant".
+     *                  Venmo profile IDs can be found in the Braintree Control Panel.
+     *                  Leaving this `null` will use the default Venmo profile.
+     * @return {@link VenmoRequest}
+     */
     public VenmoRequest profileID(String profileID) {
         this.profileID = profileID;
         return this;
     }
 
+    /**
+     * @return Whether or not to automatically vault the Venmo Account.
+     */
     public boolean shouldVault() {
         return shouldVault;
     }
 
+    /**
+     * @return The Venmo profile ID to be used during payment authorization.
+     */
     public String getProfileID() {
         return profileID;
     }
