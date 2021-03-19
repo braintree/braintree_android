6f2042efeebb8992533afd6002c4298cb5d113e5
Sarah Koop <skoop@paypal.com>
2021-03-19 16:17:41 -0500

    Remove unused callbacks and update javadocs for ThreeDSecureResultCallback.
    Signed-off-by: Steven Shropshire <steven.shropshire@getbraintree.com>

--- ThreeDSecure/src/main/java/com/braintreepayments/api/ThreeDSecureResultCallback.java
+++ ThreeDSecure/src/main/java/com/braintreepayments/api/ThreeDSecureResultCallback.java
@@ -7,8 +7,9 @@ import androidx.fragment.app.FragmentActivity;
 
 /**
  * Callback for receiving result of
- * {@link ThreeDSecureClient#initiateChallengeWithLookup(FragmentActivity, ThreeDSecureRequest, ThreeDSecureResult, ThreeDSecureResultCallback)} and
- * {@link ThreeDSecureClient#onBrowserSwitchResult(BrowserSwitchResult, ThreeDSecureResultCallback)} and
+ * {@link ThreeDSecureClient#performLookup(FragmentActivity, ThreeDSecureRequest, ThreeDSecureResultCallback)},
+ * {@link ThreeDSecureClient#initiateChallengeWithLookup(FragmentActivity, ThreeDSecureRequest, ThreeDSecureResult, ThreeDSecureResultCallback)},
+ * {@link ThreeDSecureClient#onBrowserSwitchResult(BrowserSwitchResult, ThreeDSecureResultCallback)}, and
  * {@link ThreeDSecureClient#onActivityResult(int, Intent, ThreeDSecureResultCallback)}.
  */
 public interface ThreeDSecureResultCallback {
