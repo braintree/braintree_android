6e181bdadffbfda72cb71d1c1cbd70013e3dd707
Sarah Koop <skoop@paypal.com>
2021-05-05 15:26:13 -0500

    Remove unused imports.
    Signed-off-by: Steven Shropshire <steven.shropshire@getbraintree.com>

--- Venmo/src/main/java/com/braintreepayments/api/VenmoClient.java
+++ Venmo/src/main/java/com/braintreepayments/api/VenmoClient.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.text.TextUtils;
-import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
