4345e8e398de0320e09e6b235b4196c519ea8387
Sarah Koop <skoop@paypal.com>
2021-05-06 10:38:43 -0500

    Stub out PayPalNativeClient#tokenizePayPalAccount() method.
    Signed-off-by: Steven Shropshire <steven.shropshire@getbraintree.com>

--- PayPalNative/src/main/java/com/braintreepayments/api/PayPalNativeClient.java
+++ PayPalNative/src/main/java/com/braintreepayments/api/PayPalNativeClient.java
@@ -1,8 +1,36 @@
 package com.braintreepayments.api;
 
+import androidx.fragment.app.FragmentActivity;
+
 public class PayPalNativeClient {
 
-    PayPalNativeClient(BraintreeClient braintreeClient) {
+    private final BraintreeClient braintreeClient;
 
+    public PayPalNativeClient(BraintreeClient braintreeClient) {
+        this.braintreeClient = braintreeClient;
+    }
+
+    public void tokenizePayPalAccount(final FragmentActivity activity) {
+        braintreeClient.getConfiguration(new ConfigurationCallback() {
+            @Override
+            public void onResult(Configuration configuration, Exception error) {
+                if (error != null) {
+                    // TODO: callback error
+                    return;
+                }
+
+                String payPalClientId = configuration.getPayPalClientId();
+                if (payPalClientId == null) {
+                    // TODO: callback error
+                    return;
+                }
+
+                startPayPalNativeCheckout(activity, configuration, payPalClientId);
+            }
+        });
+    }
+
+    private void startPayPalNativeCheckout(FragmentActivity activity, Configuration configuration, String payPalClientId) {
+        // TODO: configure and start paypal native checkout
     }
 }
