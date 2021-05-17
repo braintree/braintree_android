1c2820cf96158950cb03050f871f1b45290d8e1e
Jose Noriega <jonoriega@paypal.com>
2021-05-17 09:45:14 -0300

    Removed interface, use concrete type for now

--- PayPalNative/src/main/java/com/braintreepayments/api/PayPalNativeVaultRequest.java
+++ PayPalNative/src/main/java/com/braintreepayments/api/PayPalNativeVaultRequest.java
@@ -2,5 +2,5 @@ package com.braintreepayments.api;
 
 //TODO: once that native checkout supports vault, this class should be eliminated,
 // for now, we use it to fallback into the web experience
-public class PayPalNativeVaultRequest extends PayPalVaultRequest implements PayPalNativeRequest {
+public class PayPalNativeVaultRequest extends PayPalVaultRequest {
 }
