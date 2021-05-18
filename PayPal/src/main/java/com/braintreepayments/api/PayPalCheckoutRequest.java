0e6b81e3c707f9fe5972e62b4a99d1e1bbee409e
skoop <skoop@paypal.com>
2021-05-18 11:21:33 -0500

    Remove thousands separator from doc string

--- PayPal/src/main/java/com/braintreepayments/api/PayPalCheckoutRequest.java
+++ PayPal/src/main/java/com/braintreepayments/api/PayPalCheckoutRequest.java
@@ -49,8 +49,8 @@ public class PayPalCheckoutRequest extends PayPalRequest implements Parcelable {
     /**
      * @param amount The transaction amount in currency units (as * determined by setCurrencyCode).
      *               For example, "1.20" corresponds to one dollar and twenty cents. Amount must be a non-negative
-     *               number, may optionally contain exactly 2 decimal places separated by '.', optional
-     *               thousands separator ',', limited to 7 digits before the decimal point.
+     *               number, may optionally contain exactly 2 decimal places separated by '.',
+     *               limited to 7 digits before the decimal point.
      *               <p>
      *               This amount may differ slightly from the transaction amount. The exact decline rules
      *               for mismatches between this client-side amount and the final amount in the Transaction
