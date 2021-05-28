a2dc620632f9fcca3594acece6a94f12ea198899
skoop <skoop@paypal.com>
2021-05-28 09:23:09 -0500

    Remove collectRiskData

--- BraintreeDataCollector/src/test/java/com/braintreepayments/api/DataCollectorUnitTest.java
+++ BraintreeDataCollector/src/test/java/com/braintreepayments/api/DataCollectorUnitTest.java
@@ -12,18 +12,12 @@ import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.robolectric.RobolectricTestRunner;
 
-import java.util.Map;
-
-import static com.braintreepayments.api.FixturesHelper.base64Encode;
 import static junit.framework.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isNull;
-import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 @RunWith(RobolectricTestRunner.class)
@@ -120,104 +114,5 @@ public class DataCollectorUnitTest {
         String deviceData = deviceDataCaptor.getValue();
         JSONObject json = new JSONObject(deviceData);
         assertEquals("sample_correlation_id", json.getString("correlation_id"));
-    }
-
-    @Test
-    public void collectRiskData_withTokenizationKey_requestsClientMetadataIdFromPayPal() throws JSONException, InvalidArgumentException {
-        when(payPalDataCollector.getPayPalInstallationGUID(context)).thenReturn("sample_installation_guid");
-        when(payPalDataCollector.getClientMetadataId(context)).thenReturn("sample_correlation_id");
-
-        BraintreeClient braintreeClient = new MockBraintreeClientBuilder()
-                .authorization(Authorization.fromString(Fixtures.TOKENIZATION_KEY))
-                .configuration(Configuration.fromJson(Fixtures.CONFIGURATION_WITH_CARD_COLLECT_DEVICE_DATA))
-                .build();
-
-        DataCollector sut = new DataCollector(
-                braintreeClient, payPalDataCollector, kountDataCollector);
-
-        PaymentMethodNonce paymentMethodNonce = mock(PaymentMethodNonce.class);
-        when(paymentMethodNonce.getString()).thenReturn("sample_nonce");
-
-        sut.collectRiskData(context, paymentMethodNonce);
-
-        ArgumentCaptor<PayPalDataCollectorRequest> captor =
-            ArgumentCaptor.forClass(PayPalDataCollectorRequest.class);
-        verify(payPalDataCollector).getClientMetadataId(same(context), captor.capture());
-
-        PayPalDataCollectorRequest payPalDataCollectorRequest = captor.getValue();
-        assertEquals("sample_installation_guid", payPalDataCollectorRequest.getApplicationGuid());
-        assertEquals("sample_nonce", payPalDataCollectorRequest.getClientMetadataId());
-        assertTrue(payPalDataCollectorRequest.isDisableBeacon());
-
-        Map<String, String> additionalData = payPalDataCollectorRequest.getAdditionalData();
-        assertEquals(2, additionalData.size());
-        assertEquals("bt_card", additionalData.get("rda_tenant"));
-        assertEquals("integration_merchant_id", additionalData.get("mid"));
-    }
-
-    @Test
-    public void collectRiskData_withClientToken_requestsClientMetadataIdFromPayPalUsingCustomerId() throws JSONException, InvalidArgumentException {
-        when(payPalDataCollector.getPayPalInstallationGUID(context)).thenReturn("sample_installation_guid");
-        when(payPalDataCollector.getClientMetadataId(context)).thenReturn("sample_correlation_id");
-
-        BraintreeClient braintreeClient = new MockBraintreeClientBuilder()
-                .authorization(Authorization.fromString(base64Encode(Fixtures.CLIENT_TOKEN_WITH_CUSTOMER_ID_IN_AUTHORIZATION_FINGERPRINT)))
-                .configuration(Configuration.fromJson(Fixtures.CONFIGURATION_WITH_CARD_COLLECT_DEVICE_DATA))
-                .build();
-
-        DataCollector sut = new DataCollector(
-                braintreeClient, payPalDataCollector, kountDataCollector);
-
-        PaymentMethodNonce paymentMethodNonce = mock(PaymentMethodNonce.class);
-        when(paymentMethodNonce.getString()).thenReturn("sample_nonce");
-
-        sut.collectRiskData(context, paymentMethodNonce);
-
-        ArgumentCaptor<PayPalDataCollectorRequest> captor =
-                ArgumentCaptor.forClass(PayPalDataCollectorRequest.class);
-        verify(payPalDataCollector).getClientMetadataId(same(context), captor.capture());
-
-        PayPalDataCollectorRequest payPalDataCollectorRequest = captor.getValue();
-        assertEquals("sample_installation_guid", payPalDataCollectorRequest.getApplicationGuid());
-        assertEquals("sample_nonce", payPalDataCollectorRequest.getClientMetadataId());
-        assertTrue(payPalDataCollectorRequest.isDisableBeacon());
-
-        Map<String, String> additionalData = payPalDataCollectorRequest.getAdditionalData();
-        assertEquals(3, additionalData.size());
-        assertEquals("bt_card", additionalData.get("rda_tenant"));
-        assertEquals("integration_merchant_id", additionalData.get("mid"));
-        assertEquals("fake-customer-123", additionalData.get("cid"));
-    }
-
-    @Test
-    public void collectRiskData_whenFraudDetectionDisabled_doesNothing() throws JSONException {
-        BraintreeClient braintreeClient = new MockBraintreeClientBuilder()
-                .configuration(Configuration.fromJson(Fixtures.CONFIGURATION_WITHOUT_ACCESS_TOKEN))
-                .build();
-
-        DataCollector sut = new DataCollector(
-                braintreeClient, payPalDataCollector, kountDataCollector);
-
-        PaymentMethodNonce paymentMethodNonce = mock(PaymentMethodNonce.class);
-        when(paymentMethodNonce.getString()).thenReturn("sample_nonce");
-
-        sut.collectRiskData(context, paymentMethodNonce);
-        verifyNoInteractions(payPalDataCollector);
-    }
-
-    @Test
-    public void collectRiskData_whenConfigurationFetchFails_doesNothing() {
-        BraintreeClient braintreeClient = new MockBraintreeClientBuilder()
-                .configurationError(new Exception("config error"))
-                .build();
-
-        DataCollector sut = new DataCollector(
-                braintreeClient, payPalDataCollector, kountDataCollector);
-
-        PaymentMethodNonce paymentMethodNonce = mock(PaymentMethodNonce.class);
-        when(paymentMethodNonce.getString()).thenReturn("sample_nonce");
-
-        sut.collectRiskData(context, paymentMethodNonce);
-        verifyNoInteractions(payPalDataCollector);
     }
 }
