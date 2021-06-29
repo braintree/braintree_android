4f972432a9edd708d606b50f19c362b7903e791d
Steven <sshropshire@paypal.com>
2021-06-29 14:27:22 -0500

    Rename SamsungPartnerInfoBuilderTestJava to SamsungPartnerInfoBuilderTest.

--- SamsungPay/src/test/java/com/braintreepayments/api/SamsungPartnerInfoBuilderTest.java
+++ SamsungPay/src/test/java/com/braintreepayments/api/SamsungPartnerInfoBuilderTest.java
@@ -14,7 +14,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(RobolectricTestRunner.class)
-public class SamsungPartnerInfoBuilderTestJava {
+public class SamsungPartnerInfoBuilderTest {
 
     @Test
     public void build_setsSamsungServiceId() {
