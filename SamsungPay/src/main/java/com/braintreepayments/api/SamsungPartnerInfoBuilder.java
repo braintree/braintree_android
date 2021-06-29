216ac745270a3f3ee5b45c68491e02164f7727c8
Steven <sshropshire@paypal.com>
2021-06-29 15:04:00 -0500

    Add additional SamsungPartnerInfoBuilder tests.

--- SamsungPay/src/main/java/com/braintreepayments/api/SamsungPartnerInfoBuilder.java
+++ SamsungPay/src/main/java/com/braintreepayments/api/SamsungPartnerInfoBuilder.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 
 import com.samsung.android.sdk.samsungpay.v2.PartnerInfo;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import static com.samsung.android.sdk.samsungpay.v2.SpaySdk.PARTNER_SERVICE_TYPE;
 import static com.samsung.android.sdk.samsungpay.v2.SpaySdk.ServiceType.INAPP_PAYMENT;
 import static com.samsung.android.sdk.samsungpay.v2.payment.PaymentManager.EXTRA_KEY_TEST_MODE;
 
 class SamsungPartnerInfoBuilder {
+
+    private static final String BRAINTREE_TOKENIZATION_API_VERSION = "2018-10-01";
 
     private String sessionId;
     private String integrationType;
@@ -31,11 +36,23 @@ class SamsungPartnerInfoBuilder {
         return this;
     }
 
-    PartnerInfo build() {
+    PartnerInfo build() throws JSONException {
         Bundle data = new Bundle();
         data.putString(PARTNER_SERVICE_TYPE, INAPP_PAYMENT.toString());
 
         data.putBoolean(EXTRA_KEY_TEST_MODE, false);
+
+        JSONObject additionalData = new JSONObject();
+        additionalData.put("braintreeTokenizationApiVersion", BRAINTREE_TOKENIZATION_API_VERSION);
+
+        JSONObject clientSdkMetadata = new MetadataBuilder()
+                .integration(integrationType)
+                .sessionId(sessionId)
+                .version()
+                .build();
+        additionalData.put("clientSdkMetadata", clientSdkMetadata);
+
+        data.putString("additionalData", additionalData.toString());
 
         String serviceId = configuration.getSamsungPayServiceId();
         return new PartnerInfo(serviceId, data);
--- SamsungPay/src/test/java/com/braintreepayments/api/SamsungPartnerInfoBuilderTest.java
+++ SamsungPay/src/test/java/com/braintreepayments/api/SamsungPartnerInfoBuilderTest.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 
 import com.samsung.android.sdk.samsungpay.v2.PartnerInfo;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
+import org.skyscreamer.jsonassert.JSONAssert;
 
 import static com.samsung.android.sdk.samsungpay.v2.SpaySdk.PARTNER_SERVICE_TYPE;
+import static com.samsung.android.sdk.samsungpay.v2.payment.PaymentManager.EXTRA_KEY_TEST_MODE;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -17,7 +22,7 @@ import static org.mockito.Mockito.when;
 public class SamsungPartnerInfoBuilderTest {
 
     @Test
-    public void build_setsSamsungServiceId() {
+    public void build_setsSamsungServiceId() throws JSONException {
         Configuration configuration = mock(Configuration.class);
         when(configuration.getSamsungPayServiceId()).thenReturn("samsung-service-id");
 
@@ -29,8 +34,9 @@ public class SamsungPartnerInfoBuilderTest {
     }
 
     @Test
-    public void build_setsData() {
+    public void build_setsData() throws JSONException {
         Configuration configuration = mock(Configuration.class);
+        when(configuration.getSamsungPayEnvironment()).thenReturn("PRODUCTION");
 
         PartnerInfo partnerInfo = new SamsungPartnerInfoBuilder()
                 .setConfiguration(configuration)
@@ -40,6 +46,18 @@ public class SamsungPartnerInfoBuilderTest {
 
         Bundle data = partnerInfo.getData();
         assertEquals("INAPP_PAYMENT", data.getString(PARTNER_SERVICE_TYPE));
+        assertFalse(data.getBoolean(EXTRA_KEY_TEST_MODE));
 
+        JSONObject expectedAdditionalData = new JSONObject()
+                .put("braintreeTokenizationApiVersion", "2018-10-01")
+                .put("clientSdkMetadata", new JSONObject()
+                        .put("integration", "braintree-integration-type")
+                        .put("platform", "android")
+                        .put("sessionId", "braintree-session-id")
+                        .put("version", BuildConfig.VERSION_NAME)
+                );
+
+        String actualAdditionalData = data.getString("additionalData");
+        JSONAssert.assertEquals(expectedAdditionalData, new JSONObject(actualAdditionalData), true);
     }
 }
