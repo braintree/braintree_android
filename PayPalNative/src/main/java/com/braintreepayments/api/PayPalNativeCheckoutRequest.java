dd0adbacab9d7ec83ff3e76b87fb10d366bddaba
jcnoriega <jose.noriega.defferrari@gmail.com>
2021-06-30 16:55:36 -0300

    Update Doc on PayPalNativeCheckoutRequest
    Co-authored-by: Sarah Koop <skoop@paypal.com>

--- PayPalNative/src/main/java/com/braintreepayments/api/PayPalNativeCheckoutRequest.java
+++ PayPalNative/src/main/java/com/braintreepayments/api/PayPalNativeCheckoutRequest.java
@@ -5,8 +5,8 @@ public class PayPalNativeCheckoutRequest extends PayPalCheckoutRequest {
     /**
      * @param amount The transaction amount in currency units (as * determined by setCurrencyCode).
      *               For example, "1.20" corresponds to one dollar and twenty cents. Amount must be a non-negative
-     *               number, may optionally contain exactly 2 decimal places separated by '.', optional
-     *               thousands separator ',', limited to 7 digits before the decimal point.
+     *               number, may optionally contain exactly 2 decimal places separated by '.'
+     *               and is limited to 7 digits before the decimal point.
      *               <p>
      *               This amount may differ slightly from the transaction amount. The exact decline rules
      *               for mismatches between this client-side amount and the final amount in the Transaction
