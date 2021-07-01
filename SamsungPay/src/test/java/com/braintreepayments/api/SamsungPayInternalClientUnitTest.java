e2981718288027476a527af113dddb23bb21af9c
Steven <sshropshire@paypal.com>
2021-07-01 14:15:46 -0500

    Rename SamsungPayInternalClientTest to SamsungPayInternalClientUnitTest.

--- SamsungPay/src/test/java/com/braintreepayments/api/SamsungPayInternalClientUnitTest.java
+++ SamsungPay/src/test/java/com/braintreepayments/api/SamsungPayInternalClientUnitTest.java
@@ -24,7 +24,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-public class SamsungPayInternalClientTest {
+public class SamsungPayInternalClientUnitTest {
 
     @Captor
     private ArgumentCaptor<List<SpaySdk.Brand>> cardBrandsCaptor;
