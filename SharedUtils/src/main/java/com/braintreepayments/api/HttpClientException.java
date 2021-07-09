b52aa1fb3c23037b176e6d60bdffdd536402785e
Sarah Koop <skoop@paypal.com>
2021-07-09 16:03:51 -0500

    Remove unecessary TODO.
    Signed-off-by: Steven Shropshire <steven.shropshire@getbraintree.com>

--- SharedUtils/src/main/java/com/braintreepayments/api/HttpClientException.java
+++ SharedUtils/src/main/java/com/braintreepayments/api/HttpClientException.java
@@ -1,6 +1,5 @@
 package com.braintreepayments.api;
 
-// NEXT MAJOR VERSION: rename this class to HTTPClientException to matching acronym casing with other HTTP classes
 /**
  * Exception thrown when an HTTP request fails.
  */
