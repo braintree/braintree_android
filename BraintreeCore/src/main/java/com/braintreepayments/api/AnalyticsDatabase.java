b6679b91a964128e7a78ebd752fd66a89108b92c
Koop <skoop@paypal.com>
2021-08-31 09:45:24 -0500

    Fix lint error

--- BraintreeCore/src/main/java/com/braintreepayments/api/AnalyticsDatabase.java
+++ BraintreeCore/src/main/java/com/braintreepayments/api/AnalyticsDatabase.java
@@ -151,7 +151,10 @@ class AnalyticsDatabase extends SQLiteOpenHelper {
                         request.id = Integer.valueOf(ids[i]);
                         request.event = events[i];
                         request.timestamp = Long.valueOf(timestamps[i]);
-                        request.metadata = new JSONObject(cursor.getString(cursor.getColumnIndex(META_JSON)));
+                        int index = cursor.getColumnIndex(META_JSON);
+                        if (index >= 0) {
+                            request.metadata = new JSONObject(cursor.getString(index));
+                        }
                         innerList.add(request);
                     } catch (JSONException ignored) {}
                 }
