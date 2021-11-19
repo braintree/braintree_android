c8b08432da796123639e3e11132086bac50fe0fb
Koop <skoop@paypal.com>
2021-11-19 13:40:37 -0600

    Continue updating tests

--- GooglePay/src/test/java/com/braintreepayments/api/GoolgePayInternalClientUnitTestKT.kt
+++ GooglePay/src/test/java/com/braintreepayments/api/GoolgePayInternalClientUnitTestKT.kt
@@ -6,19 +6,17 @@ import com.google.android.gms.wallet.IsReadyToPayRequest
 import com.google.android.gms.wallet.PaymentsClient
 import com.google.android.gms.wallet.Wallet
 import com.google.android.gms.wallet.WalletConstants
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.mockkStatic
-import io.mockk.slot
-import junit.framework.TestCase.assertEquals
+import io.mockk.*
+import junit.framework.TestCase.*
 import org.junit.Before
 import org.junit.Test
+import java.util.concurrent.CountDownLatch
 
 class GoolgePayInternalClientUnitTestKT {
 
     private val activity = mockk<FragmentActivity>(relaxed = true)
     private val isReadyToPayCallback = mockk<GooglePayIsReadyToPayCallback>(relaxed = true)
-    private val paymentsClient = mockk<PaymentsClient>(relaxed = true)
+    private val paymentsClient = mockk<PaymentsClient>()
     private val isReadyToPayRequest = IsReadyToPayRequest.fromJson("{}")
 
     @Before
@@ -27,7 +25,7 @@ class GoolgePayInternalClientUnitTestKT {
     }
 
     @Test
-    fun `isReadyToPay requests Sandbox Wallet environment when configuration environment is Sandbox`() {
+    fun `isReadyToPay requests Test Wallet environment when configuration environment is Sandbox`() {
         val configuration = Configuration.fromJson(Fixtures.CONFIGURATION_WITH_GOOGLE_PAY)
         every { paymentsClient.isReadyToPay(isReadyToPayRequest) } returns Tasks.forResult(true)
 
@@ -38,5 +36,40 @@ class GoolgePayInternalClientUnitTestKT {
         sut.isReadyToPay(activity, configuration, isReadyToPayRequest, isReadyToPayCallback)
 
         assertEquals(WalletConstants.ENVIRONMENT_TEST, walletOptionsSlot.captured.environment)
+    }
+
+    @Test
+    fun `isReadyToPay requests Production Wallet environment when configuration environment is Production`() {
+        val configuration = Configuration.fromJson(Fixtures.CONFIGURATION_WITH_GOOGLE_PAY_PRODUCTION)
+        every { paymentsClient.isReadyToPay(isReadyToPayRequest) } returns Tasks.forResult(true)
+
+        val walletOptionsSlot = slot<Wallet.WalletOptions>()
+        every { Wallet.getPaymentsClient(any(), capture(walletOptionsSlot)) } returns paymentsClient
+
+        val sut = GooglePayInternalClient()
+        sut.isReadyToPay(activity, configuration, isReadyToPayRequest, isReadyToPayCallback)
+
+        assertEquals(WalletConstants.ENVIRONMENT_PRODUCTION, walletOptionsSlot.captured.environment)
+    }
+
+    @Test
+    fun `isReadyToPay forwards success result to callback`() {
+        val countDownLatch = CountDownLatch(1)
+        val configuration = Configuration.fromJson(Fixtures.CONFIGURATION_WITH_GOOGLE_PAY)
+        every { Wallet.getPaymentsClient(any(), any()) } returns paymentsClient
+
+        // TODO: determine why callback is not being fired here
+        coEvery { paymentsClient.isReadyToPay(isReadyToPayRequest) } coAnswers {
+            Tasks.forResult(true)
+        }
+
+        val sut = GooglePayInternalClient()
+        sut.isReadyToPay(activity, configuration, isReadyToPayRequest) { isReadyToPay, error ->
+            assertFalse(isReadyToPay)
+            assertNull(error)
+            countDownLatch.countDown()
+        }
+
+        countDownLatch.await()
     }
 }
