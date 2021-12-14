db302feacaa8d5e02ce3ff93f4b3263fd12d0eec
Steven <sshropshire@paypal.com>
2021-12-14 15:36:28 -0600

    Remove unecessary class.

--- BraintreeCore/src/main/java/com/braintreepayments/api/AppSwitchListener.java
+++
@@ -1,9 +0,0 @@
-package com.braintreepayments.api;
-
-import androidx.activity.result.ActivityResult;
-
-public interface AppSwitchListener {
-
-    void onActivityResult(ActivityResult activityResult);
-    boolean onBrowserSwitchResult(BrowserSwitchResult browserSwitchResult);
-}
