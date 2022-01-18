37fe1f0179ee812001ad474b37aef1663c88631a
sshropshire <58225613+sshropshire@users.noreply.github.com>
2022-01-18 13:16:56 -0600

    Update BraintreeCore/src/main/java/com/braintreepayments/api/AnalyticsBaseWorker.java
    Co-authored-by: Sarah Koop <skoop@paypal.com>

--- BraintreeCore/src/main/java/com/braintreepayments/api/AnalyticsBaseWorker.java
+++ BraintreeCore/src/main/java/com/braintreepayments/api/AnalyticsBaseWorker.java
@@ -14,7 +14,7 @@ import androidx.work.WorkerParameters;
  * This class is used internally by the SDK and should not be used directly.
  * It is not subject to semantic versioning and may change at any time.
  */
-// NEXT-MAJOR-VERSION: remove this class, it may no longer be needed
+// NEXT_MAJOR_VERSION: remove this class, it may no longer be needed
 public abstract class AnalyticsBaseWorker extends Worker {
 
     public AnalyticsBaseWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
