554b3bf9c7634720aa81fa45508e4dd0bf6546cf
Steven <sshropshire@paypal.com>
2022-01-21 10:58:20 -0600

    Add docstrings for ClientTokenProvider.

--- BraintreeCore/src/main/java/com/braintreepayments/api/ClientTokenProvider.java
+++ BraintreeCore/src/main/java/com/braintreepayments/api/ClientTokenProvider.java
@@ -2,6 +2,15 @@ package com.braintreepayments.api;
 
 import androidx.annotation.NonNull;
 
+/**
+ * Implement this interface to provide an asynchronous way for {@link BraintreeClient} to fetch
+ * a client token from your server when it is needed.
+ */
 public interface ClientTokenProvider {
+
+    /**
+     * Method used by {@link BraintreeClient} to fetch a client token.
+     * @param callback {@link ClientTokenCallback} to invoke to notify {@link BraintreeClient} of success (or failure) when fetching a client token
+     */
     void getClientToken(@NonNull ClientTokenCallback callback);
 }
