46478f114a27ffadb5e4c2ebedc7b9df4296a5c7
Steven <sshropshire@paypal.com>
2022-02-01 11:35:43 -0600

    Implement initial spec for ThreeDSecureAPI#authenticateCardinalJWT.

--- ThreeDSecure/src/main/java/com/braintreepayments/api/ThreeDSecureAPI.java
+++ ThreeDSecure/src/main/java/com/braintreepayments/api/ThreeDSecureAPI.java
@@ -1,6 +1,7 @@
 package com.braintreepayments.api;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 
 class ThreeDSecureAPI {
 
@@ -32,7 +33,43 @@ class ThreeDSecureAPI {
         });
     }
 
-    void authenticateCardinalJWT() {
+    void authenticateCardinalJWT(ThreeDSecureResult threeDSecureResult, String cardinalJWT, ThreeDSecureResultCallback callback) {
+        final CardNonce lookupCardNonce = threeDSecureResult.getTokenizedCard();
+        final String lookupNonce = lookupCardNonce.getString();
 
+        JSONObject body = new JSONObject();
+        try {
+            body.put("jwt", cardinalJWT);
+            body.put("paymentMethodNonce", lookupNonce);
+        } catch (JSONException ignored) {
+        }
+
+        String url = ApiClient.versionedPath(ApiClient.PAYMENT_METHOD_ENDPOINT + "/" + lookupNonce + "/three_d_secure/authenticate_from_jwt");
+        String data = body.toString();
+
+        braintreeClient.sendPOST(url, data, new HttpResponseCallback() {
+
+            @Override
+            public void onResult(String responseBody, Exception httpError) {
+//                if (responseBody != null) {
+//                    try {
+//                        ThreeDSecureResult result = ThreeDSecureResult.fromJson(responseBody);
+//                        if (result.hasError()) {
+//                            result.setTokenizedCard(lookupCardNonce);
+//                            braintreeClient.sendAnalyticsEvent("three-d-secure.verification-flow.upgrade-payment-method.failure.returned-lookup-nonce");
+//                        } else {
+//                            braintreeClient.sendAnalyticsEvent("three-d-secure.verification-flow.upgrade-payment-method.succeeded");
+//                        }
+//                        notify3DSComplete(result, callback);
+//
+//                    } catch (JSONException e) {
+//                        callback.onResult(null, e);
+//                    }
+//                } else {
+//                    braintreeClient.sendAnalyticsEvent("three-d-secure.verification-flow.upgrade-payment-method.errored");
+//                    callback.onResult(null, httpError);
+//                }
+            }
+        });
     }
 }
--- ThreeDSecure/src/test/java/com/braintreepayments/api/ThreeDSecureAPIUnitTest.java
+++ ThreeDSecure/src/test/java/com/braintreepayments/api/ThreeDSecureAPIUnitTest.java
@@ -11,20 +11,23 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.skyscreamer.jsonassert.JSONAssert;
 
 public class ThreeDSecureAPIUnitTest {
 
     private ThreeDSecureAPI sut;
 
     @Test
-    public void performLookup_sendsGraphQLPost() {
+    public void performLookup_sendsPOSTRequest() {
         BraintreeClient braintreeClient = new MockBraintreeClientBuilder().build();
         sut = new ThreeDSecureAPI(braintreeClient);
 
         ThreeDSecureRequest threeDSecureRequest = mock(ThreeDSecureRequest.class);
         String mockData = "{\"mock\":\"json\"}";
+        when(threeDSecureRequest.getNonce()).thenReturn("sample-nonce");
         when(threeDSecureRequest.build("cardinal-session-id")).thenReturn(mockData);
 
         ThreeDSecureResultCallback callback = mock(ThreeDSecureResultCallback.class);
@@ -35,7 +38,7 @@ public class ThreeDSecureAPIUnitTest {
         verify(braintreeClient).sendPOST(urlCaptor.capture(), dataCaptor.capture(), any(HttpResponseCallback.class));
 
         String url = urlCaptor.getValue();
-        assertEquals("/v1/payment_methods/null/three_d_secure/lookup", url);
+        assertEquals("/v1/payment_methods/sample-nonce/three_d_secure/lookup", url);
 
         String data = dataCaptor.getValue();
         assertSame(mockData, data);
@@ -52,7 +55,7 @@ public class ThreeDSecureAPIUnitTest {
         when(threeDSecureRequest.build(anyString())).thenReturn("{}");
 
         ThreeDSecureResultCallback callback = mock(ThreeDSecureResultCallback.class);
-        sut.performLookup(threeDSecureRequest, "cardinal-session-id", callback);
+        sut.performLookup(threeDSecureRequest, "another-session-id", callback);
 
         verify(callback).onResult(any(ThreeDSecureResult.class), (Exception) isNull());
     }
@@ -88,5 +91,31 @@ public class ThreeDSecureAPIUnitTest {
         sut.performLookup(threeDSecureRequest, "cardinal-session-id", callback);
 
         verify(callback).onResult((ThreeDSecureResult) isNull(), same(httpError));
+    }
+
+    @Test
+    public void authenticateCardinalJWT_sendsPOSTRequest() throws JSONException {
+        BraintreeClient braintreeClient = new MockBraintreeClientBuilder().build();
+        sut = new ThreeDSecureAPI(braintreeClient);
+
+        ThreeDSecureResult threeDSecureResult =
+            ThreeDSecureResult.fromJson(Fixtures.THREE_D_SECURE_LOOKUP_RESPONSE);
+        String cardinalJWT = "cardinal-jwt";
+
+        ThreeDSecureResultCallback callback = mock(ThreeDSecureResultCallback.class);
+        sut.authenticateCardinalJWT(threeDSecureResult, cardinalJWT, callback);
+
+        ArgumentCaptor<String> urlCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> dataCaptor = ArgumentCaptor.forClass(String.class);
+        verify(braintreeClient).sendPOST(urlCaptor.capture(), dataCaptor.capture(), any(HttpResponseCallback.class));
+
+        String url = urlCaptor.getValue();
+        assertEquals("/v1/payment_methods/123456-12345-12345-a-adfa/three_d_secure/authenticate_from_jwt", url);
+
+        String data = dataCaptor.getValue();
+        JSONObject expectedJSON = new JSONObject()
+                .put("jwt", "cardinal-jwt")
+                .put("paymentMethodNonce", "123456-12345-12345-a-adfa");
+        JSONAssert.assertEquals(expectedJSON, new JSONObject(data), true);
     }
 }
