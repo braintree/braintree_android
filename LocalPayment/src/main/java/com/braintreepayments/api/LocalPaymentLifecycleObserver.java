f166910376ca499a9e9d63a34b888a571d4a2e0b
Sarah Koop <skoop@paypal.com>
2022-02-14 16:08:44 -0600

    Implement LocalPaymentLifecylceObserver

--- LocalPayment/src/main/java/com/braintreepayments/api/LocalPaymentLifecycleObserver.java
+++ LocalPayment/src/main/java/com/braintreepayments/api/LocalPaymentLifecycleObserver.java
@@ -1,11 +1,16 @@
 package com.braintreepayments.api;
 
+import static com.braintreepayments.api.BraintreeRequestCodes.LOCAL_PAYMENT;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleEventObserver;
 import androidx.lifecycle.LifecycleOwner;
 
+// NEXT_MAJOR_VERSION: Update to implement DefaultLifeCycleObserver when Java 7 support is explicitly dropped.
 class LocalPaymentLifecycleObserver implements LifecycleEventObserver {
 
     @VisibleForTesting
@@ -16,9 +21,21 @@ class LocalPaymentLifecycleObserver implements LifecycleEventObserver {
     }
 
     @Override
-    public void onStateChanged(@NonNull LifecycleOwner source, @NonNull Lifecycle.Event event) {
+    public void onStateChanged(@NonNull LifecycleOwner lifecycleOwner, @NonNull Lifecycle.Event event) {
         if (event == Lifecycle.Event.ON_RESUME) {
-            // TODO: implement
+            FragmentActivity activity = null;
+            if (lifecycleOwner instanceof FragmentActivity) {
+                activity = (FragmentActivity) lifecycleOwner;
+            } else if (lifecycleOwner instanceof Fragment) {
+                activity = ((Fragment) lifecycleOwner).getActivity();
+            }
+
+            if (activity != null) {
+                BrowserSwitchResult pendingResult = localPaymentClient.getBrowserSwitchResult(activity);
+                if (pendingResult != null && pendingResult.getRequestCode() == LOCAL_PAYMENT) {
+                    localPaymentClient.onBrowserSwitchResult(activity);
+                }
+            }
         }
     }
 }
