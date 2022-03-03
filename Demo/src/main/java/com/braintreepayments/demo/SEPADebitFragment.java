57e04d62f2a19ce644a60881472707828a4eb3a8
Jax DesMarais-Leder <jdesmarais@paypal.com>
2022-03-03 14:35:23 -0600

    Add main fragment button to launch SEPADebitFragment
    Signed-off-by: Sarah Koop <skoop@paypal.com>

---
+++ Demo/src/main/java/com/braintreepayments/demo/SEPADebitFragment.java
@@ -0,0 +1,19 @@
+package com.braintreepayments.demo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SEPADebitFragment extends BaseFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_sepa_debit, container, false);
+        return view;
+    }
+}
