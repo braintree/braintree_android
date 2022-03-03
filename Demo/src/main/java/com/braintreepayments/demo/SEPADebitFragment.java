8bb2fb86b6489b734e51d2fbb23ce3096f07e053
Jax DesMarais-Leder <jdesmarais@paypal.com>
2022-03-03 14:43:29 -0600

    Add button to SEPADebitFragment
    Signed-off-by: Sarah Koop <skoop@paypal.com>

--- Demo/src/main/java/com/braintreepayments/demo/SEPADebitFragment.java
+++ Demo/src/main/java/com/braintreepayments/demo/SEPADebitFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class SEPADebitFragment extends BaseFragment {
 
@@ -14,6 +15,13 @@ public class SEPADebitFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_sepa_debit, container, false);
+        Button button = view.findViewById(R.id.sepa_debit_button);
+        button.setOnClickListener(this::launchSEPADebit);
+
         return view;
+    }
+
+    public void launchSEPADebit(View view) {
+
     }
 }
