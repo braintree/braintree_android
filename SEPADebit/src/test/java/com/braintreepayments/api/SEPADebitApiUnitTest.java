eb1a0e0f92b93160d2e1598f61c2e07a774be624
Jax DesMarais-Leder <jdesmarais@paypal.com>
2022-03-04 14:57:27 -0600

    add test for full request
    Co-authored-by: Sarah Koop <skoop@paypal.com>

--- SEPADebit/src/test/java/com/braintreepayments/api/SEPADebitApiUnitTest.java
+++ SEPADebit/src/test/java/com/braintreepayments/api/SEPADebitApiUnitTest.java
@@ -7,10 +7,13 @@ import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.mock;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 import org.robolectric.RobolectricTestRunner;
@@ -40,6 +43,30 @@ public class SEPADebitApiUnitTest {
 
         SEPADebitApi sut = new SEPADebitApi(httpClient);
 
+        SEPADebitRequest request = new SEPADebitRequest();
+        request.setAccountHolderName("John Doe");
+        request.setCustomerId("a-customer-id");
+        request.setIban("FR7618106000321234566666610");
+        request.setMandateType(SEPADebitMandateType.RECURRENT);
+        request.setMerchantAccountId("a_merchant_account_id");
+
+        sut.createMandate(request, null, createMandateCallback);
+
+        ArgumentCaptor<CreateMandateResult> captor = ArgumentCaptor.forClass(CreateMandateResult.class);
+        verify(createMandateCallback).onResult(captor.capture(), (Exception) isNull());
+
+        CreateMandateResult result = captor.getValue();
+        assertEquals("6610", result.getIbanLastFour());
+        assertEquals("https://api.test19.stage.paypal.com/directdebit/mandate/authorize?cart_id=1JH42426EL748934W\u0026auth_code=C21_A.AAdcUj4loKRxLtfw336KxbGY7dA7UsLJQTpZU3cE2h49eKkhN1OjFcLxxxzOGVzRiwOzGLlS_cS2BU4ZLKjMnR6lZSG2iQ", result.getApprovalUrl());
+        assertEquals("QkEtWDZDQkpCUU5TWENDVw", result.getBankReferenceToken());
+        assertEquals("a-customer-id", result.getCustomerId());
+        assertEquals(SEPADebitMandateType.RECURRENT, result.getMandateType());
+    }
+
+    @Test
+    public void createMandate_buildHttpRequest_withAllParams() throws JSONException {
+        SEPADebitApi sut = new SEPADebitApi(httpClient);
+
         PostalAddress billingAddress = new PostalAddress();
         billingAddress.setStreetAddress("Kantstraße 70");
         billingAddress.setExtendedAddress("#170");
@@ -54,18 +81,36 @@ public class SEPADebitApiUnitTest {
         request.setIban("FR7618106000321234566666610");
         request.setMandateType(SEPADebitMandateType.RECURRENT);
         request.setBillingAddress(billingAddress);
+        request.setMerchantAccountId("a_merchant_account_id");
 
         sut.createMandate(request, null, createMandateCallback);
 
-        ArgumentCaptor<CreateMandateResult> captor = ArgumentCaptor.forClass(CreateMandateResult.class);
-        verify(createMandateCallback).onResult(captor.capture(), (Exception) isNull());
+        ArgumentCaptor<HttpRequest> captor = ArgumentCaptor.forClass(HttpRequest.class);
+        verify(httpClient).sendRequest(captor.capture(), ArgumentMatchers.<HttpResponseCallback>any());
 
-        CreateMandateResult result = captor.getValue();
-        assertEquals("6610", result.getIbanLastFour());
-        assertEquals("https://api.test19.stage.paypal.com/directdebit/mandate/authorize?cart_id=1JH42426EL748934W\u0026auth_code=C21_A.AAdcUj4loKRxLtfw336KxbGY7dA7UsLJQTpZU3cE2h49eKkhN1OjFcLxxxzOGVzRiwOzGLlS_cS2BU4ZLKjMnR6lZSG2iQ", result.getApprovalUrl());
-        assertEquals("QkEtWDZDQkpCUU5TWENDVw", result.getBankReferenceToken());
-        assertEquals("a-customer-id", result.getCustomerId());
-        assertEquals(SEPADebitMandateType.RECURRENT, result.getMandateType());
+        HttpRequest result = captor.getValue();
+        assertEquals("POST", result.getMethod());
+        assertEquals("application/json", result.getHeaders().get("Content-Type"));
+        assertEquals("development_testing_pwpp_multi_account_merchant", result.getHeaders().get("Client-Key"));
+        assertEquals("merchants/pwpp_multi_account_merchant/client_api/v1/sepa_debit", result.getPath());
+
+        JSONObject data =new JSONObject(new String(result.getData()));
+        JSONObject sepaDebitData = data.getJSONObject("sepa_debit");
+        assertEquals("a_merchant_account_id", data.get("merchant_account_id"));
+
+        assertEquals("John Doe", sepaDebitData.get("account_holder_name"));
+        assertEquals("a-customer-id", sepaDebitData.get("customer_id"));
+        assertEquals("FR7618106000321234566666610", sepaDebitData.get("iban"));
+        assertEquals("RECURRENT", sepaDebitData.get("mandate_type"));
+
+        JSONObject billingAddressData = sepaDebitData.getJSONObject("billing_address");
+
+        assertEquals("Kantstraße 70", billingAddressData.get("address_line_1"));
+        assertEquals("#170", billingAddressData.get("address_line_2"));
+        assertEquals("Freistaat Sachsen", billingAddressData.get("admin_area_1"));
+        assertEquals("Annaberg-buchholz", billingAddressData.get("admin_area_2"));
+        assertEquals("09456", billingAddressData.get("postal_code"));
+        assertEquals("FR", billingAddressData.get("country_code"));
     }
 
     @Test
