07d66c3e3abc6826c7db4d70d2101e8b75a2a1ce
Sarah Koop <skoop@paypal.com>
2022-03-10 09:47:33 -0600

    Rename SEPA Debit to SEPA Direct Debit (#526)
    * Create SEPADebit module
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Add empty SEPADebitClient class for project structure
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Add SEPADebit module to release process
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * udpate AndroidManifest
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Update AndroidManifest
    
    * add CreateMandateRequest, SEPADebitRequest, CreateMandateResponse, SEPADebitMandateType
    
    Co-authored-by: Sarah Koop <skoop@paypal.com>
    
    * WIP - response
    
    Co-authored-by: Sarah Koop <skoop@paypal.com>
    
    * Implement SEPADebitNonce and unit test
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Spec SEPADebitClient methods
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Spec out SEPADebitLifecycleObserver
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Remove extra new line
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Remove unecessary param and clean up doc strings
    
    * Remove unecessary gitignore
    
    * update SEPADebitMandateType to enum
    
    Co-authored-by: Sarah Koop <skoop@paypal.com>
    
    * Add main fragment button to launch SEPADebitFragment
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Add button to SEPADebitFragment
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Handle SEPADebit results in demo app
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Remove extra string
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Remove unnecessary label
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Cleanup new lines
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Cleanup new line
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Create SEPADebitAPI with http request
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Add TODOs
    
    * Make http request from SEPA button
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Add CreateMandateResult and Callback
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Fix parsing typo
    
    * Implement success API unit test
    
    * pass in configuration and get merchant account ID value from merchant or config
    
    Co-authored-by: Sarah Koop <skoop@paypal.com>
    
    * add test for full request
    
    Co-authored-by: Sarah Koop <skoop@paypal.com>
    
    * add test for getting merchant_account_id from config
    
    Co-authored-by: Sarah Koop <skoop@paypal.com>
    
    * add error test cases
    
    Co-authored-by: Sarah Koop <skoop@paypal.com>
    
    * code cleanup
    
    * start work on browser switch
    
    Co-authored-by: Sarah Koop <skoop@paypal.com>
    
    * add sepa debit client test
    
    Co-authored-by: Sarah Koop <skoop@paypal.com>
    
    * Implement SEPADebitClient tokenize unit tests
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Implement constructors and unit test
    
    * Implement lifecycle observer and unit tests
    
    * Test out sample success URL
    
    * update return url
    
    Co-authored-by: Sarah Koop <skoop@paypal.com>
    
    * updates for browser switch result
    
    Co-authored-by: Sarah Koop <skoop@paypal.com>
    
    * Randomly generate IBAN and customer ID for testing
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Add browser switch metadata
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Add test for sets metadata
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Handle browser switch resulst
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Unit test remaining path
    
    Signed-off-by: Sarah Koop <skoop@paypal.com>
    
    * Cleanup tests
    
    * Update unit tests for SEPA_DEBIT request code
    
    * rename SEPADebit to SEPADirectDebit
    
    Co-authored-by: Sarah Koop <skoop@paypal.com>
    
    Co-authored-by: Jax DesMarais-Leder <jdesmarais@paypal.com>

---
+++ SEPADirectDebit/src/main/java/com/braintreepayments/api/SEPADirectDebitApi.java
@@ -0,0 +1,119 @@
+package com.braintreepayments.api;
+
+import androidx.annotation.VisibleForTesting;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.net.ssl.SSLException;
+import javax.net.ssl.SSLSocketFactory;
+
+class SEPADirectDebitApi {
+
+    // TODO: switch this to ApiClient when Sandbox is ready
+    private final HttpClient httpClient;
+
+    SEPADirectDebitApi() {
+        this(new HttpClient(getSocketFactory(), new BraintreeHttpResponseParser()));
+    }
+
+    @VisibleForTesting
+    SEPADirectDebitApi(HttpClient httpClient) {
+        this.httpClient = httpClient;
+    }
+
+    void createMandate(SEPADirectDebitRequest sepaDirectDebitRequest, Configuration configuration, String returnUrlScheme, final CreateMandateCallback callback) {
+        HttpRequest httpRequest;
+        try {
+            httpRequest = buildHttpRequest(sepaDirectDebitRequest, configuration, returnUrlScheme);
+            httpClient.sendRequest(httpRequest, new HttpResponseCallback() {
+                @Override
+                public void onResult(String responseBody, Exception httpError) {
+                    if (responseBody != null) {
+                        try {
+                            CreateMandateResult result = parseResponse(responseBody);
+                            callback.onResult(result, null);
+                        } catch (JSONException e) {
+                            callback.onResult(null, e);
+                        }
+                    } else if (httpError != null) {
+                        callback.onResult(null, httpError);
+                    }
+                }
+            });
+        } catch (JSONException e) {
+            callback.onResult(null, e);
+        }
+    }
+
+    void tokenize(String ibanLastFour, String customerId, String bankReferenceToken, String mandateType) {
+        // TODO: implement (future PR)
+    }
+
+    private CreateMandateResult parseResponse(String responseBody) throws JSONException {
+        JSONObject json = new JSONObject(responseBody);
+        JSONObject sepaDebitAccount = json.getJSONObject("message").getJSONObject("body").getJSONObject("sepaDebitAccount");
+        String approvalUrl = sepaDebitAccount.getString("approvalUrl");
+        String ibanLastFour = sepaDebitAccount.getString("ibanLastChars");
+        String customerId = sepaDebitAccount.getString("customerId");
+        String bankReferenceToken = sepaDebitAccount.getString("bankReferenceToken");
+        String mandateType = sepaDebitAccount.getString("mandateType");
+
+        return new CreateMandateResult(approvalUrl, ibanLastFour, customerId, bankReferenceToken, mandateType);
+    }
+
+    private HttpRequest buildHttpRequest(SEPADirectDebitRequest sepaDirectDebitRequest, Configuration configuration, String returnUrlScheme) throws JSONException {
+        JSONObject sepaDebitData = new JSONObject()
+            .putOpt("account_holder_name", sepaDirectDebitRequest.getAccountHolderName())
+            .putOpt("customer_id", sepaDirectDebitRequest.getCustomerId())
+            .putOpt("iban", sepaDirectDebitRequest.getIban());
+
+        if (sepaDirectDebitRequest.getMandateType() != null) {
+            sepaDebitData.putOpt("mandate_type", sepaDirectDebitRequest.getMandateType().toString());
+        }
+
+        if (sepaDirectDebitRequest.getBillingAddress() != null) {
+            JSONObject billingAddress = new JSONObject()
+                    .putOpt("address_line_1", sepaDirectDebitRequest.getBillingAddress().getStreetAddress())
+                    .putOpt("address_line_2", sepaDirectDebitRequest.getBillingAddress().getExtendedAddress())
+                    .putOpt("admin_area_1", sepaDirectDebitRequest.getBillingAddress().getLocality())
+                    .putOpt("admin_area_2", sepaDirectDebitRequest.getBillingAddress().getRegion())
+                    .putOpt("postal_code", sepaDirectDebitRequest.getBillingAddress().getPostalCode())
+                    .putOpt("country_code", sepaDirectDebitRequest.getBillingAddress().getCountryCodeAlpha2());
+
+            sepaDebitData.put("billing_address", billingAddress);
+        }
+
+        String cancelUrl = String.format("%s://sepa/cancel", returnUrlScheme);
+        String successUrl = String.format("%s://sepa/success", returnUrlScheme);
+
+        JSONObject requestData = new JSONObject()
+                .put("sepa_debit", sepaDebitData)
+                .put("cancel_url", cancelUrl)
+                .put("return_url", successUrl);
+
+        if (sepaDirectDebitRequest.getMerchantAccountId() != null) {
+            requestData.put("merchant_account_id", sepaDirectDebitRequest.getMerchantAccountId());
+        } else {
+            requestData.putOpt("merchant_account_id", configuration.getMerchantAccountId());
+        }
+
+        HttpRequest request = new HttpRequest()
+                .baseUrl("http://10.0.2.2:3000/")
+                .method("POST")
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Client-Key", "development_testing_pwpp_multi_account_merchant")
+                .path("merchants/pwpp_multi_account_merchant/client_api/v1/sepa_debit")
+                .data(requestData.toString());
+        return request;
+    }
+
+    // TODO: Remove this when feature is in sandbox and ApiClient can be used
+    private static SSLSocketFactory getSocketFactory() {
+        try {
+            return new TLSSocketFactory(BraintreeGatewayCertificate.getCertInputStream());
+        } catch (SSLException e) {
+            return null;
+        }
+    }
+}
---
+++ SEPADirectDebit/src/test/java/com/braintreepayments/api/SEPADirectDebitApiUnitTest.java
@@ -0,0 +1,182 @@
+package com.braintreepayments.api;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mock;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class SEPADirectDebitApiUnitTest {
+
+    private HttpClient httpClient;
+    private CreateMandateCallback createMandateCallback;
+    private SEPADirectDebitRequest request;
+    private PostalAddress billingAddress;
+    private String returnUrl;
+
+    @Before
+    public void beforeEach() {
+        httpClient = mock(HttpClient.class);
+        createMandateCallback = mock(CreateMandateCallback.class);
+
+        request = new SEPADirectDebitRequest();
+        request.setAccountHolderName("John Doe");
+        request.setCustomerId("a-customer-id");
+        request.setIban("FR7618106000321234566666610");
+        request.setMandateType(SEPADirectDebitMandateType.RECURRENT);
+        request.setMerchantAccountId("a_merchant_account_id");
+
+        billingAddress = new PostalAddress();
+        billingAddress.setStreetAddress("Kantstraße 70");
+        billingAddress.setExtendedAddress("#170");
+        billingAddress.setLocality("Freistaat Sachsen");
+        billingAddress.setRegion("Annaberg-buchholz");
+        billingAddress.setPostalCode("09456");
+        billingAddress.setCountryCodeAlpha2("FR");
+
+        returnUrl = "com.example";
+    }
+
+    @Test
+    public void createMandate_onSuccessfulHttpResponse_callsBackCreateMandateResult() {
+        doAnswer(new Answer<Void>() {
+            @Override
+            public Void answer(InvocationOnMock invocation) {
+                HttpResponseCallback callback = (HttpResponseCallback) invocation.getArguments()[1];
+                callback.onResult(Fixtures.SEPA_DEBIT_CREATE_MANDATE_RESPONSE, null);
+                return null;
+            }
+        }).when(httpClient).sendRequest(any(HttpRequest.class), any(HttpResponseCallback.class));
+
+        SEPADirectDebitApi sut = new SEPADirectDebitApi(httpClient);
+
+        sut.createMandate(request, null, returnUrl, createMandateCallback);
+
+        ArgumentCaptor<CreateMandateResult> captor = ArgumentCaptor.forClass(CreateMandateResult.class);
+        verify(createMandateCallback).onResult(captor.capture(), (Exception) isNull());
+
+        CreateMandateResult result = captor.getValue();
+        assertEquals("6610", result.getIbanLastFour());
+        assertEquals("https://api.test19.stage.paypal.com/directdebit/mandate/authorize?cart_id=1JH42426EL748934W\u0026auth_code=C21_A.AAdcUj4loKRxLtfw336KxbGY7dA7UsLJQTpZU3cE2h49eKkhN1OjFcLxxxzOGVzRiwOzGLlS_cS2BU4ZLKjMnR6lZSG2iQ", result.getApprovalUrl());
+        assertEquals("QkEtWDZDQkpCUU5TWENDVw", result.getBankReferenceToken());
+        assertEquals("a-customer-id", result.getCustomerId());
+        assertEquals(SEPADirectDebitMandateType.RECURRENT, result.getMandateType());
+    }
+
+    @Test
+    public void createMandate_buildHttpRequest_withAllParams() throws JSONException {
+        request.setBillingAddress(billingAddress);
+
+        SEPADirectDebitApi sut = new SEPADirectDebitApi(httpClient);
+        sut.createMandate(request, null, returnUrl, createMandateCallback);
+
+        ArgumentCaptor<HttpRequest> captor = ArgumentCaptor.forClass(HttpRequest.class);
+        verify(httpClient).sendRequest(captor.capture(), ArgumentMatchers.<HttpResponseCallback>any());
+
+        HttpRequest httpRequest = captor.getValue();
+        assertEquals("POST", httpRequest.getMethod());
+        assertEquals("application/json", httpRequest.getHeaders().get("Content-Type"));
+        assertEquals("development_testing_pwpp_multi_account_merchant", httpRequest.getHeaders().get("Client-Key"));
+        assertEquals("merchants/pwpp_multi_account_merchant/client_api/v1/sepa_debit", httpRequest.getPath());
+
+        JSONObject data =new JSONObject(new String(httpRequest.getData()));
+        JSONObject sepaDebitData = data.getJSONObject("sepa_debit");
+        assertEquals("a_merchant_account_id", data.get("merchant_account_id"));
+
+        assertEquals("John Doe", sepaDebitData.get("account_holder_name"));
+        assertEquals("a-customer-id", sepaDebitData.get("customer_id"));
+        assertEquals("FR7618106000321234566666610", sepaDebitData.get("iban"));
+        assertEquals("RECURRENT", sepaDebitData.get("mandate_type"));
+
+        JSONObject billingAddressData = sepaDebitData.getJSONObject("billing_address");
+
+        assertEquals("Kantstraße 70", billingAddressData.get("address_line_1"));
+        assertEquals("#170", billingAddressData.get("address_line_2"));
+        assertEquals("Freistaat Sachsen", billingAddressData.get("admin_area_1"));
+        assertEquals("Annaberg-buchholz", billingAddressData.get("admin_area_2"));
+        assertEquals("09456", billingAddressData.get("postal_code"));
+        assertEquals("FR", billingAddressData.get("country_code"));
+    }
+
+    @Test
+    public void createMandate_whenMerchantIdNotSetOnRequest_usesConfiguration() throws JSONException {
+        SEPADirectDebitRequest request = new SEPADirectDebitRequest();
+
+        Configuration mockConfiguration = mock(Configuration.class);
+        when(mockConfiguration.getMerchantAccountId()).thenReturn("a_merchant_account_id");
+
+        SEPADirectDebitApi sut = new SEPADirectDebitApi(httpClient);
+        sut.createMandate(request, mockConfiguration, returnUrl, createMandateCallback);
+
+        ArgumentCaptor<HttpRequest> captor = ArgumentCaptor.forClass(HttpRequest.class);
+        verify(httpClient).sendRequest(captor.capture(), ArgumentMatchers.<HttpResponseCallback>any());
+
+        HttpRequest httpRequest = captor.getValue();
+        JSONObject data =new JSONObject(new String(httpRequest.getData()));
+        assertEquals("a_merchant_account_id", data.get("merchant_account_id"));
+    }
+
+    @Test
+    public void createMandate_onInvalidResponseJSON_callsBackError() {
+        doAnswer(new Answer<Void>() {
+            @Override
+            public Void answer(InvocationOnMock invocation) {
+                HttpResponseCallback callback = (HttpResponseCallback) invocation.getArguments()[1];
+                callback.onResult("not-json", null);
+                return null;
+            }
+        }).when(httpClient).sendRequest(any(HttpRequest.class), any(HttpResponseCallback.class));
+
+        Configuration mockConfiguration = mock(Configuration.class);
+        when(mockConfiguration.getMerchantAccountId()).thenReturn("a_merchant_account_id");
+
+        SEPADirectDebitApi sut = new SEPADirectDebitApi(httpClient);
+        sut.createMandate(request, mockConfiguration, returnUrl, createMandateCallback);
+
+        ArgumentCaptor<Exception> captor = ArgumentCaptor.forClass(Exception.class);
+        verify(createMandateCallback).onResult((CreateMandateResult) isNull(), captor.capture());
+        Exception error = captor.getValue();
+
+        assertNotNull(error);
+        assertTrue(error instanceof JSONException);
+    }
+
+    @Test
+    public void createMandate_onHttpResponseError_callsBackError() {
+        final Exception exception = new Exception("http-error");
+
+        doAnswer(new Answer<Void>() {
+            @Override
+            public Void answer(InvocationOnMock invocation) {
+                HttpResponseCallback callback = (HttpResponseCallback) invocation.getArguments()[1];
+                callback.onResult(null, exception);
+                return null;
+            }
+        }).when(httpClient).sendRequest(any(HttpRequest.class), any(HttpResponseCallback.class));
+
+        Configuration mockConfiguration = mock(Configuration.class);
+        when(mockConfiguration.getMerchantAccountId()).thenReturn("a_merchant_account_id");
+
+        SEPADirectDebitApi sut = new SEPADirectDebitApi(httpClient);
+        sut.createMandate(request, mockConfiguration, returnUrl, createMandateCallback);
+
+        verify(createMandateCallback).onResult((CreateMandateResult) isNull(), same(exception));
+    }
+}
