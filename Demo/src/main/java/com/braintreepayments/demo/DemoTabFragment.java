e3250d9cc13a1c8c07323dfe72f77b8fc0496aa5
sshropshire <sshropshire@paypal.com>
2022-04-22 16:21:13 -0500

    Update DemoTab fragment to use action bar.

--- Demo/src/main/java/com/braintreepayments/demo/DemoTabFragment.java
+++ Demo/src/main/java/com/braintreepayments/demo/DemoTabFragment.java
@@ -6,7 +6,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class DemoTabFragment extends Fragment {
 
@@ -17,12 +24,26 @@ public class DemoTabFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_demo_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_demo_tab, container, false);
+
+        // setup action bar
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        NavController navController = getNavController();
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
+        return view;
     }
 
     @Override
     public void onResume() {
         super.onResume();
         actionBarController.updateTitle(this);
+    }
+
+    private NavController getNavController() {
+        Fragment navHostFragment =
+            getChildFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        return ((NavHostFragment) navHostFragment).getNavController();
     }
 }
