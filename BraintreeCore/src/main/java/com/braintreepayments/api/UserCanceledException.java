cf150ba2eb275d51ba15f9587ea3f65f3a42000a
Sarah Koop <skoop@paypal.com>
2022-05-03 16:34:48 -0500

    Add isExplicitCancellation param to UserCanceledException
    Signed-off-by: Matt Wylder <mwylder@paypal.com>

--- BraintreeCore/src/main/java/com/braintreepayments/api/UserCanceledException.java
+++ BraintreeCore/src/main/java/com/braintreepayments/api/UserCanceledException.java
@@ -5,7 +5,27 @@ package com.braintreepayments.api;
  */
 public class UserCanceledException extends BraintreeException {
 
+    private boolean isExplicitCancellation;
+
     UserCanceledException(String message) {
         super(message);
+    }
+
+    UserCanceledException(String message, boolean isExplicitCancellation) {
+        super(message);
+        this.isExplicitCancellation = isExplicitCancellation;
+    }
+
+    /**
+     * @return whether or not the user explicitly canceled the payment flow.
+     *
+     * {@link true} if the user confirms cancellation of the payment flow.
+     *
+     * {@link false} if the user returns to the app without completing the payment flow and without
+     * performing an explicit cancellation action (i.e. presses the back button or closes the
+     * browser tab)
+     */
+    public boolean isExplicitCancellation() {
+        return isExplicitCancellation;
     }
 }
