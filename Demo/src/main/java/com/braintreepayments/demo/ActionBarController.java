c0ec73303c64407e2f47a2887992591dba9c0051
sshropshire <sshropshire@paypal.com>
2022-05-04 14:33:18 -0500

    Cleanup unused code.

--- Demo/src/main/java/com/braintreepayments/demo/ActionBarController.java
+++ Demo/src/main/java/com/braintreepayments/demo/ActionBarController.java
@@ -1,9 +1,5 @@
 package com.braintreepayments.demo;
 
-import android.view.View;
-
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
@@ -18,18 +14,5 @@ public class ActionBarController {
             String authType = Settings.getAuthorizationType(activity);
             toolbar.setTitle(String.format("%s / %s", environment, authType));
         }
-//        FragmentActivity activity = fragment.getActivity();
-//        if (activity instanceof AppCompatActivity) {
-//            updateTitle((AppCompatActivity) activity);
-//        }
-    }
-
-    public void updateTitle(AppCompatActivity activity) {
-//        ActionBar actionBar = activity.getSupportActionBar();
-//        if (actionBar != null) {
-//            String environment = Settings.getEnvironment(activity);
-//            String authType = Settings.getAuthorizationType(activity);
-//            actionBar.setTitle(String.format("%s / %s", environment, authType));
-//        }
     }
 }
