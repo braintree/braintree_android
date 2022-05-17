b8427ceb95cc927a437cc9e897865e27f85172a9
Steven Shropshire <steven.shropshire@getbraintree.com>
2022-05-17 15:40:37 -0500

    Add PayPalNativeCheckoutNonce and PayPalNativeCheckoutRequest.
    Signed-off-by: Sarah Koop <skoop@paypal.com>

--- PayPalNativeCheckout/src/main/java/com/braintreepayments/api/PayPalNativeCheckoutClient.java
+++ PayPalNativeCheckout/src/main/java/com/braintreepayments/api/PayPalNativeCheckoutClient.java
@@ -1,4 +1,14 @@
 package com.braintreepayments.api;
 
 public class PayPalNativeCheckoutClient {
+
+    private PayPalNativeCheckoutListener listener;
+
+    public void tokenize(PayPalNativeCheckoutRequest request) {
+        // TODO: start native checkout
+    }
+
+     public void setListener(PayPalNativeCheckoutListener listener) {
+        this.listener = listener;
+     }
 }
