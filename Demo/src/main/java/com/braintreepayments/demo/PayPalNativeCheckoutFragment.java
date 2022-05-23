c30ef91287580eb42335c3ae3723365357d0fe85
Matt Wylder <mwylder@paypal.com>
2022-05-23 11:12:58 -0500

    renamed pay_pal to paypal
    Signed-off-by: Jax DesMarais-Leder <jdesmarais@paypal.com>

--- Demo/src/main/java/com/braintreepayments/demo/PayPalNativeCheckoutFragment.java
+++ Demo/src/main/java/com/braintreepayments/demo/PayPalNativeCheckoutFragment.java
@@ -21,7 +21,7 @@ public class PayPalNativeCheckoutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_pay_pal_native_checkout, container, false);
+        View view = inflater.inflate(R.layout.fragment_paypal_native_checkout, container, false);
         launchPayPalNativeCheckoutButton = view.findViewById(R.id.paypal_native_checkout_launch);
         launchPayPalNativeCheckoutButton.setOnClickListener(this::launchPayPalNativeCheckout);
 
