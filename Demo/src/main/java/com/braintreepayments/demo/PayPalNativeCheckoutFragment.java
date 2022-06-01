91b744ac331d9a33a30cc2e8191afcb24e8b0c4f
Kelay <kelay@paypal.com>
2022-06-01 13:53:08 -0700

    -Adding in activity

--- Demo/src/main/java/com/braintreepayments/demo/PayPalNativeCheckoutFragment.java
+++ Demo/src/main/java/com/braintreepayments/demo/PayPalNativeCheckoutFragment.java
@@ -1,36 +1,105 @@
 package com.braintreepayments.demo;
 
+import static com.braintreepayments.demo.PayPalRequestFactory.createPayPalCheckoutRequest;
+import static com.braintreepayments.demo.PayPalRequestFactory.createPayPalVaultRequest;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.braintreepayments.api.PayPalNativeCheckoutClient;
+import com.braintreepayments.api.BraintreeClient;
+import com.braintreepayments.api.DataCollector;
+import com.braintreepayments.api.PayPalAccountNonce;
+import com.braintreepayments.api.PayPalListener;
 import com.braintreepayments.api.PayPalNativeCheckoutRequest;
+import com.braintreepayments.api.PayPalNativeClient;
+import com.braintreepayments.api.PayPalRequest;
+import com.braintreepayments.api.PaymentMethodNonce;
 
-public class PayPalNativeCheckoutFragment extends Fragment {
+public class PayPalNativeCheckoutFragment extends BaseFragment implements PayPalListener {
+
+    private String deviceData;
+    private BraintreeClient braintreeClient;
+    private PayPalNativeClient payPalClient;
+    private DataCollector dataCollector;
+
     public Button launchPayPalNativeCheckoutButton;
+
     public PayPalNativeCheckoutFragment() {
 
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(
+            LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState
+    ) {
         View view = inflater.inflate(R.layout.fragment_paypal_native_checkout, container, false);
-        launchPayPalNativeCheckoutButton = view.findViewById(R.id.paypal_native_checkout_launch);
-        launchPayPalNativeCheckoutButton.setOnClickListener(this::launchPayPalNativeCheckout);
 
+        launchPayPalNativeCheckoutButton = view.findViewById(R.id.paypal_native_checkout_launch);
+        launchPayPalNativeCheckoutButton.setOnClickListener((View.OnClickListener) v -> launchPayPalNativeCheckout(false));
+        braintreeClient = getBraintreeClient();
+        payPalClient = new PayPalNativeClient(this, braintreeClient);
+        payPalClient.setListener(this);
         return view;
     }
 
-    private void launchPayPalNativeCheckout(View v) {
-        PayPalNativeCheckoutClient client = new PayPalNativeCheckoutClient();
-        PayPalNativeCheckoutRequest request = new PayPalNativeCheckoutRequest("100");
-        client.tokenize(request);
+    private void launchPayPalNativeCheckout(boolean isBillingAgreement) {
+        FragmentActivity activity = getActivity();
+        activity.setProgressBarIndeterminateVisibility(true);
+
+        dataCollector = new DataCollector(braintreeClient);
+
+        braintreeClient.getConfiguration((configuration, configError) -> {
+            if (Settings.shouldCollectDeviceData(requireActivity())) {
+                dataCollector.collectDeviceData(requireActivity(), (deviceDataResult, error) -> {
+                    if (deviceDataResult != null) {
+                        deviceData = deviceDataResult;
+                    }
+                    if (isBillingAgreement) {
+                        payPalClient.tokenizePayPalAccount(activity, createPayPalVaultRequest(activity));
+                    } else {
+                        payPalClient.tokenizePayPalAccount(activity, createPayPalCheckoutRequest(activity, "1.00"));
+                    }
+                });
+            } else {
+                if (isBillingAgreement) {
+                    payPalClient.tokenizePayPalAccount(activity, createPayPalVaultRequest(activity));
+                } else {
+                    payPalClient.tokenizePayPalAccount(activity, createPayPalCheckoutRequest(activity, "1.00"));
+                }
+            }
+        });
+    }
+
+    private void handlePayPalResult(PaymentMethodNonce paymentMethodNonce) {
+        if (paymentMethodNonce != null) {
+            super.onPaymentMethodNonceCreated(paymentMethodNonce);
+
+            PayPalNativeCheckoutFragmentDirections.ActionPayPalNativeCheckoutFragmentToDisplayNonceFragment action =
+                    PayPalNativeCheckoutFragmentDirections.actionPayPalNativeCheckoutFragmentToDisplayNonceFragment(paymentMethodNonce);
+            action.setDeviceData(deviceData);
+
+            NavHostFragment.findNavController(this).navigate(action);
+        }
+    }
+
+    @Override
+    public void onPayPalSuccess(@NonNull PayPalAccountNonce payPalAccountNonce) {
+        handlePayPalResult(payPalAccountNonce);
+    }
+
+    @Override
+    public void onPayPalFailure(@NonNull Exception error) {
+        handleError(error);
     }
 }
