eb71a7c1d51fc7a7053ff9bcdd2b1df4f7dc22b3
Jax DesMarais-Leder <jdesmarais@paypal.com>
2022-07-13 08:32:15 -0500

    remove unused import

--- TestUtils/src/main/java/com/braintreepayments/api/MockBraintreeClientBuilder.java
+++ TestUtils/src/main/java/com/braintreepayments/api/MockBraintreeClientBuilder.java
@@ -11,7 +11,6 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
