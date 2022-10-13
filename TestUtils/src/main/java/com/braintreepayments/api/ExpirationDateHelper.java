9730babed28a3e6f9c9924f3a9a6045288d43683
sshropshire <sshropshire@paypal.com>
2022-10-13 15:19:39 -0500

    Update expiration date helper to pick a year further into the future.

--- TestUtils/src/main/java/com/braintreepayments/api/ExpirationDateHelper.java
+++ TestUtils/src/main/java/com/braintreepayments/api/ExpirationDateHelper.java
@@ -5,6 +5,6 @@ import java.util.Calendar;
 public class ExpirationDateHelper {
 
     public static String validExpirationYear() {
-        return String.valueOf(Calendar.getInstance().get(Calendar.YEAR) + 1);
+        return String.valueOf(Calendar.getInstance().get(Calendar.YEAR) + 3);
     }
 }
