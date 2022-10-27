a7698599fb45ba928f33d41b20fc9c93ae773da6
sshropshire <sshropshire@paypal.com>
2022-10-27 15:23:48 -0500

    Update VenmoSharedPrefs unit test.

--- Venmo/src/test/java/com/braintreepayments/api/VenmoSharedPrefsWriterUnitTest.java
+++ Venmo/src/test/java/com/braintreepayments/api/VenmoSharedPrefsWriterUnitTest.java
@@ -3,33 +3,29 @@ package com.braintreepayments.api;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-import android.content.Context;
-
 import org.junit.Before;
 import org.junit.Test;
 
 public class VenmoSharedPrefsWriterUnitTest {
 
-    private Context context;
     private BraintreeSharedPreferences braintreeSharedPreferences;
 
     @Before
     public void beforeEach() {
-        context = mock(Context.class);
         braintreeSharedPreferences = mock(BraintreeSharedPreferences.class);
     }
 
     @Test
     public void persistVenmoVaultOption_persistsVaultOption() throws UnexpectedException {
-        VenmoSharedPrefsWriter sut = new VenmoSharedPrefsWriter(braintreeSharedPreferences);
-        sut.persistVenmoVaultOption(context, true);
+        VenmoSharedPrefsWriter sut = new VenmoSharedPrefsWriter();
+        sut.persistVenmoVaultOption(braintreeSharedPreferences, true);
         verify(braintreeSharedPreferences).putBoolean("com.braintreepayments.api.Venmo.VAULT_VENMO_KEY", true);
     }
 
     @Test
     public void getVenmoVaultOption_retrievesVaultOptionFromSharedPrefs() throws UnexpectedException {
-        VenmoSharedPrefsWriter sut = new VenmoSharedPrefsWriter(braintreeSharedPreferences);
-        sut.getVenmoVaultOption(context);
+        VenmoSharedPrefsWriter sut = new VenmoSharedPrefsWriter();
+        sut.getVenmoVaultOption(braintreeSharedPreferences);
         verify(braintreeSharedPreferences).getBoolean("com.braintreepayments.api.Venmo.VAULT_VENMO_KEY");
     }
 }
