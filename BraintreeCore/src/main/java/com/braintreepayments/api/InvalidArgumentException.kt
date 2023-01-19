aa5d025f2009d29005cf4a8c7091fd7b803f5d4e
sshropshire <sshropshire@paypal.com>
2023-01-19 16:57:56 -0600

    Update InvalidArgumentException to keep constructor module public.

--- BraintreeCore/src/main/java/com/braintreepayments/api/InvalidArgumentException.kt
+++ BraintreeCore/src/main/java/com/braintreepayments/api/InvalidArgumentException.kt
@@ -1,9 +1,17 @@
 package com.braintreepayments.api
 
+import androidx.annotation.RestrictTo
 import java.lang.Exception
 
 /**
  * Error thrown when arguments provided to a method are invalid.
- * @param message the error message
  */
-class InvalidArgumentException internal constructor(message: String?) : Exception(message)
+open class InvalidArgumentException : Exception {
+
+    /**
+     * Ref: https://github.com/Kotlin/dokka/issues/1953
+     * @param message the error message
+     * @suppress
+     */
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP) constructor(message: String?) : super(message)
+}
