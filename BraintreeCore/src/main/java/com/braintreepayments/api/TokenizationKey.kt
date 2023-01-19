1308f629d1468944b85b96eb7f4a417b5166de72
Jose Noriega <jonoriega@paypal.com>
2023-01-19 13:43:54 -0300

    Remove private enum

--- BraintreeCore/src/main/java/com/braintreepayments/api/TokenizationKey.kt
+++ BraintreeCore/src/main/java/com/braintreepayments/api/TokenizationKey.kt
@@ -14,37 +14,24 @@ internal class TokenizationKey(tokenizationKey: String) : Authorization(tokeniza
         val tokenizationKeyParts = tokenizationKey.split("_", limit = 3)
         environment = tokenizationKeyParts[0]
         merchantId = tokenizationKeyParts[2]
-        url = BraintreeEnvironment.getUrl(environment) + "merchants/" +
+        url = getUrl(environment) + "merchants/" +
                 merchantId + "/client_api/"
         configUrl = url + CONFIG_V1
     }
 
     companion object {
         const val MATCHER = "^[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9_]+$"
+        private const val DEVELOPMENT = "development"
+        private const val SANDBOX = "sandbox"
+        private const val PRODUCTION = "production"
         private const val CONFIG_V1 = "v1/configuration"
-    }
 
-    private enum class BraintreeEnvironment(
-        private val mEnvironment: String,
-        private val mUrl: String
-    ) {
-        DEVELOPMENT("development", BuildConfig.DEVELOPMENT_URL),
-        SANDBOX(
-            "sandbox",
-            "https://api.sandbox.braintreegateway.com/"
-        ),
-        PRODUCTION("production", "https://api.braintreegateway.com/");
-
-        companion object {
-            @Throws(InvalidArgumentException::class)
-            fun getUrl(environment: String): String {
-                for (braintreeEnvironment in values()) {
-                    if (braintreeEnvironment.mEnvironment == environment) {
-                        return braintreeEnvironment.mUrl
-                    }
-                }
-                throw InvalidArgumentException("Tokenization Key contained invalid environment")
-            }
+        @Throws(InvalidArgumentException::class)
+        private fun getUrl(environment: String) = when (environment) {
+            DEVELOPMENT -> BuildConfig.DEVELOPMENT_URL
+            SANDBOX -> "https://api.sandbox.braintreegateway.com/"
+            PRODUCTION -> "https://api.braintreegateway.com/"
+            else -> throw InvalidArgumentException("Tokenization Key contained invalid environment")
         }
     }
 }
