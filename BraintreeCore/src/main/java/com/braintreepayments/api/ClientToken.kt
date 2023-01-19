a31598178aeafefa1f57f48962cb1076a682c017
Jose Noriega <jonoriega@paypal.com>
2023-01-19 12:51:56 -0300

    Move documentation and init in ClientToken

--- BraintreeCore/src/main/java/com/braintreepayments/api/ClientToken.kt
+++ BraintreeCore/src/main/java/com/braintreepayments/api/ClientToken.kt
@@ -9,6 +9,8 @@ import kotlin.jvm.Throws
 /**
  * A class containing the configuration url and authorization for the current Braintree environment.
  * @param clientTokenString A client token from the Braintree Gateway
+ * @property authorizationFingerprint The authorizationFingerprint for the current session
+ * @property customerId The customer ID in the authorizationFingerprint if it is present
  * @constructor Create a new [ClientToken] instance from a client token
  * @throws InvalidArgumentException when client token is invalid
  */
@@ -16,18 +18,33 @@ internal class ClientToken @Throws(InvalidArgumentException::class) constructor(
     clientTokenString: String?
 ) : Authorization(clientTokenString) {
 
-    private var configUrl: String? = null
+    private var configUrl: String?
 
-    /**
-     * @return The authorizationFingerprint for the current session
-     */
-    var authorizationFingerprint: String? = null
+    val authorizationFingerprint: String?
 
-    /**
-     * @return The customer ID in the authorizationFingerprint if it is present
-     */
     val customerId: String?
-        get() {
+
+    init {
+        try {
+            val clientTokenStringDecoded = String(Base64.decode(clientTokenString, Base64.DEFAULT))
+            val jsonObject = JSONObject(clientTokenStringDecoded)
+            configUrl = jsonObject.getString(CONFIG_URL_KEY)
+            authorizationFingerprint = jsonObject.getString(AUTHORIZATION_FINGERPRINT_KEY)
+            customerId = parseCustomerId(authorizationFingerprint)
+        } catch (e: NullPointerException) {
+            throw InvalidArgumentException("Client token was invalid")
+        } catch (e: JSONException) {
+            throw InvalidArgumentException("Client token was invalid")
+        }
+    }
+
+    companion object {
+        const val BASE_64_MATCHER =
+            "([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)"
+        private const val CONFIG_URL_KEY = "configUrl"
+        private const val AUTHORIZATION_FINGERPRINT_KEY = "authorizationFingerprint"
+
+        private fun parseCustomerId(authorizationFingerprint: String?): String? {
             val result = authorizationFingerprint?.let { fingerPrint ->
                 val components = fingerPrint.split("&")
                 for (component in components) {
@@ -42,25 +59,6 @@ internal class ClientToken @Throws(InvalidArgumentException::class) constructor(
             }
             return result
         }
-
-    init {
-        try {
-            val clientTokenStringDecoded = String(Base64.decode(clientTokenString, Base64.DEFAULT))
-            val jsonObject = JSONObject(clientTokenStringDecoded)
-            configUrl = jsonObject.getString(CONFIG_URL_KEY)
-            authorizationFingerprint = jsonObject.getString(AUTHORIZATION_FINGERPRINT_KEY)
-        } catch (e: NullPointerException) {
-            throw InvalidArgumentException("Client token was invalid")
-        } catch (e: JSONException) {
-            throw InvalidArgumentException("Client token was invalid")
-        }
-    }
-
-    companion object {
-        const val BASE_64_MATCHER =
-            "([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)"
-        private const val CONFIG_URL_KEY = "configUrl"
-        private const val AUTHORIZATION_FINGERPRINT_KEY = "authorizationFingerprint"
     }
 
     public override fun getConfigUrl(): String {
