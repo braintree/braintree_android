161349e1baa33962acf90d52cafbb8daacfe2041
sshropshire <sshropshire@paypal.com>
2023-02-01 11:34:23 -0600

    Refactor UserCanceledException to be more Kotlin like.

--- BraintreeCore/src/main/java/com/braintreepayments/api/UserCanceledException.kt
+++ BraintreeCore/src/main/java/com/braintreepayments/api/UserCanceledException.kt
@@ -2,24 +2,17 @@ package com.braintreepayments.api
 
 /**
  * Error class thrown when a user cancels a payment flow
+ *
+ * @property isExplicitCancelation whether or not the user explicitly canceled the payment flow.
+ *
+ * This value will be true if the user manually confirms cancellation of the payment flow.
+ *
+ * This value will be false if the user returns to the app without completing the payment flow
+ * and the action performed to return to the app is unknown. For browser switching flows, this
+ * could mean the user returned to the app through multi-tasking without completing the flow,
+ * the user closed the browser tab, or the user pressed the back button.
  */
-open class UserCanceledException : BraintreeException {
-
-    internal constructor(message: String?) : super(message)
-    internal constructor(message: String?, isExplicitCancelation: Boolean) : super(message) {
-        this.isExplicitCancelation = isExplicitCancelation
-    }
-
-    /**
-     * @return whether or not the user explicitly canceled the payment flow.
-     *
-     * This value will be true if the user manually confirms cancellation of the payment flow.
-     *
-     * This value will be false if the user returns to the app without completing the payment flow
-     * and the action performed to return to the app is unknown. For browser switching flows, this
-     * could mean the user returned to the app through multi-tasking without completing the flow,
-     * the user closed the browser tab, or the user pressed the back button.
-     */
-    var isExplicitCancelation = false
-        private set
-}
+open class UserCanceledException internal constructor(
+    message: String?,
+    val isExplicitCancelation: Boolean = false
+) : BraintreeException(message)
