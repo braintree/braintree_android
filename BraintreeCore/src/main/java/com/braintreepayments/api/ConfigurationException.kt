a15fef2205fef248b489c3a17dde91a8b846db60
sshropshire <sshropshire@paypal.com>
2023-02-03 10:27:59 -0600

    Migrate ConfigurationException to use JvmOverloads.

--- BraintreeCore/src/main/java/com/braintreepayments/api/ConfigurationException.kt
+++ BraintreeCore/src/main/java/com/braintreepayments/api/ConfigurationException.kt
@@ -5,11 +5,7 @@ import androidx.annotation.RestrictTo
 /**
  * Error class thrown when a configuration value is invalid
  */
-open class ConfigurationException : BraintreeException {
-
-    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-    constructor(message: String?) : super(message)
-
-    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-    constructor(message: String?, cause: Throwable?) : super(message, cause)
-}
+open class ConfigurationException @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP) @JvmOverloads constructor(
+    message: String?,
+    cause: Throwable? = null
+) : BraintreeException(message, cause)
