1b37dad7bbab3919c53f614deaf9d6dfdd0fbe57
sshropshire <sshropshire@paypal.com>
2023-02-03 10:26:30 -0600

    Refactor BraintreeException primary constructor.

--- BraintreeCore/src/main/java/com/braintreepayments/api/BraintreeException.kt
+++ BraintreeCore/src/main/java/com/braintreepayments/api/BraintreeException.kt
@@ -6,14 +6,7 @@ import java.io.IOException
 /**
  * Parent class for exceptions encountered when using the SDK.
  */
-open class BraintreeException : IOException {
-
-    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-    constructor() : super()
-
-    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-    constructor(message: String?) : super(message)
-
-    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-    constructor(message: String?, cause: Throwable?) : super(message, cause)
-}
+open class BraintreeException @JvmOverloads @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP) constructor(
+    message: String? = null,
+    cause: Throwable? = null
+) : IOException(message, cause)
