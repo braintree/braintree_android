54837e91f1f284c1766fa3c62d4752933bd64d80
Jose Noriega <jonoriega@paypal.com>
2023-02-15 18:17:20 -0300

    Refactor uuidHelperUnitTEst

---
+++ BraintreeCore/src/main/java/com/braintreepayments/api/UUIDHelper.kt
@@ -0,0 +1,49 @@
+package com.braintreepayments.api
+
+import android.content.Context
+import androidx.annotation.VisibleForTesting
+import com.braintreepayments.api.BraintreeSharedPreferences
+import com.braintreepayments.api.UUIDHelper
+import java.util.*
+
+internal class UUIDHelper {
+    /**
+     * @param context Android Context
+     * @return A persistent UUID for this application install.
+     */
+    fun getPersistentUUID(context: Context?): String? {
+        return getPersistentUUID(BraintreeSharedPreferences.getInstance(context))
+    }
+
+    @VisibleForTesting
+    fun getPersistentUUID(braintreeSharedPreferences: BraintreeSharedPreferences): String? {
+        var uuid = braintreeSharedPreferences.getString(BRAINTREE_UUID_KEY, null)
+        if (uuid == null) {
+            uuid = formattedUUID
+            braintreeSharedPreferences.putString(BRAINTREE_UUID_KEY, uuid)
+        }
+        return uuid
+    }
+
+    val formattedUUID: String
+        get() = UUID.randomUUID().toString().replace("-", "")
+
+    fun getInstallationGUID(context: Context?): String {
+        return getInstallationGUID(BraintreeSharedPreferences.getInstance(context))
+    }
+
+    @VisibleForTesting
+    fun getInstallationGUID(braintreeSharedPreferences: BraintreeSharedPreferences): String {
+        var installationGUID = braintreeSharedPreferences.getString(INSTALL_GUID, null)
+        if (installationGUID == null) {
+            installationGUID = UUID.randomUUID().toString()
+            braintreeSharedPreferences.putString(INSTALL_GUID, installationGUID)
+        }
+        return installationGUID
+    }
+
+    companion object {
+        private const val BRAINTREE_UUID_KEY = "braintreeUUID"
+        private const val INSTALL_GUID = "InstallationGUID"
+    }
+}
---
+++ BraintreeCore/src/test/java/com/braintreepayments/api/UUIDHelperUnitTest.kt
@@ -0,0 +1,36 @@
+package com.braintreepayments.api
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNotNull
+
+import org.junit.Test
+import java.util.*
+
+class UUIDHelperUnitTest {
+    private var braintreeSharedPreferences: BraintreeSharedPreferences = mockk(relaxed = true)
+
+    @Test
+    fun getInstallationGUID_returnsNewGUIDWhenOneDoesNotExistAndPersistsIt() {
+        every {
+            braintreeSharedPreferences.getString("InstallationGUID", null)
+        } returns null
+
+        val sut = UUIDHelper()
+        val uuid = sut.getInstallationGUID(braintreeSharedPreferences)
+        assertNotNull(uuid)
+        verify { braintreeSharedPreferences.putString("InstallationGUID", uuid) }
+    }
+
+    @Test
+    fun getInstallationGUID_returnsExistingGUIDWhenOneExist() {
+        val uuid = UUID.randomUUID().toString()
+        every {
+            braintreeSharedPreferences.getString("InstallationGUID", null)
+        } returns uuid
+        val sut = UUIDHelper()
+        assertEquals(uuid, sut.getInstallationGUID(braintreeSharedPreferences))
+    }
+}
