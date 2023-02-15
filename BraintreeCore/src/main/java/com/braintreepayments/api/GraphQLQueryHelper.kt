ff344ab053feac03116dd70ac181b93d3c8dfc83
Jose Noriega <jonoriega@paypal.com>
2023-02-15 18:28:50 -0300

    Refactor StreamHelper and GraphQLQueryHelper

---
+++ BraintreeCore/src/main/java/com/braintreepayments/api/GraphQLQueryHelper.kt
@@ -0,0 +1,23 @@
+package com.braintreepayments.api
+
+import android.content.Context
+import android.content.res.Resources
+import androidx.annotation.RestrictTo
+import java.io.IOException
+import java.io.InputStream
+
+//NEXT_MAJOR_VERSION: remove class once its added to drop in
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+object GraphQLQueryHelper {
+    @JvmStatic
+    @Throws(Resources.NotFoundException::class, IOException::class)
+    fun getQuery(context: Context, queryResource: Int): String {
+        var inputStream: InputStream? = null
+        return try {
+            inputStream = context.resources.openRawResource(queryResource)
+            StreamHelper.getString(inputStream)
+        } finally {
+            inputStream?.close()
+        }
+    }
+}
