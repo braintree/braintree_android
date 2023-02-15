7ec5c01fb184239f5308a5ad870659d5997286d8
Jose Noriega <jonoriega@paypal.com>
2023-02-15 15:52:48 -0300

    Graphql constants to kotlin

---
+++ BraintreeCore/src/main/java/com/braintreepayments/api/GraphQLConstants.kt
@@ -0,0 +1,39 @@
+package com.braintreepayments.api
+
+internal object GraphQLConstants {
+    internal object Keys {
+        const val QUERY = "query"
+        const val INPUT = "input"
+        const val VARIABLES = "variables"
+        const val ERRORS = "errors"
+        const val MESSAGE = "message"
+        const val ERROR_TYPE = "errorType"
+        const val EXTENSIONS = "extensions"
+        const val INPUT_PATH = "inputPath"
+        const val LEGACY_CODE = "legacyCode"
+        const val URL = "url"
+        const val FEATURES = "features"
+        const val OPERATION_NAME = "operationName"
+    }
+
+    internal object ErrorTypes {
+        const val USER = "user_error"
+    }
+
+    internal object ErrorMessages {
+        const val UNKNOWN = "An unknown error occurred."
+        const val USER = "Input is invalid."
+    }
+
+    internal object LegacyErrorCodes {
+        const val VALIDATION_NOT_ALLOWED = "50000"
+    }
+
+    internal object Headers {
+        const val API_VERSION = "2018-03-06"
+    }
+
+    internal object Features {
+        const val TOKENIZE_CREDIT_CARDS = "tokenize_credit_cards"
+    }
+}
