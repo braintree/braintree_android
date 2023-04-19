7aee1d372d6040d411fd5b01ac68121b3c8613cb
Kevin Lay <kelay@paypal.com>
2023-04-19 10:02:42 -0700

    -Fixing imports

--- PayPalNativeCheckout/src/main/java/com/braintreepayments/api/PayPalNativeCheckoutClient.java
+++ PayPalNativeCheckout/src/main/java/com/braintreepayments/api/PayPalNativeCheckoutClient.java
@@ -5,11 +5,14 @@ import androidx.annotation.VisibleForTesting;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
-import com.paypal.checkout.*;
-import com.paypal.checkout.approve.*;
-import com.paypal.checkout.config.*;
-import com.paypal.pyplcheckout.instrumentation.constants.*;
-import com.paypal.pyplcheckout.instrumentation.di.*;
+import com.paypal.checkout.PayPalCheckout;
+import com.paypal.checkout.approve.ApprovalData;
+import com.paypal.checkout.config.CheckoutConfig;
+import com.paypal.checkout.config.Environment;
+import com.paypal.checkout.config.SettingsConfig;
+import com.paypal.checkout.config.UIConfig;
+import com.paypal.pyplcheckout.instrumentation.constants.PEnums;
+import com.paypal.pyplcheckout.instrumentation.di.PLog;
 
 import org.json.JSONException;
 import org.json.JSONObject;
