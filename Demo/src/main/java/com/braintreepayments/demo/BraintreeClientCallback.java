cde9c01cd2af0126ecb6e95694fb17d59b469d1a
sshropshire <sshropshire@paypal.com>
2023-04-25 14:38:16 -0500

    Remove unecessary import and add TODO.

--- Demo/src/main/java/com/braintreepayments/demo/BraintreeClientCallback.java
+++ Demo/src/main/java/com/braintreepayments/demo/BraintreeClientCallback.java
@@ -1,10 +1,10 @@
 package com.braintreepayments.demo;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import com.braintreepayments.api.BraintreeClient;
 
+// NEXT MAJOR VERSION: remove
 public interface BraintreeClientCallback {
     void onResult(@NonNull BraintreeClient braintreeClient);
 }
