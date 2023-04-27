62503211aa1230bf12a7233511d44dce9cc816a9
sshropshire <sshropshire@paypal.com>
2023-04-27 13:46:01 -0500

    Migrate BraintreeRequestCodesUnitTest to Kotlin.

---
+++ BraintreeCore/src/test/java/com/braintreepayments/api/BraintreeRequestCodesUnitTest.kt
@@ -0,0 +1,54 @@
+package com.braintreepayments.api
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class BraintreeRequestCodesUnitTest {
+    @Test
+    fun expectedNumberOfRequestCodes() {
+        assertEquals(8, BraintreeRequestCodes::class.java.declaredFields.size)
+    }
+
+    @Test
+    fun threeDSecure() {
+        assertEquals(13487, BraintreeRequestCodes.THREE_D_SECURE)
+    }
+
+    @Test
+    fun venmo() {
+        assertEquals(13488, BraintreeRequestCodes.VENMO)
+    }
+
+    @Test
+    fun paypal() {
+        assertEquals(13591, BraintreeRequestCodes.PAYPAL)
+    }
+
+    @Test
+    fun visaCheckout() {
+        assertEquals(13592, BraintreeRequestCodes.VISA_CHECKOUT)
+    }
+
+    @Test
+    fun googlePay() {
+        assertEquals(13593, BraintreeRequestCodes.GOOGLE_PAY)
+    }
+
+    @Test
+    fun samsungPay() {
+        assertEquals(13595, BraintreeRequestCodes.SAMSUNG_PAY)
+    }
+
+    @Test
+    fun localPayment() {
+        assertEquals(13596, BraintreeRequestCodes.LOCAL_PAYMENT)
+    }
+
+    @Test
+    fun sepDebit() {
+        assertEquals(13597, BraintreeRequestCodes.SEPA_DEBIT)
+    }
+}
