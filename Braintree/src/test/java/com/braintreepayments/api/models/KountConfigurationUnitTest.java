a742ecbb9cebcfc355ac3f071d937d02b0b7c098
Steven Shropshire <sshropshire@paypal.com>
2023-06-05 15:35:43 -0500

    Fix broken configuration kount parsing test.

--- Braintree/src/test/java/com/braintreepayments/api/models/KountConfigurationUnitTest.java
+++ Braintree/src/test/java/com/braintreepayments/api/models/KountConfigurationUnitTest.java
@@ -7,17 +7,17 @@ import org.robolectric.RobolectricTestRunner;
 
 import static com.braintreepayments.testutils.FixturesHelper.stringFromFixture;
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 @RunWith(RobolectricTestRunner.class)
 public class KountConfigurationUnitTest {
 
     @Test
-    public void parsesKountConfiguration() throws JSONException {
+    public void parsesKountConfigurationAsAlwaysDisabled() throws JSONException {
         Configuration configuration = Configuration.fromJson(stringFromFixture("configuration/with_kount.json"));
         KountConfiguration kountConfiguration = configuration.getKount();
 
-        assertTrue(kountConfiguration.isEnabled());
-        assertEquals("600000", kountConfiguration.getKountMerchantId());
+        assertFalse(kountConfiguration.isEnabled());
+        assertEquals("", kountConfiguration.getKountMerchantId());
     }
 }
