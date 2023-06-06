fd14c400aadb4002b94d12c783ed31a30b17548c
Jax DesMarais-Leder <jdesmarais@paypal.com>
2023-06-06 12:39:54 -0500

    update Fixture with BIN for GooglePayClientUnitTest

--- TestUtils/src/main/java/com/braintreepayments/api/Fixtures.kt
+++ TestUtils/src/main/java/com/braintreepayments/api/Fixtures.kt
@@ -2398,7 +2398,7 @@ object Fixtures {
             "description": "Visa •••• 1234",
             "tokenizationData": {
               "type": "PAYMENT_GATEWAY",
-              "token": "{\"androidPayCards\":[{\"type\":\"AndroidPayCard\",\"nonce\":\"d887f42c-bda5-091a-0798-af42d3ed173e\",\"description\":\"Google Pay\",\"consumed\":false,\"details\":{\"cardType\":\"Visa\",\"lastTwo\":\"34\",\"lastFour\":\"1234\"},\"binData\":{\"prepaid\":\"No\",\"healthcare\":\"No\",\"debit\":\"No\",\"durbinRegulated\":\"No\",\"commercial\":\"No\",\"payroll\":\"No\",\"issuingBank\":\"Issuing Bank USA\",\"countryOfIssuance\":\"USA\",\"productId\":\"A\"}}]}"
+              "token": "{\"androidPayCards\":[{\"type\":\"AndroidPayCard\",\"nonce\":\"d887f42c-bda5-091a-0798-af42d3ed173e\",\"description\":\"Google Pay\",\"consumed\":false,\"details\":{\"cardType\":\"Visa\",\"bin\":\"123456\",\"lastTwo\":\"34\",\"lastFour\":\"1234\"},\"binData\":{\"prepaid\":\"No\",\"healthcare\":\"No\",\"debit\":\"No\",\"durbinRegulated\":\"No\",\"commercial\":\"No\",\"payroll\":\"No\",\"issuingBank\":\"Issuing Bank USA\",\"countryOfIssuance\":\"USA\",\"productId\":\"A\"}}]}"
             },
             "type": "CARD",
             "info": {
