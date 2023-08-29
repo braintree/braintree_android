84c71d0fc76c6b244f04a1231315713e5e65dfa7
Sarah Koop <skoop@paypal.com>
2023-08-29 16:52:02 -0500

    Fix analytics URL

--- TestUtils/src/main/java/com/braintreepayments/api/Fixtures.kt
+++ TestUtils/src/main/java/com/braintreepayments/api/Fixtures.kt
@@ -906,7 +906,7 @@ object Fixtures {
             "merchantId": "some-merchant-id",
             "merchantAccountId": "some-merchant-account-id",
             "analytics": {
-                "url": "https://client-analytics.braintreegateway.com/some-merchant-id"
+                "url": "https://client-analytics.braintreegateway.com/dcpspy2brwdjr3qn"
             }
         }
     """
