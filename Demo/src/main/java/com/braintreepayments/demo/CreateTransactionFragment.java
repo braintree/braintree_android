64f882500eec6f94733bbf03189ee80337b6fe2e
Sarah Koop <skoop@paypal.com>
2023-10-03 15:50:33 -0500

    Remove references to UnionPay

--- Demo/src/main/java/com/braintreepayments/demo/CreateTransactionFragment.java
+++ Demo/src/main/java/com/braintreepayments/demo/CreateTransactionFragment.java
@@ -92,9 +92,6 @@ public class CreateTransactionFragment extends Fragment {
             if (Settings.isThreeDSecureRequired(activity)) {
                 transactionRequest.setThreeDSecureRequired(true);
             }
-        } else if (isUnionPayCardNonce(nonce)) {
-            String unionPayMerchantAccountId = Settings.getUnionPayMerchantAccountId(activity);
-            transactionRequest = new TransactionRequest(amount, nonceString, unionPayMerchantAccountId);
         } else {
             String merchantAccountId = Settings.getMerchantAccountId(activity);
             transactionRequest = new TransactionRequest(amount, nonceString, merchantAccountId);
