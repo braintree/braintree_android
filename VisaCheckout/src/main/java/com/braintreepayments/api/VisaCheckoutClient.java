1bab0c1d6336a8b75eb9383d32784b847f54a5ff
Sarah Koop <skoop@paypal.com>
2023-10-12 13:30:59 -0500

    Reformat VisaCheckout

--- VisaCheckout/src/main/java/com/braintreepayments/api/VisaCheckoutClient.java
+++ VisaCheckout/src/main/java/com/braintreepayments/api/VisaCheckoutClient.java
@@ -14,8 +14,8 @@ import org.json.JSONObject;
 import java.util.List;
 
 /**
- * Used to create and tokenize Visa Checkout. For more information see the
- * <a href="https://developer.paypal.com/braintree/docs/guides/secure-remote-commerce/overview">documentation</a>
+ * Used to create and tokenize Visa Checkout. For more information see the <a
+ * href="https://developer.paypal.com/braintree/docs/guides/secure-remote-commerce/overview">documentation</a>
  */
 public class VisaCheckoutClient {
 
@@ -33,11 +33,11 @@ public class VisaCheckoutClient {
     }
 
     /**
-     * Creates a {@link Profile.ProfileBuilder} with the merchant API key, environment, and other properties to be used with
-     * Visa Checkout.
-     *
-     * In addition to setting the `merchantApiKey` and `environment` the other properties that Braintree will fill in
-     * on the ProfileBuilder are:
+     * Creates a {@link Profile.ProfileBuilder} with the merchant API key, environment, and other
+     * properties to be used with Visa Checkout.
+     * <p>
+     * In addition to setting the `merchantApiKey` and `environment` the other properties that
+     * Braintree will fill in on the ProfileBuilder are:
      * <ul>
      *     <li>
      *         {@link Profile.ProfileBuilder#setCardBrands(String[])} A list of Card brands that your merchant account can
@@ -55,33 +55,34 @@ public class VisaCheckoutClient {
      *
      * @param callback {@link VisaCheckoutCreateProfileBuilderCallback}
      */
-    public void createProfileBuilder(@NonNull final VisaCheckoutCreateProfileBuilderCallback callback) {
-        braintreeClient.getConfiguration(new ConfigurationCallback() {
-            @Override
-            public void onResult(@Nullable Configuration configuration, @Nullable Exception e) {
-                boolean enabledAndSdkAvailable =
+    public void createProfileBuilder(
+            @NonNull final VisaCheckoutCreateProfileBuilderCallback callback) {
+        braintreeClient.getConfiguration((configuration, e) -> {
+            boolean enabledAndSdkAvailable =
                     isVisaCheckoutSDKAvailable() && configuration.isVisaCheckoutEnabled();
 
-                if (!enabledAndSdkAvailable) {
-                    callback.onResult(null, new ConfigurationException("Visa Checkout is not enabled."));
-                    return;
-                }
-
-                String merchantApiKey = configuration.getVisaCheckoutApiKey();
-                List<String> acceptedCardBrands = configuration.getVisaCheckoutSupportedNetworks();
-                String environment = Environment.SANDBOX;
-
-                if ("production".equals(configuration.getEnvironment())) {
-                    environment = Environment.PRODUCTION;
-                }
-
-                Profile.ProfileBuilder profileBuilder = new Profile.ProfileBuilder(merchantApiKey, environment);
-                profileBuilder.setCardBrands(acceptedCardBrands.toArray(new String[acceptedCardBrands.size()]));
-                profileBuilder.setDataLevel(Profile.DataLevel.FULL);
-                profileBuilder.setExternalClientId(configuration.getVisaCheckoutExternalClientId());
-
-                callback.onResult(profileBuilder, null);
+            if (!enabledAndSdkAvailable) {
+                callback.onResult(null,
+                        new ConfigurationException("Visa Checkout is not enabled."));
+                return;
             }
+
+            String merchantApiKey = configuration.getVisaCheckoutApiKey();
+            List<String> acceptedCardBrands = configuration.getVisaCheckoutSupportedNetworks();
+            String environment = Environment.SANDBOX;
+
+            if ("production".equals(configuration.getEnvironment())) {
+                environment = Environment.PRODUCTION;
+            }
+
+            Profile.ProfileBuilder profileBuilder =
+                    new Profile.ProfileBuilder(merchantApiKey, environment);
+            profileBuilder.setCardBrands(
+                    acceptedCardBrands.toArray(new String[acceptedCardBrands.size()]));
+            profileBuilder.setDataLevel(Profile.DataLevel.FULL);
+            profileBuilder.setExternalClientId(configuration.getVisaCheckoutExternalClientId());
+
+            callback.onResult(profileBuilder, null);
         });
     }
 
@@ -98,25 +99,25 @@ public class VisaCheckoutClient {
      * Tokenizes the payment summary of the Visa Checkout flow.
      *
      * @param visaPaymentSummary {@link VisaPaymentSummary} The Visa payment to tokenize.
-     * @param callback {@link VisaCheckoutTokenizeCallback}
+     * @param callback           {@link VisaCheckoutTokenizeCallback}
      */
-    public void tokenize(@NonNull VisaPaymentSummary visaPaymentSummary, @NonNull final VisaCheckoutTokenizeCallback callback) {
-        apiClient.tokenizeREST(new VisaCheckoutAccount(visaPaymentSummary), new TokenizeCallback() {
-            @Override
-            public void onResult(JSONObject tokenizationResponse, Exception exception) {
-                if (tokenizationResponse != null) {
-                    try {
-                        VisaCheckoutNonce visaCheckoutNonce = VisaCheckoutNonce.fromJSON(tokenizationResponse);
-                        callback.onResult(visaCheckoutNonce, null);
-                        braintreeClient.sendAnalyticsEvent("visacheckout.tokenize.succeeded");
-                    } catch (JSONException e) {
-                        callback.onResult(null, e);
+    public void tokenize(@NonNull VisaPaymentSummary visaPaymentSummary,
+                         @NonNull final VisaCheckoutTokenizeCallback callback) {
+        apiClient.tokenizeREST(new VisaCheckoutAccount(visaPaymentSummary),
+                (tokenizationResponse, exception) -> {
+                    if (tokenizationResponse != null) {
+                        try {
+                            VisaCheckoutNonce visaCheckoutNonce =
+                                    VisaCheckoutNonce.fromJSON(tokenizationResponse);
+                            callback.onResult(visaCheckoutNonce, null);
+                            braintreeClient.sendAnalyticsEvent("visacheckout.tokenize.succeeded");
+                        } catch (JSONException e) {
+                            callback.onResult(null, e);
+                        }
+                    } else {
+                        callback.onResult(null, exception);
+                        braintreeClient.sendAnalyticsEvent("visacheckout.tokenize.failed");
                     }
-                } else {
-                    callback.onResult(null, exception);
-                    braintreeClient.sendAnalyticsEvent("visacheckout.tokenize.failed");
-                }
-            }
-        });
+                });
     }
 }
