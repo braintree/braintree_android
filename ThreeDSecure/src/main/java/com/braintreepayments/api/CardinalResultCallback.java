31413f19e5db629def57c53caea6b9d2bde53fb5
Sarah Koop <skoop@paypal.com>
2023-10-23 09:58:49 -0500

    add missing doc strings

--- ThreeDSecure/src/main/java/com/braintreepayments/api/CardinalResultCallback.java
+++ ThreeDSecure/src/main/java/com/braintreepayments/api/CardinalResultCallback.java
@@ -2,6 +2,9 @@ package com.braintreepayments.api;
 
 import androidx.annotation.NonNull;
 
+/**
+ * Callback for receiving the results via {@link ThreeDSecureLauncher}
+ */
 public interface CardinalResultCallback {
 
     void onCardinalResult(@NonNull CardinalResult cardinalResult);
