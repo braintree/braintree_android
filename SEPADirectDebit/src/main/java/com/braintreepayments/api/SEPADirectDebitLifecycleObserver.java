0c987a91cd416213c50e89f7c8cb9b35fc3381fc
Sammy Cannillo <scannillo@paypal.com>
2023-10-30 16:56:56 -0500

    Comment out SEPADirectDebitLifecycleObserver

--- SEPADirectDebit/src/main/java/com/braintreepayments/api/SEPADirectDebitLifecycleObserver.java
+++ SEPADirectDebit/src/main/java/com/braintreepayments/api/SEPADirectDebitLifecycleObserver.java
@@ -12,31 +12,31 @@ import androidx.lifecycle.LifecycleEventObserver;
 import androidx.lifecycle.LifecycleOwner;
 
 // NEXT_MAJOR_VERSION: Update to implement DefaultLifeCycleObserver when Java 7 support is explicitly dropped.
-class SEPADirectDebitLifecycleObserver implements LifecycleEventObserver {
-
-    @VisibleForTesting
-    SEPADirectDebitClient sepaDirectDebitClient;
-
-    SEPADirectDebitLifecycleObserver(@NonNull SEPADirectDebitClient sepaDirectDebitClient) {
-        this.sepaDirectDebitClient = sepaDirectDebitClient;
-    }
-
-    @Override
-    public void onStateChanged(@NonNull LifecycleOwner lifecycleOwner, @NonNull Lifecycle.Event event) {
-        if (event == ON_RESUME) {
-            FragmentActivity activity = null;
-            if (lifecycleOwner instanceof FragmentActivity) {
-                activity = (FragmentActivity) lifecycleOwner;
-            } else if (lifecycleOwner instanceof Fragment) {
-                activity = ((Fragment) lifecycleOwner).getActivity();
-            }
-
-            if (activity != null) {
-                BrowserSwitchResult pendingResult = sepaDirectDebitClient.getBrowserSwitchResult(activity);
-                if (pendingResult != null && pendingResult.getRequestCode() == BraintreeRequestCodes.SEPA_DEBIT) {
-                    sepaDirectDebitClient.onBrowserSwitchResult(activity);
-                }
-            }
-        }
-    }
-}
+//class SEPADirectDebitLifecycleObserver implements LifecycleEventObserver {
+//
+//    @VisibleForTesting
+//    SEPADirectDebitClient sepaDirectDebitClient;
+//
+//    SEPADirectDebitLifecycleObserver(@NonNull SEPADirectDebitClient sepaDirectDebitClient) {
+//        this.sepaDirectDebitClient = sepaDirectDebitClient;
+//    }
+//
+//    @Override
+//    public void onStateChanged(@NonNull LifecycleOwner lifecycleOwner, @NonNull Lifecycle.Event event) {
+//        if (event == ON_RESUME) {
+//            FragmentActivity activity = null;
+//            if (lifecycleOwner instanceof FragmentActivity) {
+//                activity = (FragmentActivity) lifecycleOwner;
+//            } else if (lifecycleOwner instanceof Fragment) {
+//                activity = ((Fragment) lifecycleOwner).getActivity();
+//            }
+//
+//            if (activity != null) {
+//                BrowserSwitchResult pendingResult = sepaDirectDebitClient.getBrowserSwitchResult(activity);
+//                if (pendingResult != null && pendingResult.getRequestCode() == BraintreeRequestCodes.SEPA_DEBIT) {
+//                    sepaDirectDebitClient.onBrowserSwitchResult(activity);
+//                }
+//            }
+//        }
+//    }
+//}
