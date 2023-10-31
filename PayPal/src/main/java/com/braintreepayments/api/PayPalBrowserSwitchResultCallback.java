a761900a471b85d1bbe82eb46cb9240c2c6f092e
Sarah Koop <skoop@paypal.com>
2023-10-31 15:48:04 -0500

    Remove extra changes

--- PayPal/src/main/java/com/braintreepayments/api/PayPalBrowserSwitchResultCallback.java
+++ PayPal/src/main/java/com/braintreepayments/api/PayPalBrowserSwitchResultCallback.java
@@ -1,6 +1,5 @@
 package com.braintreepayments.api;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 /**
@@ -13,5 +12,5 @@ public interface PayPalBrowserSwitchResultCallback {
      * @param payPalAccountNonce {@link PayPalAccountNonce}
      * @param error              an exception that occurred while processing a PayPal result
      */
-    void onResult(@NonNull PaymentResult result);
+    void onResult(@Nullable PayPalAccountNonce payPalAccountNonce, @Nullable Exception error);
 }
--- PayPal/src/main/java/com/braintreepayments/api/PayPalInternalClient.java
+++ PayPal/src/main/java/com/braintreepayments/api/PayPalInternalClient.java
@@ -11,122 +11,114 @@ import org.json.JSONObject;
 
 class PayPalInternalClient {
 
-    private static final String CREATE_SINGLE_PAYMENT_ENDPOINT =
-            "paypal_hermes/create_payment_resource";
-    private static final String SETUP_BILLING_AGREEMENT_ENDPOINT =
-            "paypal_hermes/setup_billing_agreement";
+    private static final String CREATE_SINGLE_PAYMENT_ENDPOINT = "paypal_hermes/create_payment_resource";
+    private static final String SETUP_BILLING_AGREEMENT_ENDPOINT = "paypal_hermes/setup_billing_agreement";
 
     private final String cancelUrl;
     private final String successUrl;
 
     private final BraintreeClient braintreeClient;
-    private final DataCollector dataCollector;
+    private final PayPalDataCollector payPalDataCollector;
     private final ApiClient apiClient;
 
     PayPalInternalClient(BraintreeClient braintreeClient) {
-        this(braintreeClient, new DataCollector(braintreeClient), new ApiClient(braintreeClient));
+        this(braintreeClient, new PayPalDataCollector(braintreeClient), new ApiClient(braintreeClient));
     }
 
     @VisibleForTesting
-    PayPalInternalClient(BraintreeClient braintreeClient, DataCollector dataCollector, ApiClient apiClient) {
+    PayPalInternalClient(BraintreeClient braintreeClient, PayPalDataCollector payPalDataCollector, ApiClient apiClient) {
         this.braintreeClient = braintreeClient;
-        this.dataCollector = dataCollector;
+        this.payPalDataCollector = payPalDataCollector;
         this.apiClient = apiClient;
 
-        this.cancelUrl =
-                String.format("%s://onetouch/v1/cancel", braintreeClient.getReturnUrlScheme());
-        this.successUrl =
-                String.format("%s://onetouch/v1/success", braintreeClient.getReturnUrlScheme());
+        this.cancelUrl = String.format("%s://onetouch/v1/cancel", braintreeClient.getReturnUrlScheme());
+        this.successUrl = String.format("%s://onetouch/v1/success", braintreeClient.getReturnUrlScheme());
     }
 
-    void sendRequest(final Context context, final PayPalRequest payPalRequest,
-                     final PayPalInternalClientCallback callback) {
-        braintreeClient.getAuthorization((authorization, authError) -> {
-            if (authorization != null) {
-                braintreeClient.getConfiguration((configuration, configError) -> {
-                    if (configuration == null) {
-                        callback.onResult(null, configError);
-                        return;
-                    }
-                    try {
-                        final boolean isBillingAgreement =
-                                payPalRequest instanceof PayPalVaultRequest;
-                        String endpoint = isBillingAgreement
-                                ? SETUP_BILLING_AGREEMENT_ENDPOINT : CREATE_SINGLE_PAYMENT_ENDPOINT;
-                        String url = String.format("/v1/%s", endpoint);
+    void sendRequest(final Context context, final PayPalRequest payPalRequest, final PayPalInternalClientCallback callback) {
+        braintreeClient.getAuthorization(new AuthorizationCallback() {
+            @Override
+            public void onAuthorizationResult(@Nullable final Authorization authorization, @Nullable Exception authError) {
+                if (authorization != null) {
+                    braintreeClient.getConfiguration(new ConfigurationCallback() {
+                        @Override
+                        public void onResult(@Nullable final Configuration configuration, @Nullable Exception configError) {
+                            if (configuration == null) {
+                                callback.onResult(null, configError);
+                                return;
+                            }
+                            try {
+                                final boolean isBillingAgreement = payPalRequest instanceof PayPalVaultRequest;
+                                String endpoint = isBillingAgreement
+                                        ? SETUP_BILLING_AGREEMENT_ENDPOINT : CREATE_SINGLE_PAYMENT_ENDPOINT;
+                                String url = String.format("/v1/%s", endpoint);
 
-                        String requestBody =
-                                payPalRequest.createRequestBody(configuration, authorization,
-                                        successUrl, cancelUrl);
+                                String requestBody = payPalRequest.createRequestBody(configuration, authorization, successUrl, cancelUrl);
 
-                        braintreeClient.sendPOST(url, requestBody,
-                                (responseBody, httpError) -> {
-                                    if (responseBody != null) {
-                                        try {
-                                            PayPalResponse payPalResponse =
-                                                    new PayPalResponse(payPalRequest)
-                                                            .successUrl(successUrl);
+                                braintreeClient.sendPOST(url, requestBody, new HttpResponseCallback() {
 
-                                            PayPalPaymentResource paypalPaymentResource =
-                                                    PayPalPaymentResource.fromJson(responseBody);
-                                            String redirectUrl =
-                                                    paypalPaymentResource.getRedirectUrl();
-                                            if (redirectUrl != null) {
-                                                Uri parsedRedirectUri = Uri.parse(redirectUrl);
+                                    @Override
+                                    public void onResult(String responseBody, Exception httpError) {
+                                        if (responseBody != null) {
+                                            try {
+                                                PayPalResponse payPalResponse = new PayPalResponse(payPalRequest)
+                                                        .successUrl(successUrl);
 
-                                                String pairingIdKey =
-                                                        isBillingAgreement ? "ba_token" : "token";
-                                                String pairingId =
-                                                        parsedRedirectUri.getQueryParameter(
-                                                                pairingIdKey);
-                                                String clientMetadataId =
-                                                        payPalRequest.getRiskCorrelationId() != null
-                                                                ?
-                                                                payPalRequest.getRiskCorrelationId() :
-                                                                dataCollector.getClientMetadataId(
-                                                                        context, configuration);
+                                                PayPalPaymentResource paypalPaymentResource = PayPalPaymentResource.fromJson(responseBody);
+                                                String redirectUrl = paypalPaymentResource.getRedirectUrl();
+                                                if (redirectUrl != null) {
+                                                    Uri parsedRedirectUri = Uri.parse(redirectUrl);
 
-                                                if (pairingId != null) {
-                                                    payPalResponse
-                                                            .pairingId(pairingId)
-                                                            .clientMetadataId(clientMetadataId);
+                                                    String pairingIdKey = isBillingAgreement ? "ba_token" : "token";
+                                                    String pairingId = parsedRedirectUri.getQueryParameter(pairingIdKey);
+                                                    String clientMetadataId = payPalRequest.getRiskCorrelationId() != null
+                                                            ? payPalRequest.getRiskCorrelationId() : payPalDataCollector.getClientMetadataId(context, configuration);
+
+                                                    if (pairingId != null) {
+                                                        payPalResponse
+                                                                .pairingId(pairingId)
+                                                                .clientMetadataId(clientMetadataId);
+                                                    }
+
+                                                    payPalResponse.approvalUrl(parsedRedirectUri.toString());
                                                 }
+                                                callback.onResult(payPalResponse, null);
 
-                                                payPalResponse.approvalUrl(
-                                                        parsedRedirectUri.toString());
+                                            } catch (JSONException exception) {
+                                                callback.onResult(null, exception);
                                             }
-                                            callback.onResult(payPalResponse, null);
-
-                                        } catch (JSONException exception) {
-                                            callback.onResult(null, exception);
+                                        } else {
+                                            callback.onResult(null, httpError);
                                         }
-                                    } else {
-                                        callback.onResult(null, httpError);
                                     }
                                 });
-                    } catch (JSONException exception) {
-                        callback.onResult(null, exception);
-                    }
-                });
-            } else {
-                callback.onResult(null, authError);
+                            } catch (JSONException exception) {
+                                callback.onResult(null, exception);
+                            }
+                        }
+                    });
+                } else {
+                    callback.onResult(null, authError);
+                }
             }
         });
     }
 
     void tokenize(PayPalAccount payPalAccount, final PayPalBrowserSwitchResultCallback callback) {
-        apiClient.tokenizeREST(payPalAccount, (tokenizationResponse, exception) -> {
-            if (tokenizationResponse != null) {
-                try {
-                    PayPalAccountNonce payPalAccountNonce =
-                            PayPalAccountNonce.fromJSON(tokenizationResponse);
-                    callback.onResult(new PaymentResult.Success(payPalAccountNonce));
+        apiClient.tokenizeREST(payPalAccount, new TokenizeCallback() {
+            @Override
+            public void onResult(JSONObject tokenizationResponse, Exception exception) {
+                if (tokenizationResponse != null) {
+                    try {
+                        PayPalAccountNonce payPalAccountNonce = PayPalAccountNonce.fromJSON(tokenizationResponse);
+                        callback.onResult(payPalAccountNonce, null);
 
-                } catch (JSONException e) {
-                    callback.onResult(new PaymentResult.Failure(e));
+                    } catch (JSONException e) {
+                        callback.onResult(null, e);
+                    }
+                } else {
+                    callback.onResult(null, exception);
                 }
-            } else {
-                callback.onResult(new PaymentResult.Failure(exception));
             }
         });
     }
