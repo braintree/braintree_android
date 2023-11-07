5f21237f48b1626d926447e64dd94cc506a2be33
Sarah Koop <skoop@paypal.com>
2023-11-07 09:56:34 -0600

    Add google pay analytics

---
+++ GooglePay/src/main/java/com/braintreepayments/api/GooglePayAnalytics.java
@@ -0,0 +1,23 @@
+package com.braintreepayments.api;
+
+enum GooglePayAnalytics {
+
+    PAYMENT_REQUEST_STARTED("google-pay:payment-request:started"),
+    PAYMENT_REQUEST_FAILED("google-pay:payment-request:failed"),
+    PAYMENT_REQUEST_SUCCEEDED("google-pay:payment-request:succeeded"),
+
+    TOKENIZE_STARTED("google-pay:tokenize:started"),
+    TOKENIZE_FAILED("google-pay:tokenize:failed"),
+    TOKENIZE_NETWORK_CONNECTION_LOST("google-pay:tokenize:network-connection:failed"),
+    TOKENIZE_SUCCEEDED("google-pay:tokenize:succeeded");
+
+    private final String event;
+
+    GooglePayAnalytics(String event) {
+        this.event = event;
+    }
+
+    String getEvent() {
+        return event;
+    }
+}
