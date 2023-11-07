c85d3355fc6048d0501d3de585710946148e1f8d
Sarah Koop <skoop@paypal.com>
2023-11-07 11:27:34 -0600

    Add venmo analytics event test

---
+++ Venmo/src/test/java/com/braintreepayments/api/VenmoAnalyticsUnitTest.java
@@ -0,0 +1,27 @@
+package com.braintreepayments.api;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class VenmoAnalyticsUnitTest {
+
+    @Test
+    public void testAnalyticsEvents_sendsExpectedEventNames() {
+        assertEquals("venmo:tokenize:app-switch:canceled",
+                VenmoAnalytics.APP_SWITCH_CANCELED.getEvent());
+        assertEquals("venmo:tokenize:app-switch:failed",
+                VenmoAnalytics.APP_SWITCH_FAILED.getEvent());
+        assertEquals("venmo:tokenize:app-switch:succeeded",
+                VenmoAnalytics.APP_SWITCH_SUCCEEDED.getEvent());
+        assertEquals("venmo:tokenize:failed", VenmoAnalytics.TOKENIZE_FAILED.getEvent());
+        assertEquals("venmo:tokenize:started", VenmoAnalytics.TOKENIZE_STARTED.getEvent());
+        assertEquals("venmo:tokenize:succeeded", VenmoAnalytics.TOKENIZE_SUCCEEDED.getEvent());
+        assertEquals("venmo:tokenize:network-connection:failed",
+                VenmoAnalytics.TOKENIZE_NETWORK_CONNECTION_LOST.getEvent());
+    }
+
+}
