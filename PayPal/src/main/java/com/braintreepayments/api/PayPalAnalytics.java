b6a3be57ff2ada9993e7bebc095a7fe83137e6a8
Sarah Koop <skoop@paypal.com>
2023-11-07 10:13:57 -0600

    Add PayPal analytics enum

--- PayPal/src/main/java/com/braintreepayments/api/PayPalAnalytics.java
+++ PayPal/src/main/java/com/braintreepayments/api/PayPalAnalytics.java
@@ -1,4 +1,25 @@
 package com.braintreepayments.api;
 
 enum PayPalAnalytics {
+
+    TOKENIZATION_STARTED("paypal:tokenize:started"),
+    TOKENIZATION_FAILED("paypal:tokenize:failed"),
+    TOKENIZATION_SUCCEEDED("paypal:tokenize:succeeded"),
+    TOKENIZATION_NETWORK_CONNECTION_FAILED("paypal:tokenize:network-connection:failed"),
+
+    BROWSER_PRESENTATION_SUCCEEDED("paypal:tokenize:browser-presentation:succeeded"),
+    BROWSER_PRESENTATION_FAILED("paypal:tokenize:browser-presentation:failed"),
+
+    BROWSER_LOGIN_CANCELED("paypal:tokenize:browser-login:canceled");
+
+    private final String event;
+
+    PayPalAnalytics(String event) {
+        this.event = event;
+    }
+
+    String getEvent() {
+        return event;
+    }
 }
+
