1212b31c3039b69d8859728531c5263755677c63
mhastings <mhastings@paypal.com>
2023-12-06 11:43:29 -0600

    remove special char e.g.

--- BraintreeCore/src/main/java/com/braintreepayments/api/ShopperInsightRequest.kt
+++ BraintreeCore/src/main/java/com/braintreepayments/api/ShopperInsightRequest.kt
@@ -5,7 +5,7 @@ package com.braintreepayments.api
  *
  * @property email The email address of the shopper.
  * @property phoneCountryCode The international country code for the shopper's phone number
- * (e.g., "+1" for the United States).
+ * (e.g., "1" for the United States).
  * @property phoneNationalNumber The national segment of the shopper's phone number
  * (excluding the country code).
  */
