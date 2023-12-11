e2a932034ef677104d7d8a145e78da4ecfd60550
Sai <saperi@paypal.com>
2023-12-11 11:33:03 -0600

    more kdocs

--- BraintreeCore/src/main/java/com/braintreepayments/api/ShopperInsightCallback.kt
+++ BraintreeCore/src/main/java/com/braintreepayments/api/ShopperInsightCallback.kt
@@ -1,7 +1,7 @@
 package com.braintreepayments.api
 
 /**
- * A callback that returns information on whether someone is a PayPal/Venmo shopper.
+ * A callback that returns information on whether someone is a PayPal or a Venmo shopper.
  */
 fun interface ShopperInsightCallback {
     fun onResult(result: ShopperInsightResult)
--- BraintreeCore/src/main/java/com/braintreepayments/api/ShopperInsightResult.kt
+++ BraintreeCore/src/main/java/com/braintreepayments/api/ShopperInsightResult.kt
@@ -1,8 +1,17 @@
 package com.braintreepayments.api
 
+/**
+ * The result object returned when insights about a shopper is requested.
+ */
 sealed class ShopperInsightResult {
 
+    /**
+     * @property response The response object describing the shopper's insights.
+     */
     class Success(val response: ShopperInsightInfo) : ShopperInsightResult()
 
+    /**
+     * @property error An object that describes the error that occurred.
+     */
     class Failure(val error: Exception) : ShopperInsightResult()
 }
