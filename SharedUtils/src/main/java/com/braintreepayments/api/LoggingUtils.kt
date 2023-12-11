2dbc07c583af99a35f6f9dd98281ae7a032642f8
Sarah Koop <skoop@paypal.com>
2023-12-11 15:05:00 -0600

    VenmoListener NPE Fix (#848)
    * Check for listener before returning result
    
    * Add unit test
    
    * Add CHANGELOG
    
    * Fix spacing
    
    * Log warning if listener is null
    
    * Move logging to utils class
    
    * Update tag name
    
    * Fix lint

---
+++ SharedUtils/src/main/java/com/braintreepayments/api/LoggingUtils.kt
@@ -0,0 +1,14 @@
+package com.braintreepayments.api
+
+import androidx.annotation.RestrictTo
+
+/**
+ * @suppress
+ */
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+object LoggingUtils {
+
+    const val TAG = "Braintree SDK"
+
+    const val LISTENER_WARNING = "Unable to deliver result to null listener"
+}
