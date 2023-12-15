4671fecd27498412aff5fedabc2fa20da60263b0
mhastings <mhastings@paypal.com>
2023-12-15 14:35:26 -0600

    remove unused import

--- Demo/src/main/java/com/braintreepayments/demo/ShoppingInsightsFragment.kt
+++ Demo/src/main/java/com/braintreepayments/demo/ShoppingInsightsFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.Switch
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
