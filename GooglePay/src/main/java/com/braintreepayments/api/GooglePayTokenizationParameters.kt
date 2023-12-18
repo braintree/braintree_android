5633f121b20e021c9cbea5d98446e40685a28e71
Sarah Koop <skoop@paypal.com>
2023-12-18 11:44:12 -0600

    Google Pay Public Methods Single Result (#862)
    * Update Google Pay
    
    * Update CHANGELOG
    
    * Fix spacing

---
+++ GooglePay/src/main/java/com/braintreepayments/api/GooglePayTokenizationParameters.kt
@@ -0,0 +1,21 @@
+package com.braintreepayments.api
+
+import com.google.android.gms.wallet.PaymentMethodTokenizationParameters
+
+/**
+ * A request used to get Braintree specific tokenization parameters for a Google Pay
+ */
+sealed class GooglePayTokenizationParameters {
+
+    /**
+     * The request was successfully created
+     */
+    class Success(val parameters: PaymentMethodTokenizationParameters,
+                  val allowedCardNetworks: Collection<Integer>
+    ) : GooglePayTokenizationParameters()
+
+    /**
+     * There was an [error] creating the request
+     */
+    class Failure(val error: Exception) : GooglePayTokenizationParameters()
+}
