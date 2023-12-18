1da84e1604ed93c06dc29def282b4c08ac2fbef0
Sarah Koop <skoop@paypal.com>
2023-12-18 11:37:57 -0600

    Fix spacing

--- GooglePay/src/main/java/com/braintreepayments/api/GooglePayTokenizationParameters.kt
+++ GooglePay/src/main/java/com/braintreepayments/api/GooglePayTokenizationParameters.kt
@@ -3,15 +3,16 @@ package com.braintreepayments.api
 import com.google.android.gms.wallet.PaymentMethodTokenizationParameters
 
 /**
- * A request used to launch the Venmo app for continuation of the Venmo payment flow.
+ * A request used to get Braintree specific tokenization parameters for a Google Pay
  */
 sealed class GooglePayTokenizationParameters {
 
     /**
-     * The request was successfully created and is ready to be launched by [VenmoLauncher]
+     * The request was successfully created
      */
-    class Success(val parameters: PaymentMethodTokenizationParameters, val allowedCardNetworks:
-    Collection<Integer>) : GooglePayTokenizationParameters()
+    class Success(val parameters: PaymentMethodTokenizationParameters,
+                  val allowedCardNetworks: Collection<Integer>
+    ) : GooglePayTokenizationParameters()
 
     /**
      * There was an [error] creating the request
