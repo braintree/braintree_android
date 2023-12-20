c5c025ed9c73f4311da9cd18f91f05b6a4e33dc6
Sai <saperi@paypal.com>
2023-12-20 15:49:31 -0600

    fixing lint issue

--- ShopperInsights/src/main/java/com/braintreepayments/api/ShopperInsightsAnalytics.kt
+++ ShopperInsights/src/main/java/com/braintreepayments/api/ShopperInsightsAnalytics.kt
@@ -1,8 +1,8 @@
 package com.braintreepayments.api
 
 sealed class ShopperInsightsAnalytics(val event: String) {
-    object PayPalPresented: ShopperInsightsAnalytics("shopper-insights:paypal-presented")
-    object PayPalSelected: ShopperInsightsAnalytics("shopper-insights:paypal-selected")
-    object VenmoPresented: ShopperInsightsAnalytics("shopper-insights:venmo-presented")
-    object VenmoSelected: ShopperInsightsAnalytics("shopper-insights:venmo-selected")
+    object PayPalPresented : ShopperInsightsAnalytics("shopper-insights:paypal-presented")
+    object PayPalSelected : ShopperInsightsAnalytics("shopper-insights:paypal-selected")
+    object VenmoPresented : ShopperInsightsAnalytics("shopper-insights:venmo-presented")
+    object VenmoSelected : ShopperInsightsAnalytics("shopper-insights:venmo-selected")
 }
