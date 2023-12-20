31f10d9ead6d3fa7228e897d7b341412215a407c
Max Hastings <128730898+MaxHastingsPP@users.noreply.github.com>
2023-12-20 13:20:19 -0600

    Add Shopping Insights Demo App Integration (#854)
    * Add Shopping Insights Fragment
    
    * move insight row
    
    * added shopping insight vm, ui and integration
    
    * fix linting
    
    * Add email and phone null switches
    
    * remove unused import
    
    * import ShopperInsights module
    
    ---------
    
    Co-authored-by: Sai <saperi@paypal.com>

--- Demo/src/main/java/com/braintreepayments/demo/MainFragment.java
+++ Demo/src/main/java/com/braintreepayments/demo/MainFragment.java
@@ -37,6 +37,7 @@ public class MainFragment extends BaseFragment {
         Button samsungButton = view.findViewById(R.id.samsung_pay);
         Button sepaDirectDebitButton = view.findViewById(R.id.sepa_debit);
         Button payPalNativeCheckoutButton = view.findViewById(R.id.paypal_native_checkout);
+        Button shoppingInsightsButton = view.findViewById(R.id.shopping_insight);
 
         cardsButton.setOnClickListener(this::launchCards);
         payPalButton.setOnClickListener(this::launchPayPal);
@@ -47,6 +48,7 @@ public class MainFragment extends BaseFragment {
         samsungButton.setOnClickListener(this::launchSamsungPay);
         payPalNativeCheckoutButton.setOnClickListener(this::launchPayPalNativeCheckout);
         sepaDirectDebitButton.setOnClickListener(this::launchSEPADirectDebit);
+        shoppingInsightsButton.setOnClickListener(this::launchShoppingInsights);
 
         return view;
     }
@@ -124,6 +126,12 @@ public class MainFragment extends BaseFragment {
 
     public void launchSEPADirectDebit(View v) {
         NavDirections action = MainFragmentDirections.actionMainFragmentToSepaDirectDebitFragment();
+        Navigation.findNavController(v).navigate(action);
+    }
+
+    public void launchShoppingInsights(View v) {
+        NavDirections action =
+                MainFragmentDirections.actionMainFragmentToShoppingInsightsFragment();
         Navigation.findNavController(v).navigate(action);
     }
 }
---
+++ Demo/src/main/java/com/braintreepayments/demo/ShoppingInsightsFragment.kt
@@ -0,0 +1,66 @@
+package com.braintreepayments.demo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.switchmaterial.SwitchMaterial
+import com.google.android.material.textfield.TextInputLayout
+
+/**
+ * Fragment for handling shopping insights.
+ */
+class ShoppingInsightsFragment : Fragment() {
+
+    private lateinit var responseTextView: TextView
+    private lateinit var actionButton: Button
+    private lateinit var emailInput: TextInputLayout
+    private lateinit var countryCodeInput: TextInputLayout
+    private lateinit var nationalNumberInput: TextInputLayout
+    private lateinit var emailNullSwitch: SwitchMaterial
+    private lateinit var phoneNullSwitch: SwitchMaterial
+
+    private val viewModel: ShoppingInsightViewModel by lazy {
+        ViewModelProvider(this)[ShoppingInsightViewModel::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_shopping_insights, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initializeViews(view)
+        setupActionButton()
+    }
+
+    private fun initializeViews(view: View) {
+        responseTextView = view.findViewById(R.id.responseTextView)
+        actionButton = view.findViewById(R.id.actionButton)
+        emailInput = view.findViewById(R.id.emailInput)
+        countryCodeInput = view.findViewById(R.id.countryCodeInput)
+        nationalNumberInput = view.findViewById(R.id.nationalNumberInput)
+        emailNullSwitch = view.findViewById(R.id.emailNullSwitch)
+        phoneNullSwitch = view.findViewById(R.id.phoneNullSwitch)
+    }
+
+    private fun setupActionButton() {
+        actionButton.setOnClickListener {
+            val email = if (emailNullSwitch.isChecked) null else emailInput.editText?.text.toString()
+            val countryCode = if (phoneNullSwitch.isChecked) null else countryCodeInput.editText?.text.toString()
+            val nationalNumber = if (phoneNullSwitch.isChecked) null else nationalNumberInput.editText?.text.toString()
+            viewModel.getRecommendedPaymentMethods(email, countryCode, nationalNumber)
+                .observe(viewLifecycleOwner) {
+                responseTextView.text = it.toString()
+            }
+        }
+    }
+}
