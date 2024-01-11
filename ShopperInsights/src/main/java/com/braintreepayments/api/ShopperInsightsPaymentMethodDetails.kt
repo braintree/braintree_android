aa7b6a148597e982b09bb5813bc17457f94b4684
mhastings <mhastings@paypal.com>
2024-01-11 12:48:11 -0600

    rename to eligibleInPayPalNetwork

--- ShopperInsights/src/main/java/com/braintreepayments/api/ShopperInsightsPaymentMethodDetails.kt
+++ ShopperInsights/src/main/java/com/braintreepayments/api/ShopperInsightsPaymentMethodDetails.kt
@@ -6,13 +6,13 @@ import org.json.JSONObject
  * Details of a specific payment method.
  *
  * @property canBeVaulted Indicates if the payment method can be saved for future transactions.
- * @property eligibleInPaypalNetwork Indicates if the payment method is eligible in the PayPal network.
+ * @property eligibleInPayPalNetwork Indicates if the payment method is eligible in the PayPal network.
  * @property recommended Indicates if this payment method is recommended for the shopper.
  * @property recommendedPriority The priority ranking of this payment method if recommended.
  */
 internal data class ShopperInsightsPaymentMethodDetails(
     val canBeVaulted: Boolean,
-    val eligibleInPaypalNetwork: Boolean,
+    val eligibleInPayPalNetwork: Boolean,
     val recommended: Boolean,
     val recommendedPriority: Int
 ) {
@@ -20,7 +20,7 @@ internal data class ShopperInsightsPaymentMethodDetails(
         fun fromJson(jsonObject: JSONObject): ShopperInsightsPaymentMethodDetails {
             return ShopperInsightsPaymentMethodDetails(
                 canBeVaulted = jsonObject.optBoolean("can_be_vaulted"),
-                eligibleInPaypalNetwork = jsonObject.optBoolean("eligible_in_paypal_network"),
+                eligibleInPayPalNetwork = jsonObject.optBoolean("eligible_in_paypal_network"),
                 recommended = jsonObject.optBoolean("recommended"),
                 recommendedPriority = jsonObject.optInt("recommended_priority")
             )
