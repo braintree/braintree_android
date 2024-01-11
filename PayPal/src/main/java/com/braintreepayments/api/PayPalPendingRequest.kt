1df9be0bbc32c17dcbc915813e49281c13ab3be2
Sarah Koop <skoop@paypal.com>
2024-01-11 14:28:18 -0600

    Add to and from json string methods

--- PayPal/src/main/java/com/braintreepayments/api/PayPalPendingRequest.kt
+++ PayPal/src/main/java/com/braintreepayments/api/PayPalPendingRequest.kt
@@ -1,5 +1,7 @@
 package com.braintreepayments.api
 
+import org.json.JSONObject
+
 /**
  * A pending request for the PayPal web-based authentication flow created by invoking
  * [PayPalLauncher.launch]. This pending request should be stored locally within the app or
@@ -12,7 +14,28 @@ sealed class PayPalPendingRequest {
      * A pending request was successfully started. This [PayPalPendingRequest.Started] should be
      * stored and passed to [PayPalLauncher.handleReturnToAppFromBrowser]
      */
-    class Started(val request: PayPalBrowserSwitchRequest) : PayPalPendingRequest()
+    class Started(val request: PayPalBrowserSwitchRequest) : PayPalPendingRequest() {
+        fun toJsonString(): String {
+            val json = JSONObject()
+            json.put("browserSwitchRequest", request.browserSwitchRequest.toJson())
+            return json.toString()
+        }
+
+        companion object {
+            fun fromJsonString(jsonString: String): Started {
+                val json = JSONObject(jsonString)
+                return Started(
+                    PayPalBrowserSwitchRequest(
+                        BrowserSwitchRequest.fromJson(
+                            json.getString(
+                                "browserSwitchRequest"
+                            )
+                        )
+                    )
+                )
+            }
+        }
+    }
 
     /**
      * An error occurred launching the PayPal browser flow. See [error] for details.
