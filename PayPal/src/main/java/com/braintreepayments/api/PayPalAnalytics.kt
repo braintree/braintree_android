280322f6515516218cc51fc8414ec46cae914f3b
Sarah Koop <skoop@paypal.com>
2024-02-05 09:26:40 -0600

    Convert PayPalAnalytics Enum

--- PayPal/src/main/java/com/braintreepayments/api/PayPalAnalytics.kt
+++ PayPal/src/main/java/com/braintreepayments/api/PayPalAnalytics.kt
@@ -1,14 +1,14 @@
 package com.braintreepayments.api
 
-internal enum class PayPalAnalytics(@JvmField val event: String) {
+internal object PayPalAnalytics {
 
     // Conversion Events
-    TOKENIZATION_STARTED("paypal:tokenize:started"),
-    TOKENIZATION_FAILED("paypal:tokenize:failed"),
-    TOKENIZATION_SUCCEEDED("paypal:tokenize:succeeded"),
-    BROWSER_LOGIN_CANCELED("paypal:tokenize:browser-login:canceled"),
+    const val TOKENIZATION_STARTED = "paypal:tokenize:started"
+    const val TOKENIZATION_FAILED = "paypal:tokenize:failed"
+    const val TOKENIZATION_SUCCEEDED = "paypal:tokenize:succeeded"
+    const val BROWSER_LOGIN_CANCELED = "paypal:tokenize:browser-login:canceled"
 
     // Browser Presentation Events
-    BROWSER_SWITCH_SUCCEEDED("paypal:tokenize:browser-presentation:succeeded"),
-    BROWSER_SWITCH_FAILED("paypal:tokenize:browser-presentation:failed")
+    const val BROWSER_SWITCH_SUCCEEDED = "paypal:tokenize:browser-presentation:succeeded"
+    const val BROWSER_SWITCH_FAILED = "paypal:tokenize:browser-presentation:failed"
 }
