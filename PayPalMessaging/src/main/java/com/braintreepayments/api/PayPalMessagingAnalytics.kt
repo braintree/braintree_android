8163fd601ede121f2e7171d35b33be4fc738d1f2
Justin Warmkessel <Jwarmkessel@paypal.com>
2024-02-06 14:04:16 -1000

    update enum for analytics event names

--- PayPalMessaging/src/main/java/com/braintreepayments/api/PayPalMessagingAnalytics.kt
+++ PayPalMessaging/src/main/java/com/braintreepayments/api/PayPalMessagingAnalytics.kt
@@ -1,7 +1,7 @@
 package com.braintreepayments.api;
 
-internal enum class PayPalMessagingAnalytics(val value: String) {
-    STARTED("paypal-messaging:create-view:started"),
-    FAILED("paypal-messaging:create-view:failed"),
-    SUCCEEDED("paypal-messaging:create-view:succeeded")
+internal object PayPalMessagingAnalytics {
+    const val STARTED = "paypal-messaging:create-view:started"
+    const val FAILED = "paypal-messaging:create-view:failed"
+    const val SUCCEEDED = "paypal-messaging:create-view:succeeded"
 }
