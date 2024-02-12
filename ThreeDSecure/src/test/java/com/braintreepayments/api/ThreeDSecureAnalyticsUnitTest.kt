18ad69438e30f7e4ad0d2141143078c2f28e11f6
Sarah Koop <skoop@paypal.com>
2024-02-12 10:17:30 -0600

    Fix Analytics Unit tests

--- ThreeDSecure/src/test/java/com/braintreepayments/api/ThreeDSecureAnalyticsUnitTest.kt
+++ ThreeDSecure/src/test/java/com/braintreepayments/api/ThreeDSecureAnalyticsUnitTest.kt
@@ -7,34 +7,26 @@ class ThreeDSecureAnalyticsUnitTest {
 
     @Test
     fun testAnalyticsEvents_sendsExpectedEventNames() {
-        assertEquals("3ds:verify:started", ThreeDSecureAnalytics.VERIFY_STARTED.event)
-        assertEquals("3ds:verify:succeeded", ThreeDSecureAnalytics.VERIFY_SUCCEEDED.event)
-        assertEquals("3ds:verify:failed", ThreeDSecureAnalytics.VERIFY_FAILED.event)
-        assertEquals("3ds:verify:canceled", ThreeDSecureAnalytics.VERIFY_CANCELED.event)
+        assertEquals("3ds:verify:started", ThreeDSecureAnalytics.VERIFY_STARTED)
+        assertEquals("3ds:verify:succeeded", ThreeDSecureAnalytics.VERIFY_SUCCEEDED)
+        assertEquals("3ds:verify:failed", ThreeDSecureAnalytics.VERIFY_FAILED)
+        assertEquals("3ds:verify:canceled", ThreeDSecureAnalytics.VERIFY_CANCELED)
         assertEquals(
             "3ds:verify:lookup:succeeded",
-            ThreeDSecureAnalytics.LOOKUP_SUCCEEDED.event
+            ThreeDSecureAnalytics.LOOKUP_SUCCEEDED
         )
-        assertEquals("3ds:verify:lookup:failed", ThreeDSecureAnalytics.LOOKUP_FAILED.event)
+        assertEquals("3ds:verify:lookup:failed", ThreeDSecureAnalytics.LOOKUP_FAILED)
         assertEquals(
             "3ds:verify:lookup:challenge-required",
-            ThreeDSecureAnalytics.CHALLENGE_REQUIRED.event
-        )
-        assertEquals(
-            "3ds:verify:challenge.succeeded",
-            ThreeDSecureAnalytics.CHALLENGE_SUCCEEDED.event
-        )
-        assertEquals(
-            "3ds:verify:challenge.failed",
-            ThreeDSecureAnalytics.CHALLENGE_FAILED.event
+            ThreeDSecureAnalytics.CHALLENGE_REQUIRED
         )
         assertEquals(
             "3ds:verify:authenticate-jwt:succeeded",
-            ThreeDSecureAnalytics.JWT_AUTH_SUCCEEDED.event
+            ThreeDSecureAnalytics.JWT_AUTH_SUCCEEDED
         )
         assertEquals(
             "3ds:verify:authenticate-jwt:failed",
-            ThreeDSecureAnalytics.JWT_AUTH_FAILED.event
+            ThreeDSecureAnalytics.JWT_AUTH_FAILED
         )
     }
 }
