02ffde81f3972e89245ce6ac48894c58fdb40e6c
Sarah Koop <skoop@paypal.com>
2024-02-15 10:44:07 -0600

    Add VisaCheckoutAnalytics

---
+++ VisaCheckout/src/main/java/com/braintreepayments/api/VisaCheckoutAnalytics.kt
@@ -0,0 +1,9 @@
+package com.braintreepayments.api
+
+internal object VisaCheckoutAnalytics {
+
+    // Conversion Events
+    const val TOKENIZE_STARTED = "visa-checkout:tokenize:started"
+    const val TOKENIZE_SUCCEEDED = "visa-checkout:tokenize:succeeded"
+    const val TOKENIZE_FAILED = "visa-checkout:tokenize:failed"
+}
