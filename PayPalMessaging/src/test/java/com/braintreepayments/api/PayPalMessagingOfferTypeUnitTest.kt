b0ef1651d073412f13364246146ecf07ab5bd75e
Jax DesMarais-Leder <jdesmarais@paypal.com>
2024-02-20 14:44:03 -0600

    update line lengths for linter

--- PayPalMessaging/src/test/java/com/braintreepayments/api/PayPalMessagingOfferTypeUnitTest.kt
+++ PayPalMessaging/src/test/java/com/braintreepayments/api/PayPalMessagingOfferTypeUnitTest.kt
@@ -7,21 +7,33 @@ import org.junit.Test
 class PayPalMessagingOfferTypeUnitTest {
     @Test
     fun testOfferType_withPayLaterShortTerm_returnsRawValuePayLaterShortTerm() {
-        assertEquals(PayPalMessageOfferType.PAY_LATER_SHORT_TERM, PayPalMessagingOfferType.PAY_LATER_SHORT_TERM.offerTypeRawValue)
+        assertEquals(
+            PayPalMessageOfferType.PAY_LATER_SHORT_TERM,
+            PayPalMessagingOfferType.PAY_LATER_SHORT_TERM.offerTypeRawValue
+        )
     }
 
     @Test
     fun testOfferType_withPayLaterLongTerm_returnsRawValuePayLaterLongTerm() {
-        assertEquals(PayPalMessageOfferType.PAY_LATER_LONG_TERM, PayPalMessagingOfferType.PAY_LATER_LONG_TERM.offerTypeRawValue)
+        assertEquals(
+            PayPalMessageOfferType.PAY_LATER_LONG_TERM,
+            PayPalMessagingOfferType.PAY_LATER_LONG_TERM.offerTypeRawValue
+        )
     }
 
     @Test
     fun testOfferType_withPayLaterPayInOne_returnsRawValuePayLaterPayIn1() {
-        assertEquals(PayPalMessageOfferType.PAY_LATER_PAY_IN_1, PayPalMessagingOfferType.PAY_LATER_PAY_IN_ONE.offerTypeRawValue)
+        assertEquals(
+            PayPalMessageOfferType.PAY_LATER_PAY_IN_1,
+            PayPalMessagingOfferType.PAY_LATER_PAY_IN_ONE.offerTypeRawValue
+        )
     }
 
     @Test
     fun testOfferType_withPayPalCreditNoInterest_returnsRawValuePayPalCreditNoInterest() {
-        assertEquals(PayPalMessageOfferType.PAYPAL_CREDIT_NO_INTEREST, PayPalMessagingOfferType.PAYPAL_CREDIT_NO_INTEREST.offerTypeRawValue)
+        assertEquals(
+            PayPalMessageOfferType.PAYPAL_CREDIT_NO_INTEREST,
+            PayPalMessagingOfferType.PAYPAL_CREDIT_NO_INTEREST.offerTypeRawValue
+        )
     }
 }
