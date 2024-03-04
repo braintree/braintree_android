02effcceb32ad44fc7e31fa73f51cd704c0e17cc
richherrera <ricardherrera@paypal.com>
2024-03-04 14:25:28 -0600

    Fix: send pairing id instead of client id on Native checkout flow

--- PayPalNativeCheckout/src/main/java/com/braintreepayments/api/PayPalNativeCheckoutClient.java
+++ PayPalNativeCheckout/src/main/java/com/braintreepayments/api/PayPalNativeCheckoutClient.java
@@ -147,10 +147,10 @@ public class PayPalNativeCheckoutClient {
             final PayPalNativeRequest payPalRequest,
             final Configuration configuration
     ) {
-        braintreeClient.setPayPalContextID(configuration.getPayPalClientId());
         internalPayPalClient.sendRequest(activity, payPalRequest, (payPalResponse, error) -> {
             if (payPalResponse != null) {
                 String analyticsPrefix = payPalRequest instanceof PayPalNativeCheckoutVaultRequest ? "billing-agreement" : "single-payment";
+                braintreeClient.setPayPalContextID(payPalResponse.getPairingId());
                 braintreeClient.sendAnalyticsEvent(String.format("paypal-native.%s.started", analyticsPrefix));
 
                 Environment environment;
