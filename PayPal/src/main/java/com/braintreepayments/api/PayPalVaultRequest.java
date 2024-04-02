311b29cc4714e6a5d770d386094306d0bcbb9502
Timothy Chow <tichow@paypal.com>
2024-04-02 16:18:57 -0500

    Update Magnes SDK to 5.5.0 and add javadocs for the hasUserLocationConsent param in Local Payment and PayPal modules

--- PayPal/src/main/java/com/braintreepayments/api/PayPalVaultRequest.java
+++ PayPal/src/main/java/com/braintreepayments/api/PayPalVaultRequest.java
@@ -27,7 +27,13 @@ public class PayPalVaultRequest extends PayPalRequest implements Parcelable {
     }
 
     /**
-     * TODO: add javadoc for hasUserLocationConsent param
+     * @param hasUserLocationConsent is an optional parameter that informs the SDK
+     * if your application has obtained consent from the user to collect location data in compliance with
+     * <a href="https://support.google.com/googleplay/android-developer/answer/10144311#personal-sensitive">Google Play Developer Program policies</a>
+     * This flag enables PayPal to collect necessary information required for Fraud Detection and Risk Management.
+     *
+     * @see <a href="https://support.google.com/googleplay/android-developer/answer/10144311#personal-sensitive">User Data policies for the Google Play Developer Program </a>
+     * @see <a href="https://support.google.com/googleplay/android-developer/answer/9799150?hl=en#Prominent%20in-app%20disclosure">Examples of prominent in-app disclosures</a>
      */
     public PayPalVaultRequest(boolean hasUserLocationConsent) {
         super(hasUserLocationConsent);
