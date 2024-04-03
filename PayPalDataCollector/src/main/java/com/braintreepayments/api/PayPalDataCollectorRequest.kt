023f6ff0c4928662f77264c69ba4467b3da78f9f
Timothy Chow <tichow@paypal.com>
2024-04-03 09:36:19 -0500

    Bump Detekt to 1.23.6 for ignoring maxLineLength for comments that contain URLs

--- PayPalDataCollector/src/main/java/com/braintreepayments/api/PayPalDataCollectorRequest.kt
+++ PayPalDataCollector/src/main/java/com/braintreepayments/api/PayPalDataCollectorRequest.kt
@@ -5,11 +5,11 @@ package com.braintreepayments.api
  *
  * @property hasUserLocationConsent is an optional parameter that informs the SDK
  * if your application has obtained consent from the user to collect location data in compliance with
- * <a href="https://support.google.com/googleplay/android-developer/answer/10144311#personal-sensitive">Google Play Developer Program policies</a>
+ * [Google Play Developer Program policies](https://support.google.com/googleplay/android-developer/answer/10144311#personal-sensitive)
  * This flag enables PayPal to collect necessary information required for Fraud Detection and Risk Management.
  *
- * @see <a href="https://support.google.com/googleplay/android-developer/answer/10144311#personal-sensitive">User Data policies for the Google Play Developer Program </a>
- * @see <a href="https://support.google.com/googleplay/android-developer/answer/9799150?hl=en#Prominent%20in-app%20disclosure">Examples of prominent in-app disclosures</a>
+ * @see [User Data policies for the Google Play Developer Program](https://support.google.com/googleplay/android-developer/answer/10144311#personal-sensitive)
+ * @see [Examples of prominent in-app disclosures](https://support.google.com/googleplay/android-developer/answer/9799150?hl=en#Prominent%20in-app%20disclosure)
  */
 data class PayPalDataCollectorRequest(
     val hasUserLocationConsent: Boolean
