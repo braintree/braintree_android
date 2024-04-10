d18f2ffc050b2cb2988756fcb0f3ce9c38bc8ad5
Sai <saperi@paypal.com>
2024-04-09 18:26:52 -0700

    fix more package names

--- Demo/src/main/java/com/braintreepayments/demo/BraintreeClientFactory.java
+++ Demo/src/main/java/com/braintreepayments/demo/BraintreeClientFactory.java
@@ -1,9 +1,5 @@
 package com.braintreepayments.demo;
 
-import android.content.Context;
-
-import com.braintreepayments.api.BraintreeClient;
-
 // TODO: remove when AuthorizationProvider is released
 public class BraintreeClientFactory {
 
