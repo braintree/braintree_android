c4cdca4ce0249c96161a17a457334304914de2e3
Tim Chow <tichow@paypal.com>
2024-04-16 12:44:45 -0500

    Update hasUserLocationConsent doc to remove optional
    Co-authored-by: Sarah Koop <skoop@paypal.com>

--- PayPalNativeCheckout/src/main/java/com/braintreepayments/api/PayPalNativeCheckoutVaultRequest.java
+++ PayPalNativeCheckout/src/main/java/com/braintreepayments/api/PayPalNativeCheckoutVaultRequest.java
@@ -23,7 +23,7 @@ public class PayPalNativeCheckoutVaultRequest extends PayPalNativeRequest implem
     }
 
     /**
-     * @param hasUserLocationConsent is an optional parameter that informs the SDK
+     * @param hasUserLocationConsent informs the SDK
      * if your application has obtained consent from the user to collect location data in compliance with
      * <a href="https://support.google.com/googleplay/android-developer/answer/10144311#personal-sensitive">Google Play Developer Program policies</a>
      * This flag enables PayPal to collect necessary information required for Fraud Detection and Risk Management.
