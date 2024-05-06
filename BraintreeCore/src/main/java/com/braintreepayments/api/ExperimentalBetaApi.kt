3a277cd2b9d8b5c9465eb0623a6a31cfc2666d1d
Sai <saperi@paypal.com>
2024-05-06 12:00:30 -0700

    Add kdocs to annotation class

--- BraintreeCore/src/main/java/com/braintreepayments/api/ExperimentalBetaApi.kt
+++ BraintreeCore/src/main/java/com/braintreepayments/api/ExperimentalBetaApi.kt
@@ -1,4 +1,9 @@
 package com.braintreepayments.api
 
+/**
+ * This annotation signals that the annotated class is an experimental feature that is in beta.
+ * No guarantees are made on the stability of the beta API. They can be modified or removed in a
+ * future release.
+ */
 @RequiresOptIn
 annotation class ExperimentalBetaApi
