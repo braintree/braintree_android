254eeb1b5bb157fb29a1ca39f56029917bb13a3e
Sai <saperi@paypal.com>
2024-05-10 12:35:20 -0700

    Pass appLinkUrl to braintree client on the demo app when user uses client token

--- Demo/src/main/java/com/braintreepayments/demo/BraintreeClientFactory.java
+++ Demo/src/main/java/com/braintreepayments/demo/BraintreeClientFactory.java
@@ -1,13 +1,14 @@
 package com.braintreepayments.demo;
 
 import android.content.Context;
+import android.net.Uri;
 
 import com.braintreepayments.api.BraintreeClient;
 
 // TODO: remove when AuthorizationProvider is released
 public class BraintreeClientFactory {
 
-    static public BraintreeClient createBraintreeClientWithAuthorizationProvider(Context context) {
-        return new BraintreeClient(context, new DemoClientTokenProvider(context));
+    static public BraintreeClient createBraintreeClientWithAuthorizationProvider(Context context, Uri appLinkUri) {
+        return new BraintreeClient(context, new DemoClientTokenProvider(context), null, appLinkUri);
     }
 }
