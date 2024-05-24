9ce80d247ee0a6bdd5111323c9bea7f8a2e21469
Timothy Chow <tichow@paypal.com>
2024-05-24 09:53:06 -0500

    Remove deprecated request constructors that do not contain the hasUserLocationConsent boolean

--- LocalPayment/src/main/java/com/braintreepayments/api/localpayment/LocalPaymentRequest.java
+++ LocalPayment/src/main/java/com/braintreepayments/api/localpayment/LocalPaymentRequest.java
@@ -50,14 +50,6 @@ public class LocalPaymentRequest {
     private final boolean hasUserLocationConsent;
 
     /**
-     * Deprecated. Use {@link LocalPaymentRequest#LocalPaymentRequest(boolean)} instead.
-     **/
-    @Deprecated
-    public LocalPaymentRequest() {
-        this(false);
-    }
-
-    /**
      * @param hasUserLocationConsent is an optional parameter that informs the SDK
      * if your application has obtained consent from the user to collect location data in compliance with
      * <a href="https://support.google.com/googleplay/android-developer/answer/10144311#personal-sensitive">Google Play Developer Program policies</a>
