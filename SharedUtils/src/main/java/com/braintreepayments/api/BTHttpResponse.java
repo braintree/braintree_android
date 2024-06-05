fe01836a3a16da36e18caa0285c925c0ad3dd7d4
richherrera <ricardherrera@paypal.com>
2024-06-05 09:48:33 -0600

    Rename BTHTTPResponse to BTHttpResponse

--- SharedUtils/src/main/java/com/braintreepayments/api/BTHttpResponse.java
+++ SharedUtils/src/main/java/com/braintreepayments/api/BTHttpResponse.java
@@ -1,11 +1,11 @@
 package com.braintreepayments.api;
 
-public class BTHTTPResponse {
+public class BTHttpResponse {
     long startTime;
     long endTime;
     String body;
 
-    public BTHTTPResponse(long startTime, long endTime, String body) {
+    public BTHttpResponse(long startTime, long endTime, String body) {
         this.startTime = startTime;
         this.endTime = endTime;
         this.body = body;
