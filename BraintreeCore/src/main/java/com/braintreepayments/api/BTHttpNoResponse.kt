caf45514645171f6e585e121b9dfd03480375561
richherrera <ricardherrera@paypal.com>
2024-06-07 14:20:45 -0600

    Fix lint

--- BraintreeCore/src/main/java/com/braintreepayments/api/BTHttpNoResponse.kt
+++ BraintreeCore/src/main/java/com/braintreepayments/api/BTHttpNoResponse.kt
@@ -2,5 +2,6 @@ package com.braintreepayments.api
 
 internal class BTHttpNoResponse : BTHttpResponseCallback {
     override fun onResult(response: BTHttpResponse?, httpError: Exception?) {
+        // No-op
     }
 }
