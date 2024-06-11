fcaa774468403cb60f72ecac368411d248c1bdcd
richherrera <ricardherrera@paypal.com>
2024-06-11 10:53:07 -0600

    Update BTHttpResponseCallback interface

--- SharedUtils/src/main/java/com/braintreepayments/api/BTHttpResponseCallback.kt
+++ SharedUtils/src/main/java/com/braintreepayments/api/BTHttpResponseCallback.kt
@@ -3,8 +3,11 @@ package com.braintreepayments.api
 import androidx.annotation.MainThread
 import androidx.annotation.RestrictTo
 
+/**
+ * @suppress
+ */
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-interface BTHttpResponseCallback {
+fun interface BTHttpResponseCallback {
     @MainThread
     fun onResult(response: BTHttpResponse?, httpError: Exception?)
 }
