0fd0c36855f7b8cb9144e2cebf05badf2c0cfa5a
Rich Herrera <ricardherrera@paypal.com>
2024-06-11 10:30:03 -0600

    Add note
    Co-authored-by: sshropshire <58225613+sshropshire@users.noreply.github.com>

--- BraintreeCore/src/main/java/com/braintreepayments/api/AnalyticsEventParams.kt
+++ BraintreeCore/src/main/java/com/braintreepayments/api/AnalyticsEventParams.kt
@@ -8,5 +8,6 @@ data class AnalyticsEventParams constructor(
     var linkType: String?,
     var isVaultRequest: Boolean,
 ) {
+  // TODO: this is a convenience constructor for Java; remove after Kotlin migration is complete
   constructor() : this(null, null, false)
 }
