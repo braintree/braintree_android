fa734e964296b2dda10156edc4ccc0d271284fc1
richherrera <ricardherrera@paypal.com>
2024-06-25 16:48:34 -0600

    Delete unnecessary classes

--- BraintreeCore/src/main/java/com/braintreepayments/api/HttpNoTimingResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.braintreepayments.api
-
-internal class HttpNoTimingResponse : HttpTimingResponseCallback {
-    override fun onResult(response: HttpResponse?, httpError: Exception?) {
-        // No-op
-    }
-}
