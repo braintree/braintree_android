81edc12dd7ea165c02392dfdaf35917e052393a7
sshropshire <sshropshire@paypal.com>
2024-07-08 10:55:59 -0500

    Add comment string.

--- BraintreeCore/src/main/java/com/braintreepayments/api/AnalyticsEventBlob.kt
+++ BraintreeCore/src/main/java/com/braintreepayments/api/AnalyticsEventBlob.kt
@@ -4,7 +4,10 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-// Ref: https://www.reddit.com/r/javascript/comments/10st04/what_is_a_json_blob/
+/**
+ * Store Analytics as a JSON string. The schema of the Analytics data is enforced JSON
+ * at the JSON level. JSON encoded events can be sent directly to the analytics server.
+ */
 @Entity(tableName = "analytics_event_blob")
 data class AnalyticsEventBlob(
     @ColumnInfo(name = "json_string") val jsonString: String
