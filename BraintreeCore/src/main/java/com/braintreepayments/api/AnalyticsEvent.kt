02e61b68db0e1bc780d0cbf652bbafc8561a22e5
sshropshire <sshropshire@paypal.com>
2024-07-08 11:21:09 -0500

    Add missing RestrictTo annotation.

--- BraintreeCore/src/main/java/com/braintreepayments/api/AnalyticsEvent.kt
+++ BraintreeCore/src/main/java/com/braintreepayments/api/AnalyticsEvent.kt
@@ -1,5 +1,8 @@
 package com.braintreepayments.api
 
+import androidx.annotation.RestrictTo
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 internal data class AnalyticsEvent constructor(
     val name: String,
     val payPalContextId: String? = null,
