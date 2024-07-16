f14dae03ff6788b45347ed121f7990f4c960e8d7
Timothy Chow <tichow@paypal.com>
2024-07-16 15:20:03 -0500

    Remove todo in passing test

--- BraintreeCore/src/test/java/com/braintreepayments/api/core/AnalyticsClientUnitTest.kt
+++ BraintreeCore/src/test/java/com/braintreepayments/api/core/AnalyticsClientUnitTest.kt
@@ -123,9 +123,7 @@ class AnalyticsClientUnitTest {
         assertEquals(authorization.toString(), workSpec.input.getString("authorization"))
         assertEquals("sample-session-id", workSpec.input.getString("sessionId"))
         assertEquals("sample-integration", workSpec.input.getString("integration"))
-
-        // TODO: Fix this test
-
+        
         // language=JSON
         val expectedJSON = """
         {
