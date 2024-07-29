703937fbcc084f28744b39ac96d924c07a6b5ebd
Timothy Chow <tichow@paypal.com>
2024-07-29 10:52:41 -0500

    Convert Nonce classes to Kotlin and implement Parcelable using the Parcelize annotation

--- BraintreeCore/src/main/java/com/braintreepayments/api/core/PaymentMethodNonce.kt
+++ BraintreeCore/src/main/java/com/braintreepayments/api/core/PaymentMethodNonce.kt
@@ -1,9 +1,6 @@
 package com.braintreepayments.api.core
 
-import android.os.Parcel
 import android.os.Parcelable
-import androidx.annotation.RestrictTo
-import androidx.annotation.VisibleForTesting
 
 /**
  * Base class representing a method of payment for a customer. [PaymentMethodNonce] represents the
@@ -14,40 +11,7 @@ import androidx.annotation.VisibleForTesting
  * actions.
  * @property isDefault `true` if this payment method is the default for the current customer, `false` otherwise
  */
-open class PaymentMethodNonce : Parcelable {
-
-    companion object {
-        @JvmField
-        val CREATOR = object : Parcelable.Creator<PaymentMethodNonce> {
-            override fun createFromParcel(`in`: Parcel) = PaymentMethodNonce(`in`)
-            override fun newArray(size: Int) = arrayOfNulls<PaymentMethodNonce>(size)
-        }
-    }
-
-    open val string: String
+abstract class PaymentMethodNonce(
+    open val string: String,
     open val isDefault: Boolean
-
-    /**
-     * @suppress
-     */
-    @VisibleForTesting
-    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-    constructor(nonce: String, isDefault: Boolean) {
-        string = nonce
-        this.isDefault = isDefault
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeString(string)
-        dest.writeByte(if (isDefault) 1.toByte() else 0.toByte())
-    }
-
-    protected constructor(inParcel: Parcel) {
-        string = inParcel.readString() ?: ""
-        isDefault = inParcel.readByte() > 0
-    }
-}
+) : Parcelable
