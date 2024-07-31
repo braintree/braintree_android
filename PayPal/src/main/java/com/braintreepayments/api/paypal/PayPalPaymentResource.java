f18bc5375397eb3497963e00e93e94d93392da90
richherrera <ricardherrera@paypal.com>
2024-07-31 09:51:52 -0600

    Map payPalApprovalURL key from response

--- PayPal/src/main/java/com/braintreepayments/api/paypal/PayPalPaymentResource.java
+++ PayPal/src/main/java/com/braintreepayments/api/paypal/PayPalPaymentResource.java
@@ -14,6 +14,7 @@ class PayPalPaymentResource {
     private static final String REDIRECT_URL_KEY = "redirectUrl";
     private static final String AGREEMENT_SETUP_KEY = "agreementSetup";
     private static final String APPROVAL_URL_KEY = "approvalUrl";
+    private static final String PAYPAL_APP_APPROVAL_URL_KEY = "paypalAppApprovalUrl";
 
     private String redirectUrl;
 
@@ -38,7 +39,7 @@ class PayPalPaymentResource {
      * @return a PayPal payment resource
      * @throws JSONException when json input is invalid
      */
-    static PayPalPaymentResource fromJson(String jsonString) throws JSONException {
+    static PayPalPaymentResource fromJson(String jsonString, String linkType) throws JSONException {
         JSONObject json = new JSONObject(jsonString);
 
         PayPalPaymentResource payPalPaymentResource = new PayPalPaymentResource();
@@ -47,7 +48,13 @@ class PayPalPaymentResource {
             payPalPaymentResource.redirectUrl(Json.optString(redirectJson, REDIRECT_URL_KEY, ""));
         } else {
             redirectJson = json.optJSONObject(AGREEMENT_SETUP_KEY);
-            payPalPaymentResource.redirectUrl(Json.optString(redirectJson, APPROVAL_URL_KEY, ""));
+            String payPalApprovalURL = Json.optString(redirectJson, PAYPAL_APP_APPROVAL_URL_KEY, "");
+            
+            if (linkType.equals("universal") && !payPalApprovalURL.isEmpty()) {
+                payPalPaymentResource.redirectUrl(payPalApprovalURL);
+            } else {
+                payPalPaymentResource.redirectUrl(Json.optString(redirectJson, APPROVAL_URL_KEY, ""));
+            }
         }
         return payPalPaymentResource;
     }
