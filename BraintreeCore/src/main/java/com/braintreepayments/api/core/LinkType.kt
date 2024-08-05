5304888f5cf041837d8d310455aacc15bf1224ea
richherrera <ricardherrera@paypal.com>
2024-08-05 11:53:21 -0600

    Linting

--- BraintreeCore/src/main/java/com/braintreepayments/api/core/LinkType.kt
+++ BraintreeCore/src/main/java/com/braintreepayments/api/core/LinkType.kt
@@ -2,7 +2,8 @@ package com.braintreepayments.api.core
 
 /**
  * Used to describe the link type for analytics
- * Note: This enum is exposed for internal Braintree use only. Do not use. It is not covered by Semantic Versioning and may change or be removed at any time.
+ * Note: This enum is exposed for internal Braintree use only. Do not use.
+ * It is not covered by Semantic Versioning and may change or be removed at any time.
  */
 public enum class LinkType(val stringValue: String)  {
     UNIVERSAL("universal"),
