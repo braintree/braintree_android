8a762142cfb7d5940f49867c7afcbb6b92ee6142
richherrera <ricardherrera@paypal.com>
2024-08-05 10:59:03 -0600

    Add linkType enum

---
+++ BraintreeCore/src/main/java/com/braintreepayments/api/core/LinkType.kt
@@ -0,0 +1,10 @@
+package com.braintreepayments.api.core
+
+/**
+ * Used to describe the link type for analytics
+ * Note: This enum is exposed for internal Braintree use only. Do not use. It is not covered by Semantic Versioning and may change or be removed at any time.
+ */
+public enum class LinkType(val stringValue: String)  {
+    UNIVERSAL("universal"),
+    DEEPLINK("deeplink")
+}
