6112f5bb346dc2ebee7a127e8ae1fe36dcf5bc1a
Timothy Chow <tichow@paypal.com>
2024-08-05 10:21:04 -0500

    Add merchantAccountId to PayPalVaultEditRequest

--- PayPal/src/main/java/com/braintreepayments/api/paypal/vaultedit/PayPalVaultEditRequest.kt
+++ PayPal/src/main/java/com/braintreepayments/api/paypal/vaultedit/PayPalVaultEditRequest.kt
@@ -3,10 +3,15 @@ package com.braintreepayments.api.paypal.vaultedit
 import com.braintreepayments.api.ExperimentalBetaApi
 
 /**
+ * Request containing details for the Edit FI flow.
  *
  * Note: **This feature is in beta. It's public API may change in future releases.**
+ *
+ * @property editPaypalVaultId PayPal vault ID to edit
+ * @property merchantAccountId optional ID of the merchant account
  */
 @ExperimentalBetaApi
 data class PayPalVaultEditRequest(
-    val editPaypalVaultId: String
+    val editPaypalVaultId: String,
+    val merchantAccountId: String? = null,
 )
