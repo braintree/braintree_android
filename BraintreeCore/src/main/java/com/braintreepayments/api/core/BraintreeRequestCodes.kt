8549b02bcd1a66efea2fbac87d7d7ee5c67fcda3
Timothy Chow <tichow@paypal.com>
2024-08-06 16:20:44 -0500

    Update BraintreeRequestCodes to an enum type

--- BraintreeCore/src/main/java/com/braintreepayments/api/core/BraintreeRequestCodes.kt
+++ BraintreeCore/src/main/java/com/braintreepayments/api/core/BraintreeRequestCodes.kt
@@ -1,28 +1,14 @@
 package com.braintreepayments.api.core
 
-import androidx.annotation.IntDef
-
 /**
  * Request codes that Braintree uses when communicating between Android Activities and Fragments.
  */
-@Retention(AnnotationRetention.SOURCE)
-@IntDef(
-    BraintreeRequestCodes.THREE_D_SECURE,
-    BraintreeRequestCodes.VENMO,
-    BraintreeRequestCodes.PAYPAL,
-    BraintreeRequestCodes.VISA_CHECKOUT,
-    BraintreeRequestCodes.GOOGLE_PAY,
-    BraintreeRequestCodes.LOCAL_PAYMENT,
-    BraintreeRequestCodes.SEPA_DEBIT
-)
-annotation class BraintreeRequestCodes {
-    companion object {
-        const val THREE_D_SECURE = 13487
-        const val VENMO = 13488
-        const val PAYPAL = 13591
-        const val VISA_CHECKOUT = 13592
-        const val GOOGLE_PAY = 13593
-        const val LOCAL_PAYMENT = 13596
-        const val SEPA_DEBIT = 13597
-    }
+enum class BraintreeRequestCodes(val code: Int) {
+    THREE_D_SECURE(13487),
+    VENMO(13488),
+    PAYPAL(13591),
+    VISA_CHECKOUT(13592),
+    GOOGLE_PAY(13593),
+    LOCAL_PAYMENT(13596),
+    SEPA_DEBIT(13597),
 }
