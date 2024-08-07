b8320a25d54b975f9b34c728ef4e8d0ddb2506ee
Tim Chow <tichow@paypal.com>
2024-08-07 14:05:49 -0500

    Add PayPalVaultEditResult.Success properties (#1098)
    * Add PayPalVaultEditResult.Success properties
    
    * Add kdocs to PayPalVaultEditResult.Success properties
    
    * Update clientMetadataId kdoc

--- PayPal/src/main/java/com/braintreepayments/api/paypal/vaultedit/PayPalVaultEditResult.kt
+++ PayPal/src/main/java/com/braintreepayments/api/paypal/vaultedit/PayPalVaultEditResult.kt
@@ -1,22 +1,39 @@
 package com.braintreepayments.api.paypal.vaultedit
 
 import com.braintreepayments.api.ExperimentalBetaApi
+import com.braintreepayments.api.core.PostalAddress
 
 @ExperimentalBetaApi
 sealed class PayPalVaultEditResult {
 
     /**
      * The PayPal vault edit flow completed successfully.
+     *
+     * @property clientMetadataId This ID is used to link subsequent retry attempts if payment is
+     * declined.
+     * @property payerId ID of the payer
+     * @property email email address of the payer
+     * @property firstName first name of the payer
+     * @property lastName last name of the payer
+     * @property phone phone number of the payer
+     * @property shippingAddress shipping address of the payer
+     * @property fundingSourceDescription description of the funding source
      */
-    @Suppress("EmptyDefaultConstructor")
-    class Success(
-        // TODO: add FI details
+    class Success internal constructor(
+        val clientMetadataId: String,
+        val payerId: String?,
+        val email: String?,
+        val firstName: String?,
+        val lastName: String?,
+        val phone: String?,
+        val shippingAddress: PostalAddress?,
+        val fundingSourceDescription: String?
     ) : PayPalVaultEditResult()
 
     /**
      * There was an [error] in the PayPal vault edit flow.
      */
-    class Failure(val error: Exception) : PayPalVaultEditResult()
+    class Failure internal constructor(val error: Exception) : PayPalVaultEditResult()
 
     /**
      * The user canceled the PayPal vault edit flow.
