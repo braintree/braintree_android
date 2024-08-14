0da2c762572407b62211dd8f8e7e70159e18b7b4
Justin Warmkessel <Jwarmkessel@paypal.com>
2024-08-14 08:39:02 -1000

    Update PR comments. Remove the alert event and add a comment.

--- PayPal/src/main/java/com/braintreepayments/api/paypal/PayPalAnalytics.kt
+++ PayPal/src/main/java/com/braintreepayments/api/paypal/PayPalAnalytics.kt
@@ -8,10 +8,10 @@ internal object PayPalAnalytics {
     const val TOKENIZATION_SUCCEEDED = "paypal:tokenize:succeeded"
     const val BROWSER_LOGIN_CANCELED = "paypal:tokenize:browser-login:canceled"
 
+    // Edit FI Events
     const val EDITFI_STARTED = "paypal:edit:started"
     const val EDITFI_BROWSER_PRESENTATION_SUCCEEDED = "paypal:edit:browser-presentation:succeeded"
     const val EDITFI_BROWSER_PRESENTATION_FAILED = "paypal:edit:browser-presentation:failed"
-    const val EDITFI_BROWSER_LOGIN_ALERT_CANCELED = "paypal:edit:browser-login:alert-canceled"
     const val EDITFI_BROWSER_LOGIN_CANCELED = "paypal:edit:browser-login:canceled"
     const val EDITFI_FAILED = "paypal:edit:failed"
     const val EDITFI_SUCCEEDED = "paypal:edit:succeeded"
