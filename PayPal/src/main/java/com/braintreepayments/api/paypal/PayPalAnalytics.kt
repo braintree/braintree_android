9445ea8b08a0bc6c79e866222fefad93be221455
warmkesselj <150195168+warmkesselj@users.noreply.github.com>
2024-08-15 08:06:31 -1000

    EDITFI analytics names (#1113)
    * EDITFI analytics names
    
    * Update PR comments. Remove the alert event and add a comment.
    
    * Update const naming for readability

--- PayPal/src/main/java/com/braintreepayments/api/paypal/PayPalAnalytics.kt
+++ PayPal/src/main/java/com/braintreepayments/api/paypal/PayPalAnalytics.kt
@@ -7,4 +7,12 @@ internal object PayPalAnalytics {
     const val TOKENIZATION_FAILED = "paypal:tokenize:failed"
     const val TOKENIZATION_SUCCEEDED = "paypal:tokenize:succeeded"
     const val BROWSER_LOGIN_CANCELED = "paypal:tokenize:browser-login:canceled"
+
+    // Edit FI Events
+    const val EDIT_FI_STARTED = "paypal:edit:started"
+    const val EDIT_FI_BROWSER_PRESENTATION_SUCCEEDED = "paypal:edit:browser-presentation:succeeded"
+    const val EDIT_FI_BROWSER_PRESENTATION_FAILED = "paypal:edit:browser-presentation:failed"
+    const val EDIT_FI_BROWSER_LOGIN_CANCELED = "paypal:edit:browser-login:canceled"
+    const val EDIT_FI_FAILED = "paypal:edit:failed"
+    const val EDIT_FI_SUCCEEDED = "paypal:edit:succeeded"
 }
