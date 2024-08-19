dcd7931ebb840d8fbe2a7f29137447cb9bd554e9
Archana Shivashankar <ashivashankar@paypal.com>
2024-08-19 15:13:13 -0700

    LocalPaymentApi class to Kotlin

---
+++ LocalPayment/src/main/java/com/braintreepayments/api/localpayment/LocalPaymentApi.kt
@@ -0,0 +1,85 @@
+package com.braintreepayments.api.localpayment
+
+import com.braintreepayments.api.core.BraintreeClient
+import com.braintreepayments.api.localpayment.LocalPaymentNonce.Companion.fromJSON
+import com.braintreepayments.api.sharedutils.HttpResponseCallback
+import org.json.JSONException
+import org.json.JSONObject
+
+internal class LocalPaymentApi(private val braintreeClient: BraintreeClient) {
+    fun createPaymentMethod(
+        request: LocalPaymentRequest,
+        callback: LocalPaymentInternalAuthRequestCallback
+    ) {
+        val returnUrl =
+            braintreeClient.getReturnUrlScheme() + "://" + LocalPaymentClient.LOCAL_PAYMENT_SUCCESS
+        val cancel =
+            braintreeClient.getReturnUrlScheme() + "://" + LocalPaymentClient.LOCAL_PAYMENT_CANCEL
+
+        val url = "/v1/local_payments/create"
+        braintreeClient.sendPOST(url = url, data = request.build(returnUrl, cancel),
+            responseCallback = HttpResponseCallback { responseBody: String?, httpError: Exception? ->
+                if (responseBody != null) {
+                    try {
+                        val responseJson = JSONObject(responseBody)
+                        val redirectUrl = responseJson.getJSONObject("paymentResource")
+                            .getString("redirectUrl")
+                        val paymentToken = responseJson.getJSONObject("paymentResource")
+                            .getString("paymentToken")
+
+                        val transaction =
+                            LocalPaymentAuthRequestParams(request, redirectUrl, paymentToken)
+                        callback.onLocalPaymentInternalAuthResult(transaction, null)
+                    } catch (e: JSONException) {
+                        callback.onLocalPaymentInternalAuthResult(null, e)
+                    }
+                } else {
+                    callback.onLocalPaymentInternalAuthResult(null, httpError)
+                }
+            })
+    }
+
+    fun tokenize(
+        merchantAccountId: String?, responseString: String?, clientMetadataID: String?,
+        callback: LocalPaymentInternalTokenizeCallback
+    ) {
+        val payload = JSONObject()
+
+        try {
+            payload.put("merchant_account_id", merchantAccountId)
+
+            val paypalAccount = JSONObject()
+                .put("intent", "sale")
+                .put("response", JSONObject().put("webURL", responseString))
+                .put("options", JSONObject().put("validate", false))
+                .put("response_type", "web")
+                .put("correlation_id", clientMetadataID)
+            payload.put("paypal_account", paypalAccount)
+
+            val metaData = JSONObject()
+                .put("source", "client")
+                .put("integration", braintreeClient.integrationType.stringValue)
+                .put("sessionId", braintreeClient.sessionId)
+            payload.put("_meta", metaData)
+
+            val url = "/v1/payment_methods/paypal_accounts"
+            braintreeClient.sendPOST(
+                url = url,
+                data = payload.toString(),
+                responseCallback = HttpResponseCallback { responseBody: String?, httpError: Exception? ->
+                    if (responseBody != null) {
+                        try {
+                            val result =
+                                fromJSON(JSONObject(responseBody))
+                            callback.onResult(result, null)
+                        } catch (jsonException: JSONException) {
+                            callback.onResult(null, jsonException)
+                        }
+                    } else {
+                        callback.onResult(null, httpError)
+                    }
+                })
+        } catch (ignored: JSONException) { /* do nothing */
+        }
+    }
+}
