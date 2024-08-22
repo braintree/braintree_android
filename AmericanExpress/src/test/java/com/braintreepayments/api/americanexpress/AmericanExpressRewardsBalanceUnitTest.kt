f330e3c145d6135782f01f2d0cd2ad9f71dad181
Archana Shivashankar <ashivashankar@paypal.com>
2024-08-21 19:49:47 -0700

    AmericanExpressRewardsBalanceUnitTest class to Kotlin

---
+++ AmericanExpress/src/test/java/com/braintreepayments/api/americanexpress/AmericanExpressRewardsBalanceUnitTest.kt
@@ -0,0 +1,86 @@
+package com.braintreepayments.api.americanexpress
+
+import android.os.Parcel
+import com.braintreepayments.api.americanexpress.AmericanExpressRewardsBalance.Companion.fromJson
+import com.braintreepayments.api.testutils.Fixtures
+import org.json.JSONException
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class AmericanExpressRewardsBalanceUnitTest {
+    @Test
+    @Throws(JSONException::class)
+    fun fromJson_parsesResponse() {
+        val rewardsBalance = fromJson(Fixtures.AMEX_REWARDS_BALANCE_SUCCESS)
+
+        assertEquals("0.0070", rewardsBalance.conversionRate)
+        assertEquals("316795.03", rewardsBalance.currencyAmount)
+        assertEquals("USD", rewardsBalance.currencyIsoCode)
+        assertEquals("715f4712-8690-49ed-8cc5-d7fb1c2d", rewardsBalance.requestId)
+        assertEquals("45256433", rewardsBalance.rewardsAmount)
+        assertEquals("Points", rewardsBalance.rewardsUnit)
+        assertNull(rewardsBalance.errorMessage)
+        assertNull(rewardsBalance.errorCode)
+    }
+
+    @Test
+    @Throws(JSONException::class)
+    fun fromJson_parsesResponseForIneligibleCard() {
+        val rewardsBalance = fromJson(Fixtures.AMEX_REWARDS_BALANCE_INELIGIBLE_CARD)
+
+        assertNull(rewardsBalance.conversionRate)
+        assertNull(rewardsBalance.currencyAmount)
+        assertNull(rewardsBalance.currencyIsoCode)
+        assertNull(rewardsBalance.requestId)
+        assertNull(rewardsBalance.rewardsAmount)
+        assertNull(rewardsBalance.rewardsUnit)
+        assertEquals("Card is ineligible", rewardsBalance.errorMessage)
+        assertEquals("INQ2002", rewardsBalance.errorCode)
+    }
+
+    @Test
+    @Throws(JSONException::class)
+    fun parcelsCorrectly() {
+        val rewardsBalanceFromJson = fromJson(Fixtures.AMEX_REWARDS_BALANCE_SUCCESS)
+
+        val parcel = Parcel.obtain()
+        rewardsBalanceFromJson.writeToParcel(parcel, 0)
+        parcel.setDataPosition(0)
+        val rewardsBalance =
+            AmericanExpressRewardsBalance.CREATOR.createFromParcel(parcel)!!
+
+        assertEquals("0.0070", rewardsBalance.conversionRate)
+        assertEquals("316795.03", rewardsBalance.currencyAmount)
+        assertEquals("USD", rewardsBalance.currencyIsoCode)
+        assertEquals("715f4712-8690-49ed-8cc5-d7fb1c2d", rewardsBalance.requestId)
+        assertEquals("45256433", rewardsBalance.rewardsAmount)
+        assertEquals("Points", rewardsBalance.rewardsUnit)
+        assertNull(rewardsBalance.errorMessage)
+        assertNull(rewardsBalance.errorCode)
+    }
+
+    @Test
+    @Throws(JSONException::class)
+    fun parcelsCorrectly_forErrorResponse() {
+        val rewardsBalanceFromJson = fromJson(Fixtures.AMEX_REWARDS_BALANCE_INSUFFICIENT_POINTS)
+
+        val parcel = Parcel.obtain()
+        rewardsBalanceFromJson.writeToParcel(parcel, 0)
+        parcel.setDataPosition(0)
+        val rewardsBalance =
+            AmericanExpressRewardsBalance.CREATOR.createFromParcel(parcel)!!
+
+        assertNull(rewardsBalance.conversionRate)
+        assertNull(rewardsBalance.currencyAmount)
+        assertNull(rewardsBalance.currencyIsoCode)
+        assertNull(rewardsBalance.requestId)
+        assertNull(rewardsBalance.rewardsAmount)
+        assertNull(rewardsBalance.rewardsUnit)
+        assertEquals("Insufficient points on card", rewardsBalance.errorMessage)
+        assertEquals("INQ2003", rewardsBalance.errorCode)
+    }
+}
