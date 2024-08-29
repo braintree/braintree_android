9161f4ceba236cfdb2103a75f0619a28fea946ff
Archana Shivashankar <ashivashankar@paypal.com>
2024-08-29 00:24:52 -0700

    VenmoPaymentAuthResultInfo class to Kotlin

---
+++ Venmo/src/main/java/com/braintreepayments/api/venmo/VenmoPaymentAuthResultInfo.kt
@@ -0,0 +1,12 @@
+package com.braintreepayments.api.venmo
+
+import com.braintreepayments.api.BrowserSwitchFinalResult
+
+/**
+ * Details of a [VenmoPaymentAuthResult.Success]
+ */
+class VenmoPaymentAuthResultInfo internal constructor(private val browserSwitchSuccess: BrowserSwitchFinalResult.Success) {
+
+    val browserSwitchResultInfo: BrowserSwitchFinalResult.Success
+        get() = browserSwitchSuccess
+}
