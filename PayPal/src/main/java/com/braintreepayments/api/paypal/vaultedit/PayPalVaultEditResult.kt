c240c7e7d700eb7256f342f3261c3085bb306431
Justin Warmkessel <Jwarmkessel@paypal.com>
2024-09-09 10:54:58 -0700

    Update the PayPalVaultEditResult to just return the correlationId

--- PayPal/src/main/java/com/braintreepayments/api/paypal/vaultedit/PayPalVaultEditResult.kt
+++ PayPal/src/main/java/com/braintreepayments/api/paypal/vaultedit/PayPalVaultEditResult.kt
@@ -1,7 +1,6 @@
 package com.braintreepayments.api.paypal.vaultedit
 
 import com.braintreepayments.api.ExperimentalBetaApi
-import com.braintreepayments.api.core.PostalAddress
 
 @ExperimentalBetaApi
 sealed class PayPalVaultEditResult {
@@ -9,34 +8,24 @@ sealed class PayPalVaultEditResult {
     /**
      * The PayPal vault edit flow completed successfully.
      *
-     * @property clientMetadataId This ID is used to link subsequent retry attempts if payment is
-     * declined.
-     * @property payerId ID of the payer
-     * @property email email address of the payer
-     * @property firstName first name of the payer
-     * @property lastName last name of the payer
-     * @property phone phone number of the payer
-     * @property shippingAddress shipping address of the payer
-     * @property fundingSourceDescription description of the funding source
+     * @property riskCorrelationId This ID is used to link subsequent retry attempts if payment is declined
      */
     class Success internal constructor(
-        val clientMetadataId: String,
-        val payerId: String?,
-        val email: String?,
-        val firstName: String?,
-        val lastName: String?,
-        val phone: String?,
-        val shippingAddress: PostalAddress?,
-        val fundingSourceDescription: String?
+        val riskCorrelationId: String,
     ) : PayPalVaultEditResult()
 
     /**
      * There was an [error] in the PayPal vault edit flow.
      */
-    class Failure internal constructor(val error: Exception) : PayPalVaultEditResult()
+    class Failure internal constructor(
+        val riskCorrelationId: String,
+        val error: Exception
+    ) : PayPalVaultEditResult()
 
     /**
      * The user canceled the PayPal vault edit flow.
      */
-    object Cancel : PayPalVaultEditResult()
+    class Cancel internal constructor(
+        val riskCorrelationIdZ: String
+    ) : PayPalVaultEditResult()
 }
