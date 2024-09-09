660a6d21b5fd8c434911e22f41e30cc22ade2a76
Tim Chow <tichow@paypal.com>
2024-09-09 11:37:29 -0500

    Update the demo app to Retrofit 2.11.0 (#1143)

--- Demo/src/main/java/com/braintreepayments/demo/internal/ApiClientRequestInterceptor.java
+++ Demo/src/main/java/com/braintreepayments/demo/internal/ApiClientRequestInterceptor.java
@@ -1,14 +1,27 @@
 package com.braintreepayments.demo.internal;
 
+import androidx.annotation.NonNull;
+
 import com.braintreepayments.demo.BuildConfig;
 
-import retrofit.RequestInterceptor;
+import java.io.IOException;
 
-public class ApiClientRequestInterceptor implements RequestInterceptor {
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
 
+public class ApiClientRequestInterceptor implements Interceptor {
+
+    @NonNull
     @Override
-    public void intercept(RequestFacade request) {
-        request.addHeader("User-Agent", "braintree/android-demo-app/" + BuildConfig.VERSION_NAME);
-        request.addHeader("Accept", "application/json");
+    public Response intercept(@NonNull Chain chain) throws IOException {
+        Request request = chain.request();
+        Request modifiedRequest;
+
+        modifiedRequest = request.newBuilder()
+            .addHeader("User-Agent", "braintree/android-demo-app/" + BuildConfig.VERSION_NAME)
+            .addHeader("Accept", "application/json")
+            .build();
+        return chain.proceed(modifiedRequest);
     }
 }
