ec85b9e328513eafd02c5a02626d30867e2d97f4
Sai <saperi@paypal.com>
2024-09-09 13:23:50 -0500

    Add gpg file and decryption script

---
+++ .github/executables/decrypt_secret.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Decrypt the file
+mkdir $HOME/secrets
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output $HOME/secrets/braintree_demo_app.json \
+.github/files/braintree-demo-app-7789ca8ae9e9.json.gpg
