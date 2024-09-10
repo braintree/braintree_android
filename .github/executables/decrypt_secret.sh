6fdd6184595f4550030bcaf36f63a487a6ead24d
Sai <saperi@paypal.com>
2024-09-10 12:26:09 -0500

    fix script

--- .github/executables/decrypt_secret.sh
+++ .github/executables/decrypt_secret.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
 # Get arguments
-while getopts passphrase:input:output: flag
+while getopts p:i:o: flag
 do
     case "${flag}" in
-        passphrase) passphrase=${OPTARG};;
-        input) input=${OPTARG};;
-        output) output=${OPTARG};;
+        p) passphrase=${OPTARG};;
+        i) input=${OPTARG};;
+        o) output=${OPTARG};;
     esac
 done
 echo "Passphrase: $passphrase";
 echo "Input: $input";
 echo "Output: $output";
 
-# Decrypt the file
-mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$passphrase" \
