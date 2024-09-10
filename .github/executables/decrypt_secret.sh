f43e9f311f8386074ecfe687b778d0c4eb5b13e5
Sai <saperi@paypal.com>
2024-09-10 12:13:33 -0500

    modify script

--- .github/executables/decrypt_secret.sh
+++ .github/executables/decrypt_secret.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
+# Get arguments
+while getopts passphrase:input:output: flag
+do
+    case "${flag}" in
+        passphrase) passphrase=${OPTARG};;
+        input) input=${OPTARG};;
+        output) output=${OPTARG};;
+    esac
+done
+echo "Passphrase: $passphrase";
+echo "Input: $input";
+echo "Output: $output";
+
 # Decrypt the file
 mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/braintree_demo_app-eb501d54ba5f.json \
-./.github/files/braintree-demo-app-eb501d54ba5f.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$passphrase" \
+--output "$output" "$input" \
+&& cat "$output"
