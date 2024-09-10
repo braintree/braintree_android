16aad127a9dfdc1429fb364044951934643e3090
Sai <saperi@paypal.com>
2024-09-09 23:46:38 -0500

    updating service account and related files

--- .github/executables/decrypt_secret.sh
+++ .github/executables/decrypt_secret.sh
@@ -5,5 +5,5 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/braintree_demo_app.json \
-./.github/files/braintree-demo-app-7789ca8ae9e9.json.gpg
+--output $HOME/secrets/braintree_demo_app-eb501d54ba5f.json \
+./.github/files/braintree-demo-app-eb501d54ba5f.json.gpg
