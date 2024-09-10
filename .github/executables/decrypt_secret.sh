fe14e74845a147b4586c909ef6a16b22f6c700da
Sai <saperi@paypal.com>
2024-09-10 12:47:25 -0500

    modify script

--- .github/executables/decrypt_secret.sh
+++ .github/executables/decrypt_secret.sh
@@ -9,12 +9,7 @@ do
         o) output=${OPTARG};;
     esac
 done
-echo "Passphrase: $passphrase";
-echo "Input: $input";
-echo "Output: $output";
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$passphrase" \
---output "$output" "$input" \
-&& cat "$output"
+gpg --quiet --batch --yes --decrypt --passphrase="$passphrase" --output "$output" "$input"
