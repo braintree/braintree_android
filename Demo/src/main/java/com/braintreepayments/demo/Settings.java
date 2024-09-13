1d039109f8abd20b3fc3b9a37269367d5af6fbf2
richherrera <ricardherrera@paypal.com>
2024-09-13 14:17:58 -0600

    Change token key, use stage instead of sandbox

--- Demo/src/main/java/com/braintreepayments/demo/Settings.java
+++ Demo/src/main/java/com/braintreepayments/demo/Settings.java
@@ -21,7 +21,8 @@ public class Settings {
     private static final String PRODUCTION_BASE_SERVER_URL = MERCHANT_SERVER_URL + "/braintree/production/";
     private static final String MOCKED_PAY_PAL_SANDBOX_SERVER_URL = MERCHANT_SERVER_URL + "/braintree/mock_pay_pal/";
 
-    private static final String SANDBOX_TOKENIZATION_KEY = "sandbox_tmxhyf7d_dcpspy2brwdjr3qn";
+    //private static final String SANDBOX_TOKENIZATION_KEY = "sandbox_tmxhyf7d_dcpspy2brwdjr3qn";
+    private static final String SANDBOX_TOKENIZATION_KEY = "sandbox_jy4fvpfg_v7x2rb226dx4pr7b";
     private static final String PRODUCTION_TOKENIZATION_KEY = "production_t2wns2y2_dfy45jdj3dxkmz5m";
     private static final String MOCKED_PAY_PAL_SANDBOX_TOKENIZATION_KEY = "sandbox_q7v35n9n_555d2htrfsnnmfb3";
 
