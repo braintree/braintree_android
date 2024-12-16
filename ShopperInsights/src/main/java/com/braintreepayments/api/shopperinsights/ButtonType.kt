d7bfdd75d69d33e94820d92b6442e5bf90d5fe43
Justin Warmkessel <Jwarmkessel@paypal.com>
2024-12-16 12:24:19 -0800

    Update comments for button type

--- ShopperInsights/src/main/java/com/braintreepayments/api/shopperinsights/ButtonType.kt
+++ ShopperInsights/src/main/java/com/braintreepayments/api/shopperinsights/ButtonType.kt
@@ -4,7 +4,18 @@ package com.braintreepayments.api.shopperinsights
 The button type to be displayed or presented
  */
 enum class ButtonType(internal val stringValue: String) {
+    /**
+     * PayPal button
+     */
     PAYPAL("paypal"),
+
+    /**
+     * Venmo button
+     */
     VENMO("venmo"),
+
+    /**
+     * Other button
+     */
     OTHER("other"),
 }
