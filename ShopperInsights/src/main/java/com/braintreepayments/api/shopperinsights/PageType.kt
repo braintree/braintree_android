3cbe0c52da25ecd61c0453f98898b79686e2fbd5
Justin Warmkessel <Jwarmkessel@paypal.com>
2024-12-16 12:27:28 -0800

    Update comments

--- ShopperInsights/src/main/java/com/braintreepayments/api/shopperinsights/PageType.kt
+++ ShopperInsights/src/main/java/com/braintreepayments/api/shopperinsights/PageType.kt
@@ -1,15 +1,61 @@
 package com.braintreepayments.api.shopperinsights
 
+/**
+ * The page or view that a button is displayed on.
+ */
 enum class PageType {
+    /**
+     * The homepage
+     */
     HOMEPAGE,
+
+    /**
+     * The about page
+     */
     ABOUT,
+
+    /**
+     * The contact page
+     */
     CONTACT,
+
+    /**
+     * A product category page
+     */
     PRODUCT_CATEGORY,
+
+    /**
+     * The product details page
+     */
     PRODUCT_DETAILS,
+
+    /**
+     * The search page
+     */
     SEARCH,
+
+    /**
+     * The checkout page
+     */
     CHECKOUT,
+
+    /**
+     * The order review page
+     */
     ORDER_REVIEW,
+
+    /**
+     * The order confirmation page
+     */
     ORDER_CONFIRMATION,
-    OTHER,
-    MINI_CART
+
+    /**
+     * The mini cart
+     */
+    MINI_CART,
+
+    /**
+     * Some other page
+     */
+    OTHER
 }
