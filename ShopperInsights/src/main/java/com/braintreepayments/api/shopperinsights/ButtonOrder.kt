a633a706fe4258063fb9b07aa5f0a172fdb65922
Justin Warmkessel <Jwarmkessel@paypal.com>
2024-12-16 12:22:43 -0800

    Update comments button order

--- ShopperInsights/src/main/java/com/braintreepayments/api/shopperinsights/ButtonOrder.kt
+++ ShopperInsights/src/main/java/com/braintreepayments/api/shopperinsights/ButtonOrder.kt
@@ -1,9 +1,52 @@
 package com.braintreepayments.api.shopperinsights
 
+/**
+ * The order or ranking in which payment buttons appear.
+ */
 enum class ButtonOrder {
+
+    /**
+     * First place
+     */
     FIRST,
+
+    /**
+     * Second place
+     */
     SECOND,
+
+    /**
+     * Third place
+     */
     THIRD,
+
+    /**
+     * Fourth place
+     */
     FOURTH,
+
+    /**
+     * Fifth place
+     */
+    FIFTH,
+
+    /**
+     * Sixth place
+     */
+    SIXTH,
+
+    /**
+     * Seventh place
+     */
+    SEVENTH,
+
+    /**
+     * Eighth place
+     */
+    Eights,
+
+    /**
+     * Greater than Eighth place
+     */
     OTHER
 }
