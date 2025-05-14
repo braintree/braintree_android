b5b262ef4eb1a6c7aba52e42bbdbd5e82f1479db
Timothy Chow <tichow@paypal.com>
2025-05-14 14:33:02 -0500

    Add CustomerRecommendationsResult and PaymentOptions

--- ShopperInsights/src/main/java/com/braintreepayments/api/shopperinsights/v2/CustomerSessionRequest.kt
+++ ShopperInsights/src/main/java/com/braintreepayments/api/shopperinsights/v2/CustomerSessionRequest.kt
@@ -10,6 +10,8 @@ import com.braintreepayments.api.core.ExperimentalBetaApi
  * @property payPalAppInstalled If the PayPal app is installed on the device.
  * @property venmoAppInstalled If the Venmo app is installed on the device.
  * @property purchaseUnits List of purchase units containing the amount and currency code.
+ *
+ * Warning: This feature is in beta. It's public API may change or be removed in future releases.
  */
 @ExperimentalBetaApi
 data class CustomerSessionRequest(
