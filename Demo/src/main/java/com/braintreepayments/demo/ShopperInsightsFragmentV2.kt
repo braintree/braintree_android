5327f26b5a034dae46481afba70ef486a6b180ca
Sai <saperi@paypal.com>
2025-07-21 13:47:08 -0700

    modify detekt-config.yml

--- Demo/src/main/java/com/braintreepayments/demo/ShopperInsightsFragmentV2.kt
+++ Demo/src/main/java/com/braintreepayments/demo/ShopperInsightsFragmentV2.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -77,78 +78,78 @@ class ShopperInsightsFragmentV2 : BaseFragment() {
             "com.braintreepayments.demo.braintree"
         )
 
-        return view()
-    }
-
-    @Suppress("LongMethod")
-    private fun view(): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                Column(modifier = Modifier.padding(8.dp)) {
-                    var emailText by rememberSaveable { mutableStateOf("PR1_merchantname@personal.example.com") }
-                    var countryCodeText by rememberSaveable { mutableStateOf("1") }
-                    var nationalNumberText by rememberSaveable { mutableStateOf("4082321001") }
-                    TextField(
-                        value = emailText,
-                        onValueChange = { newValue -> emailText = newValue },
-                        label = { Text("Email") },
-                        modifier = Modifier.padding(4.dp),
-                    )
-                    Row {
-                        TextField(
-                            value = countryCodeText,
-                            onValueChange = { newValue -> countryCodeText = newValue },
-                            label = { Text("Country code") },
-                            modifier = Modifier
-                                .padding(4.dp)
-                                .weight(1f)
-                        )
-                        TextField(
-                            value = nationalNumberText,
-                            onValueChange = { newValue -> nationalNumberText = newValue },
-                            label = { Text("National Number") },
-                            modifier = Modifier
-                                .padding(4.dp)
-                                .weight(2f)
-                        )
-                    }
+                MainContent()
+            }
+        }
+    }
 
-                    Button(
-                        enabled = shopperInsightsClientSuccessfullyInstantiated,
-                        onClick = { handleCreateCustomerSession(emailText, countryCodeText, nationalNumberText) }
-                    ) {
-                        Text(text = "Create customer session")
-                    }
-                    Button(
-                        enabled = shopperInsightsClientSuccessfullyInstantiated,
-                        onClick = { handleUpdateCustomerSession(emailText, countryCodeText, nationalNumberText) }
-                    ) {
-                        Text(text = "Update customer session")
-                    }
-                    Button(
-                        enabled = shopperInsightsClientSuccessfullyInstantiated,
-                        onClick = { handleGetRecommendations(viewModel.sessionId.value) }
-                    ) {
-                        Text(text = "Get recommendations")
-                    }
-                    val sessionId = viewModel.sessionId.collectAsState().value
-                    Text(if (sessionId.isNotEmpty()) "Session Id = $sessionId" else "")
-                    val recommendations = viewModel.recommendations.collectAsState().value
-                    Text(if (recommendations.isNotEmpty()) "Recommendations = $recommendations" else "")
-                    val isInPayPalNetwork = viewModel.isInPayPalNetwork.collectAsState().value
-                    Button(
-                        enabled = isInPayPalNetwork && recommendations.first().paymentOption == "PAYPAL",
-                        onClick = { launchPayPalVault(emailText, countryCodeText, nationalNumberText, sessionId) }
-                    ) {
-                        Text(text = "PayPal")
-                    }
-                    Button(
-                        enabled = isInPayPalNetwork && recommendations.first().paymentOption == "VENMO",
-                        onClick = { launchVenmo(emailText, countryCodeText, nationalNumberText, sessionId) }
-                    ) {
-                        Text(text = "Venmo")
-                    }
-                }
+    @Composable
+    private fun MainContent() {
+        Column(modifier = Modifier.padding(8.dp)) {
+            var emailText by rememberSaveable { mutableStateOf("PR1_merchantname@personal.example.com") }
+            var countryCodeText by rememberSaveable { mutableStateOf("1") }
+            var nationalNumberText by rememberSaveable { mutableStateOf("4082321001") }
+            TextField(
+                value = emailText,
+                onValueChange = { newValue -> emailText = newValue },
+                label = { Text("Email") },
+                modifier = Modifier.padding(4.dp),
+            )
+            Row {
+                TextField(
+                    value = countryCodeText,
+                    onValueChange = { newValue -> countryCodeText = newValue },
+                    label = { Text("Country code") },
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .weight(1f)
+                )
+                TextField(
+                    value = nationalNumberText,
+                    onValueChange = { newValue -> nationalNumberText = newValue },
+                    label = { Text("National Number") },
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .weight(2f)
+                )
+            }
+
+            Button(
+                enabled = shopperInsightsClientSuccessfullyInstantiated,
+                onClick = { handleCreateCustomerSession(emailText, countryCodeText, nationalNumberText) }
+            ) {
+                Text(text = "Create customer session")
+            }
+            Button(
+                enabled = shopperInsightsClientSuccessfullyInstantiated,
+                onClick = { handleUpdateCustomerSession(emailText, countryCodeText, nationalNumberText) }
+            ) {
+                Text(text = "Update customer session")
+            }
+            Button(
+                enabled = shopperInsightsClientSuccessfullyInstantiated,
+                onClick = { handleGetRecommendations(viewModel.sessionId.value) }
+            ) {
+                Text(text = "Get recommendations")
+            }
+            val sessionId = viewModel.sessionId.collectAsState().value
+            Text(if (sessionId.isNotEmpty()) "Session Id = $sessionId" else "")
+            val recommendations = viewModel.recommendations.collectAsState().value
+            Text(if (recommendations.isNotEmpty()) "Recommendations = $recommendations" else "")
+            val isInPayPalNetwork = viewModel.isInPayPalNetwork.collectAsState().value
+            Button(
+                enabled = isInPayPalNetwork && recommendations.first().paymentOption == "PAYPAL",
+                onClick = { launchPayPalVault(emailText, countryCodeText, nationalNumberText, sessionId) }
+            ) {
+                Text(text = "PayPal")
+            }
+            Button(
+                enabled = isInPayPalNetwork && recommendations.first().paymentOption == "VENMO",
+                onClick = { launchVenmo(emailText, countryCodeText, nationalNumberText, sessionId) }
+            ) {
+                Text(text = "Venmo")
             }
         }
     }
