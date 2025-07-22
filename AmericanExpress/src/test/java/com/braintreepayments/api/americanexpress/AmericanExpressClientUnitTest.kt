40f46efeb93b0a0059449ec4485604d8afdb4375
anoguier <anoguier@paypal.com>
2025-07-22 17:04:48 -0500

    Convert test and add functionality to MockkBraintreeClientBuilder

--- AmericanExpress/src/test/java/com/braintreepayments/api/americanexpress/AmericanExpressClientUnitTest.kt
+++ AmericanExpress/src/test/java/com/braintreepayments/api/americanexpress/AmericanExpressClientUnitTest.kt
@@ -2,14 +2,19 @@ package com.braintreepayments.api.americanexpress
 
 import com.braintreepayments.api.core.BraintreeClient
 import com.braintreepayments.api.sharedutils.HttpResponseCallback
-import org.junit.Before
-import org.junit.runner.RunWith
-import org.robolectric.RobolectricTestRunner
+import com.braintreepayments.api.testutils.Fixtures
+import com.braintreepayments.api.testutils.MockkBraintreeClientBuilder
 import io.mockk.mockk
 import io.mockk.slot
 import io.mockk.verify
+import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 @RunWith(RobolectricTestRunner::class)
 class AmericanExpressClientUnitTest {
@@ -35,5 +40,30 @@ class AmericanExpressClientUnitTest {
             "/v1/payment_methods/amex_rewards_balance?paymentMethodNonce=fake-nonce&currencyIsoCode=USD",
             url
         )
+    }
+
+    @Test
+    fun getRewardsBalance_callsListenerWithRewardsBalanceOnSuccess() {
+        val braintreeClient:BraintreeClient = MockkBraintreeClientBuilder().sendGETSuccessfulResponse(
+            Fixtures.AMEX_REWARDS_BALANCE_SUCCESS).build()
+
+        val sut = AmericanExpressClient(braintreeClient)
+        sut.getRewardsBalance("fake-nonce", "USD", amexRewardsCallback)
+        val amexRewardsSlot = slot<AmericanExpressResult>()
+        verify {  amexRewardsCallback.onAmericanExpressResult(capture(amexRewardsSlot)) }
+
+        val result = amexRewardsSlot.captured
+        assertTrue { result is AmericanExpressResult.Success }
+
+        val rewardsBalance = (result as AmericanExpressResult.Success).rewardsBalance
+        assertNotNull(rewardsBalance)
+        assertEquals("0.0070", rewardsBalance.conversionRate)
+        assertEquals("316795.03", rewardsBalance.currencyAmount)
+        assertEquals("USD", rewardsBalance.currencyIsoCode)
+        assertEquals("715f4712-8690-49ed-8cc5-d7fb1c2d", rewardsBalance.requestId)
+        assertEquals("45256433", rewardsBalance.rewardsAmount)
+        assertEquals("Points", rewardsBalance.rewardsUnit)
+        assertNull(rewardsBalance.errorCode)
+        assertNull(rewardsBalance.errorMessage)
     }
 }
