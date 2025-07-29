51564c2f3cd925c3a1b1260bb5ff80054b5e2fae
anoguier <anoguier@paypal.com>
2025-07-29 15:54:12 -0500

    Convert `AuthenticationInsightUnitTest` from Java to Kotlin

--- Card/src/test/java/com/braintreepayments/api/card/AuthenticationInsightUnitTest.kt
+++ Card/src/test/java/com/braintreepayments/api/card/AuthenticationInsightUnitTest.kt
@@ -1,63 +1,68 @@
-package com.braintreepayments.api.card;
+package com.braintreepayments.api.card
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNull;
+import android.os.Parcel
+import kotlinx.parcelize.parcelableCreator
+import org.json.JSONException
+import org.json.JSONObject
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
-import android.os.Parcel;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
-
-@RunWith(RobolectricTestRunner.class)
-public class AuthenticationInsightUnitTest {
+@RunWith(RobolectricTestRunner::class)
+class AuthenticationInsightUnitTest {
     @Test
-    public void fromJson_successfullyParsesPSDTWO() throws JSONException {
-        JSONObject response = new JSONObject()
-                .put("customerAuthenticationRegulationEnvironment", "psdtwo");
+    @Throws(JSONException::class)
+    fun `parses PSDTWO regulation environment from JSON`() {
+        val response = JSONObject().apply {
+            put("customerAuthenticationRegulationEnvironment", "psdtwo")
+        }
+        val authenticationInsight = AuthenticationInsight.fromJson(response)
 
-        AuthenticationInsight authenticationInsight = AuthenticationInsight.fromJson(response);
-
-        assertEquals("psd2", authenticationInsight.getRegulationEnvironment());
-    }
-    @Test
-    public void fromJson_onUnknownRegulationEnvironment_returnsUnknown() throws JSONException {
-        JSONObject response = new JSONObject()
-                .put("customerAuthenticationRegulationEnvironment", "FaKeVaLuE");
-
-        AuthenticationInsight authenticationInsight = AuthenticationInsight.fromJson(response);
-
-        assertEquals("fakevalue", authenticationInsight.getRegulationEnvironment());
+        assertEquals("psd2", authenticationInsight?.regulationEnvironment)
     }
 
     @Test
-    public void fromJson_withRegulationEnvironmentKey_returnsValue() throws JSONException {
-        JSONObject response = new JSONObject()
-                .put("regulationEnvironment", "UNREGULATED");
+    @Throws(JSONException::class)
+    fun `parses unknown regulation environment from JSON and returns unknown`() {
+        val response = JSONObject().apply {
+            put("customerAuthenticationRegulationEnvironment", "FaKeVaLuE")
+        }
+        val authenticationInsight = AuthenticationInsight.fromJson(response)
 
-        AuthenticationInsight authenticationInsight = AuthenticationInsight.fromJson(response);
-
-        assertEquals("unregulated", authenticationInsight.getRegulationEnvironment());
+        assertEquals("fakevalue", authenticationInsight?.regulationEnvironment)
     }
 
     @Test
-    public void fromJson_onNullJsonObject_returnsNull() {
-        assertNull(AuthenticationInsight.fromJson(null));
+    @Throws(JSONException::class)
+    fun `parses regulation environment key from JSON and returns its value`() {
+        val response = JSONObject().apply {
+            put("regulationEnvironment", "UNREGULATED")
+        }
+        val authenticationInsight = AuthenticationInsight.fromJson(response)
+
+        assertEquals("unregulated", authenticationInsight?.regulationEnvironment)
     }
 
     @Test
-    public void parcelsCorrectly() throws JSONException{
-        AuthenticationInsight authInsight = AuthenticationInsight.fromJson(new JSONObject()
-                .put("regulationEnvironment", "psdtwo"));
+    fun `receives null JSON object and returns null`() {
+        assertNull(AuthenticationInsight.fromJson(null))
+    }
 
-        Parcel parcel = Parcel.obtain();
-        authInsight.writeToParcel(parcel, 0);
-        parcel.setDataPosition(0);
+    @Test
+    @Throws(JSONException::class)
+    fun `parcels Auth Insight from JSON and returns correct regulation environment`() {
+        val response = JSONObject().apply {
+            put("regulationEnvironment", "psdtwo")
+        }
+        val authenticationInsight = AuthenticationInsight.fromJson(response)
+        val parcel = Parcel.obtain().apply {
+            authenticationInsight?.writeToParcel(this, 0)
+            setDataPosition(0)
+        }
+        val parceledInsight = parcelableCreator<AuthenticationInsight>().createFromParcel(parcel)
 
-        AuthenticationInsight parceledInsight = AuthenticationInsight.CREATOR.createFromParcel(parcel);
-
-        assertEquals(authInsight.getRegulationEnvironment(), parceledInsight.getRegulationEnvironment());
+        assertEquals(authenticationInsight?.regulationEnvironment, parceledInsight.regulationEnvironment)
     }
 }
