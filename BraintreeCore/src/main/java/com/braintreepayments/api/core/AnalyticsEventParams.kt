c8ed3d84cefcfd8fa7e5bea0ab94f631d717627e
Jax DesMarais-Leder <jdesmarais@paypal.com>
2025-08-06 10:51:22 -0500

    Update BraintreeCore/src/main/java/com/braintreepayments/api/core/AnalyticsEventParams.kt

--- BraintreeCore/src/main/java/com/braintreepayments/api/core/AnalyticsEventParams.kt
+++ BraintreeCore/src/main/java/com/braintreepayments/api/core/AnalyticsEventParams.kt
@@ -20,7 +20,7 @@ import androidx.annotation.RestrictTo
  * @property buttonOrder The order or ranking in which payment buttons appear.
  * @property pageType The page or view that a button is displayed on.
  * @property errorDescription use this to provide more detail on a failure event.
- * @property contextType Indicates whether the request was a Billing Agreement (BA-TOKEN) 
+ * @property contextType Indicates whether the request was a Billing Agreement (BA-TOKEN)
  * or Checkout (EC-TOKEN) request.
  */
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
