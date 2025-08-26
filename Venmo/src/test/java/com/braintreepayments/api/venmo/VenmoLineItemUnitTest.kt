5e5e0a00cadd7533f3a15a35a79eab2fe233858f
anoguier <anoguier@paypal.com>
2025-08-26 14:09:53 -0500

    convert `VenmoLineItemUnitTest` from Java to Kotlin

--- Venmo/src/test/java/com/braintreepayments/api/venmo/VenmoLineItemUnitTest.kt
+++ Venmo/src/test/java/com/braintreepayments/api/venmo/VenmoLineItemUnitTest.kt
@@ -1,34 +1,37 @@
-package com.braintreepayments.api.venmo;
+package com.braintreepayments.api.venmo
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
+import org.json.JSONException
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
-import static junit.framework.Assert.assertEquals;
-
-@RunWith(RobolectricTestRunner.class)
-public class VenmoLineItemUnitTest {
+@RunWith(RobolectricTestRunner::class)
+class VenmoLineItemUnitTest {
 
     @Test
-    public void toJson_setsKeysAndValues() throws JSONException {
-        VenmoLineItem item = new VenmoLineItem(VenmoLineItemKind.DEBIT, "An Item", 1, "2");
-        item.setDescription("A new item");
-        item.setProductCode("abc-123");
-        item.setUnitTaxAmount("1.50");
-        item.setUrl("http://example.com");
+    @Throws(JSONException::class)
+    fun `creates JSONObject from VenmoLineItem and preserves all keys and value correctly`() {
+        val item = VenmoLineItem(
+            kind = VenmoLineItemKind.DEBIT,
+            name = "An Item",
+            quantity = 1,
+            unitAmount = "2",
+            description = "A new item",
+            productCode = "abc-123",
+            unitTaxAmount = "1.50",
+            url = "http://example.com"
+        )
 
-        JSONObject json = item.toJson();
+        val json = item.toJson()
 
-        assertEquals("DEBIT", json.getString("type"));
-        assertEquals("An Item", json.getString("name"));
-        assertEquals("1", json.getString("quantity"));
-        assertEquals("2", json.getString("unitAmount"));
-        assertEquals("A new item", json.getString("description"));
-        assertEquals("abc-123", json.getString("productCode"));
-        assertEquals("1.50", json.getString("unitTaxAmount"));
-        assertEquals("http://example.com", json.getString("url"));
+        assertEquals("DEBIT", json.getString("type"))
+        assertEquals("An Item", json.getString("name"))
+        assertEquals("1", json.getString("quantity"))
+        assertEquals("2", json.getString("unitAmount"))
+        assertEquals("A new item", json.getString("description"))
+        assertEquals("abc-123", json.getString("productCode"))
+        assertEquals("1.50", json.getString("unitTaxAmount"))
+        assertEquals("http://example.com", json.getString("url"))
     }
 }
-
