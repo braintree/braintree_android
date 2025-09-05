ea8399991f1faf05720bf92b854497f875a93c29
anoguier <anoguier@paypal.com>
2025-09-05 15:42:26 -0500

    convert `ThreeDSecureV2LabelCustomizationUnitTest` from Java to Kotlin

--- ThreeDSecure/src/test/java/com/braintreepayments/api/threedsecure/ThreeDSecureV2LabelCustomizationUnitTest.kt
+++ ThreeDSecure/src/test/java/com/braintreepayments/api/threedsecure/ThreeDSecureV2LabelCustomizationUnitTest.kt
@@ -1,60 +1,58 @@
-package com.braintreepayments.api.threedsecure;
+package com.braintreepayments.api.threedsecure
 
-import android.os.Parcel;
+import android.os.Parcel
+import junit.framework.TestCase.assertEquals
+import kotlinx.parcelize.parcelableCreator
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
-import com.braintreepayments.api.threedsecure.ThreeDSecureV2LabelCustomization;
-import com.cardinalcommerce.shared.userinterfaces.LabelCustomization;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
-
-import static org.junit.Assert.assertEquals;
-
-@RunWith(RobolectricTestRunner.class)
-public class ThreeDSecureV2LabelCustomizationUnitTest {
+@RunWith(RobolectricTestRunner::class)
+class ThreeDSecureV2LabelCustomizationUnitTest {
 
     @Test
-    public void setsAllCardinalClassProperties() {
-        ThreeDSecureV2LabelCustomization sut = new ThreeDSecureV2LabelCustomization();
-        sut.setHeadingTextColor("#00FF00");
-        sut.setHeadingTextFontName("Comic Sans");
-        sut.setHeadingTextFontSize(12);
-        sut.setTextColor("#ff0000");
-        sut.setTextFontSize(19);
-        sut.setTextFontName("Arial");
+    fun `sets all cardinalClass properties correctly`() {
+        val sut = ThreeDSecureV2LabelCustomization(
+            headingTextColor = "#00FF00",
+            headingTextFontName = "Comic Sans",
+            headingTextFontSize = 12,
+            textColor = "#ff0000",
+            textFontName = "Arial",
+            textFontSize = 19
+        )
 
-        LabelCustomization cardinalLabelCustomization = sut.getCardinalLabelCustomization();
-        assertEquals("#00FF00", cardinalLabelCustomization.getHeadingTextColor());
-        assertEquals("Comic Sans", cardinalLabelCustomization.getHeadingTextFontName());
-        assertEquals(12, cardinalLabelCustomization.getHeadingTextFontSize());
-        assertEquals("#ff0000", cardinalLabelCustomization.getTextColor());
-        assertEquals(19, cardinalLabelCustomization.getTextFontSize());
-        assertEquals("Arial", cardinalLabelCustomization.getTextFontName());
+        val cardinalLabelCustomization = sut.cardinalLabelCustomization
+
+        assertEquals("#00FF00", cardinalLabelCustomization.headingTextColor)
+        assertEquals("Comic Sans", cardinalLabelCustomization.headingTextFontName)
+        assertEquals(12, cardinalLabelCustomization.headingTextFontSize)
+        assertEquals("#ff0000", cardinalLabelCustomization.textColor)
+        assertEquals(19, cardinalLabelCustomization.textFontSize)
+        assertEquals("Arial", cardinalLabelCustomization.textFontName)
     }
 
     @Test
-    public void writeToParcel() {
-        ThreeDSecureV2LabelCustomization customization = new ThreeDSecureV2LabelCustomization();
-        customization.setHeadingTextColor("#FFFFFF");
-        customization.setHeadingTextFontName("Times New Roman");
-        customization.setHeadingTextFontSize(30);
-        customization.setTextColor("#121212");
-        customization.setTextFontName("Helvetica");
-        customization.setTextFontSize(15);
+    fun `creates ThreeDSecureV2LabelCustomization and parcels it correctly`() {
+        val customization = ThreeDSecureV2LabelCustomization(
+            headingTextColor = "#FFFFFF",
+            headingTextFontName = "Times New Roman",
+            headingTextFontSize = 30,
+            textColor = "#121212",
+            textFontName = "Helvetica",
+            textFontSize = 15
+        )
 
-        Parcel parcel = Parcel.obtain();
-        customization.writeToParcel(parcel, 0);
-        parcel.setDataPosition(0);
+        val parcel = Parcel.obtain().apply {
+            customization.writeToParcel(this, 0)
+            setDataPosition(0)
+        }
 
-        ThreeDSecureV2LabelCustomization actual =
-                ThreeDSecureV2LabelCustomization.CREATOR.createFromParcel(parcel);
-
-        assertEquals("#FFFFFF", actual.getHeadingTextColor());
-        assertEquals("Times New Roman", actual.getHeadingTextFontName());
-        assertEquals(30, actual.getHeadingTextFontSize());
-        assertEquals("#121212", actual.getTextColor());
-        assertEquals("Helvetica", actual.getTextFontName());
-        assertEquals(15, actual.getTextFontSize());
+        val actual = parcelableCreator<ThreeDSecureV2LabelCustomization>().createFromParcel(parcel)
+        assertEquals("#FFFFFF", actual.headingTextColor)
+        assertEquals("Times New Roman", actual.headingTextFontName)
+        assertEquals(30, actual.headingTextFontSize)
+        assertEquals("#121212", actual.textColor)
+        assertEquals(15, actual.textFontSize)
+        assertEquals("Helvetica", actual.textFontName)
     }
 }
