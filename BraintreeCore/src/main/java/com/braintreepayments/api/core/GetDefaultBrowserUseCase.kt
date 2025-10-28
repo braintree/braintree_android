4323534e9b9b21288ef15e974ff52d5dd6f11d9e
Sai <saperi@paypal.com>
2025-10-28 12:38:55 -0700

    Add a use-case for returning the default browser of the device

---
+++ BraintreeCore/src/main/java/com/braintreepayments/api/core/GetDefaultBrowserUseCase.kt
@@ -0,0 +1,24 @@
+package com.braintreepayments.api.core
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
+
+/**
+ * Use case that returns the default browser that should be used for navigating from App Switch or browser back into the
+ * merchant app.
+ */
+class GetDefaultBrowserUseCase(private val merchantRepository: MerchantRepository) {
+
+    operator fun invoke(packageManager: PackageManager): String? {
+        val browserIntent = Intent(Intent.ACTION_VIEW, merchantRepository.appLinkReturnUri).apply {
+            addCategory(Intent.CATEGORY_BROWSABLE)
+        }
+        val resolveInfo: ResolveInfo? = packageManager.resolveActivity(browserIntent, PackageManager.MATCH_DEFAULT_ONLY)
+
+        if (resolveInfo != null && resolveInfo.activityInfo != null) {
+            return resolveInfo.activityInfo.packageName
+        }
+        return null // No default browser found or resolved
+    }
+}
