06db89e225a2bfa77fe565d4653a6b6c89ed0e78
saperi22 <104481964+saperi22@users.noreply.github.com>
2025-11-20 12:00:07 -0800

    App links unsupported browser feature (#1473)
    * Unsupported browser usecase (#1460)
    
    * Add a use-case for returning the default browser of the device
    
    * Add a use-case to compare default browser to an app link compatible browser
    
    * Remove internal modifier and use RestrictTo annotation to restrict usage to library group.
    
    * use contains instead of strict equality to check for browser compatibility
    
    * Use app links only on supported browsers (#1463)
    
    * Add a use-case to compare default browser to an app link compatible browser
    
    * Add tests
    
    * Fix linter
    
    * refactor use case
    
    # Conflicts:
    #	BraintreeCore/src/test/java/com/braintreepayments/api/core/GetDefaultBrowserUseCaseUnitTest.kt
    
    * Address PR comments
    
     Conflicts:
    	BraintreeCore/src/main/java/com/braintreepayments/api/core/GetDefaultBrowserUseCase.kt
    	BraintreeCore/src/test/java/com/braintreepayments/api/core/GetDefaultBrowserUseCaseUnitTest.kt
    
    * rename variable
    
    * rename class
    
    * Add checks to confirm if browsers support app links before using app links feature
    
    * fix tests
    
    * Refactor use-cases
    
    * modify use cases
    
    * linter fixes
    
    * Update GetDefaultAppUseCase signature
    
    * fix tests
    
    * condense code
    
    * condense code
    
    * move tests
    
    * refactoring
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * Add CHANGELOG.md entry
    
    * Use annotation to clarify intent of the Uri being passed.
    
    * use https://www.paypal.com/checkout instead of example.com for default uri
    
    * add known limitations to CHANGELOG.md
    
    * modify CHANGELOG.md
    
    * update CHANGELOG.md

---
+++ BraintreeCore/src/test/java/com/braintreepayments/api/core/usecase/GetAppLinksCompatibleBrowserUseCaseUnitTest.kt
@@ -0,0 +1,168 @@
+package com.braintreepayments.api.core.usecase
+
+import androidx.core.net.toUri
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@RunWith(RobolectricTestRunner::class)
+class GetAppLinksCompatibleBrowserUseCaseUnitTest {
+
+    private val getDefaultAppUseCase: GetDefaultAppUseCase = mockk(relaxed = true)
+
+    internal lateinit var sut: GetAppLinksCompatibleBrowserUseCase
+    private val sampleUri = "https://example.com".toUri()
+
+    @Before
+    fun setUp() {
+        sut = GetAppLinksCompatibleBrowserUseCase(getDefaultAppUseCase)
+    }
+
+    @Test
+    fun `when invoke is called with Chrome as default browser, returns true`() {
+        every { getDefaultAppUseCase(sampleUri) } returns "com.android.chrome"
+
+        val result = sut(sampleUri)
+
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun `when invoke is called with Chrome canary as default browser, returns true`() {
+        every { getDefaultAppUseCase(sampleUri) } returns "com.android.chrome.canary"
+
+        val result = sut(sampleUri)
+
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun `when invoke is called with incomplete appId of Chrome as default browser, returns false`() {
+        every { getDefaultAppUseCase(sampleUri) } returns "com.android"
+
+        val result = sut(sampleUri)
+
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun `when invoke is called with Brave as default browser, returns true`() {
+        every { getDefaultAppUseCase(sampleUri) } returns "com.brave.browser"
+
+        val result = sut(sampleUri)
+
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun `when invoke is called with Samsung Browser as default browser, returns true`() {
+        every { getDefaultAppUseCase(sampleUri) } returns "com.sec.android.app.sbrowser"
+
+        val result = sut(sampleUri)
+
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun `when invoke is called with Firefox as default browser, returns true`() {
+        every { getDefaultAppUseCase(sampleUri) } returns "org.mozilla.firefox"
+
+        val result = sut(sampleUri)
+
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun `when invoke is called with Microsoft Edge as default browser, returns true`() {
+        every { getDefaultAppUseCase(sampleUri) } returns "com.microsoft.emmx"
+
+        val result = sut(sampleUri)
+
+        assertEquals(true, result)
+    }
+
+    @Test
+    fun `when invoke is called with Mi Browser as default browser, returns false`() {
+        every { getDefaultAppUseCase(sampleUri) } returns "com.mi.globalbrowser"
+
+        val result = sut(sampleUri)
+
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun `when invoke is called with UC Browser as default browser, returns false`() {
+        every { getDefaultAppUseCase(sampleUri) } returns "com.UCMobile.intl"
+
+        val result = sut(sampleUri)
+
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun `when invoke is called with DuckDuckGo as default browser, returns false`() {
+        every { getDefaultAppUseCase(sampleUri) } returns "com.duckduckgo.mobile.android"
+
+        val result = sut(sampleUri)
+
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun `when invoke is called with Opera as default browser, returns false`() {
+        every { getDefaultAppUseCase(sampleUri) } returns "com.opera.browser"
+
+        val result = sut(sampleUri)
+
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun `when invoke is called with Opera GX as default browser, returns false`() {
+        every { getDefaultAppUseCase(sampleUri) } returns "com.opera.gx"
+
+        val result = sut(sampleUri)
+
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun `when invoke is called with Opera Mini as default browser, returns false`() {
+        every { getDefaultAppUseCase(sampleUri) } returns "com.opera.mini.native"
+
+        val result = sut(sampleUri)
+
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun `when invoke is called with Yandex Browser as default browser, returns false`() {
+        every { getDefaultAppUseCase(sampleUri) } returns "com.yandex.browser"
+
+        val result = sut(sampleUri)
+
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun `when invoke is called with unknown browser as default browser, returns false`() {
+        every { getDefaultAppUseCase(sampleUri) } returns "com.unknown.browser"
+
+        val result = sut(sampleUri)
+
+        assertEquals(false, result)
+    }
+
+    @Test
+    fun `when invoke is called and default browser is null, returns false`() {
+        every { getDefaultAppUseCase(sampleUri) } returns null
+
+        val result = sut(sampleUri)
+
+        assertEquals(false, result)
+    }
+}
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # Braintree Android SDK Release Notes
 
+## unreleased
+
+* All Modules
+    * Fallback to using deep links when the user's default browser does not support app links.
+        * Known limitation: Flow doesn't work on UC and Opera GX browsers for both app links and deep links. See #1473 
+          for testing note.
+
 ## 5.19.0 (2025-11-18)
 
 * Card
