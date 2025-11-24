63e015fd9874a2f1ab27d3f884a3dc1483a56764
Sara Vasquez <saravasquez@paypal.com>
2025-11-24 13:28:44 -0500

    CHANGELOG entry

--- Demo/src/main/java/com/braintreepayments/demo/MainFragment.kt
+++ Demo/src/main/java/com/braintreepayments/demo/MainFragment.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
