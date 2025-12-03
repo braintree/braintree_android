895a048129b2bd96d43f1f59179383ab2e1f0940
anoguier <anoguier@paypal.com>
2025-12-03 16:07:38 -0600

    complete venmo flow that is initiated by the click of the button

--- Demo/src/main/java/com/braintreepayments/demo/PaymentButtonsFragment.java
+++ Demo/src/main/java/com/braintreepayments/demo/PaymentButtonsFragment.java
@@ -13,18 +13,13 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.NavDirections;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.braintreepayments.api.core.UserCanceledException;
 import com.braintreepayments.api.uicomponents.PayPalButton;
 import com.braintreepayments.api.uicomponents.PayPalButtonColor;
 import com.braintreepayments.api.uicomponents.VenmoButton;
 import com.braintreepayments.api.uicomponents.VenmoButtonColor;
 import com.braintreepayments.api.venmo.VenmoAccountNonce;
-import com.braintreepayments.api.venmo.VenmoClient;
-import com.braintreepayments.api.venmo.VenmoLauncher;
 import com.braintreepayments.api.venmo.VenmoLineItem;
 import com.braintreepayments.api.venmo.VenmoLineItemKind;
-import com.braintreepayments.api.venmo.VenmoPaymentAuthRequest;
-import com.braintreepayments.api.venmo.VenmoPaymentAuthResult;
 import com.braintreepayments.api.venmo.VenmoPaymentMethodUsage;
 import com.braintreepayments.api.venmo.VenmoPendingRequest;
 import com.braintreepayments.api.venmo.VenmoRequest;
@@ -38,8 +33,7 @@ public class PaymentButtonsFragment extends BaseFragment {
     private PayPalButton payPalButton;
     private MaterialButtonToggleGroup payPalToggleGroup;
     private VenmoButton venmoButton;
-    private VenmoClient venmoClient;
-    private VenmoLauncher venmoLauncher;
+    private VenmoRequest venmoRequest;
     private MaterialButtonToggleGroup venmoToggleGroup;
 
 
@@ -73,12 +67,6 @@ public class PaymentButtonsFragment extends BaseFragment {
 
         venmoButton = view.findViewById(R.id.venmo_payment_button);
         venmoToggleGroup = view.findViewById(R.id.venmo_button_toggle_group);
-        venmoButton.setVenmoClickListener(new VenmoButton.OnVenmoClickListener() {
-            @Override
-            public void onVenmoClick(VenmoButton view) {
-                launchVenmo(view);
-            }
-        });
         venmoToggleGroup.addOnButtonCheckedListener(
                 new MaterialButtonToggleGroup.OnButtonCheckedListener() {
                     @Override
@@ -99,85 +87,6 @@ public class PaymentButtonsFragment extends BaseFragment {
                 }
         );
 
-        if (venmoClient == null) {
-            if (Settings.useAppLinkReturn(requireContext())) {
-                venmoClient = new VenmoClient(
-                        requireContext(),
-                        super.getAuthStringArg(),
-                        Uri.parse("https://mobile-sdk-demo-site-838cead5d3ab.herokuapp.com/braintree-payments"),
-                        "com.braintreepayments.demo.braintree"
-                );
-            } else {
-                venmoClient = new VenmoClient(requireContext(), super.getAuthStringArg());
-            }
-        }
-        venmoLauncher = new VenmoLauncher();
-
-
-        return view;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        venmoButton.handleReturnToApp(requireActivity().getIntent());
-//        VenmoPendingRequest.Started venmoPendingRequest = getVenmoPendingRequest();
-//        if (venmoPendingRequest != null) {
-//            VenmoPaymentAuthResult paymentAuthResult = venmoLauncher.handleReturnToApp(venmoPendingRequest, requireActivity().getIntent());
-//            if (paymentAuthResult instanceof VenmoPaymentAuthResult.Success) {
-//                completeVenmoFlow((VenmoPaymentAuthResult.Success) paymentAuthResult);
-//            } else {
-//                handleError(new Exception("User did not complete payment flow"));
-//            }
-//            clearVenmoPendingRequest();
-//        }
-    }
-
-    private void handleVenmoResult(VenmoResult result) {
-        if (result instanceof VenmoResult.Success) {
-            handleVenmoAccountNonce(((VenmoResult.Success) result).getNonce());
-        } else if (result instanceof VenmoResult.Failure) {
-            handleError(((VenmoResult.Failure) result).getError());
-        } else if (result instanceof VenmoResult.Cancel) {
-            handleError(new UserCanceledException("User canceled Venmo"));
-        }
-    }
-
-    private void handleVenmoAccountNonce(VenmoAccountNonce venmoAccountNonce) {
-        super.onPaymentMethodNonceCreated(venmoAccountNonce);
-
-        NavDirections action = PaymentButtonsFragmentDirections.actionPaymentButtonsFragmentToDisplayNonceFragment(venmoAccountNonce);
-        NavHostFragment.findNavController(this).navigate(action);
-    }
-
-//    public void launchVenmo(View v) {
-//        FragmentActivity activity = getActivity();
-//
-//        getActivity().setProgressBarIndeterminateVisibility(true);
-//
-//        boolean shouldVault =
-//                Settings.vaultVenmo(activity) && !TextUtils.isEmpty(Settings.getCustomerId(activity));
-//
-//        VenmoPaymentMethodUsage venmoPaymentMethodUsage = shouldVault ?
-//                VenmoPaymentMethodUsage.MULTI_USE : VenmoPaymentMethodUsage.SINGLE_USE;
-//        VenmoRequest venmoRequest = new VenmoRequest(venmoPaymentMethodUsage);
-//        venmoRequest.setProfileId(null);
-//        venmoRequest.setShouldVault(shouldVault);
-//        venmoRequest.setCollectCustomerBillingAddress(true);
-//        venmoRequest.setCollectCustomerShippingAddress(true);
-//        venmoRequest.setTotalAmount("20");
-//        venmoRequest.setSubTotalAmount("18");
-//        venmoRequest.setTaxAmount("1");
-//        venmoRequest.setShippingAmount("1");
-//        ArrayList<VenmoLineItem> lineItems = new ArrayList<>();
-//        lineItems.add(new VenmoLineItem(VenmoLineItemKind.CREDIT, "Some Item", 1, "2"));
-//        lineItems.add(new VenmoLineItem(VenmoLineItemKind.DEBIT, "Two Items", 2, "10"));
-//        venmoRequest.setLineItems(lineItems);
-//
-//        startVenmoFlow(venmoRequest);
-//    }
-
-    private VenmoRequest createVenmoRequest() {
         FragmentActivity activity = getActivity();
 
         getActivity().setProgressBarIndeterminateVisibility(true);
@@ -187,7 +96,7 @@ public class PaymentButtonsFragment extends BaseFragment {
 
         VenmoPaymentMethodUsage venmoPaymentMethodUsage = shouldVault ?
                 VenmoPaymentMethodUsage.MULTI_USE : VenmoPaymentMethodUsage.SINGLE_USE;
-        VenmoRequest venmoRequest = new VenmoRequest(venmoPaymentMethodUsage);
+        venmoRequest = new VenmoRequest(venmoPaymentMethodUsage);
         venmoRequest.setProfileId(null);
         venmoRequest.setShouldVault(shouldVault);
         venmoRequest.setCollectCustomerBillingAddress(true);
@@ -201,26 +110,49 @@ public class PaymentButtonsFragment extends BaseFragment {
         lineItems.add(new VenmoLineItem(VenmoLineItemKind.DEBIT, "Two Items", 2, "10"));
         venmoRequest.setLineItems(lineItems);
 
-        return venmoRequest;
-    }
+        venmoButton.initialize(
+                super.getAuthStringArg(),
+                Uri.parse("https://mobile-sdk-demo-site-838cead5d3ab.herokuapp.com/braintree-payments"),
+                "com.braintreepayments.demo.braintree"
+        );
 
-    private void startVenmoFlow(VenmoRequest venmoRequest) {
-        venmoClient.createPaymentAuthRequest(requireActivity(), venmoRequest, (paymentAuthRequest) -> {
-            if (paymentAuthRequest instanceof VenmoPaymentAuthRequest.Failure) {
-                handleError(((VenmoPaymentAuthRequest.Failure) paymentAuthRequest).getError());
-            } else if (paymentAuthRequest instanceof VenmoPaymentAuthRequest.ReadyToLaunch) {
-                VenmoPendingRequest pendingRequest = venmoLauncher.launch(requireActivity(), (VenmoPaymentAuthRequest.ReadyToLaunch) paymentAuthRequest);
-                if (pendingRequest instanceof VenmoPendingRequest.Started) {
-                    storeVenmoPendingRequest((VenmoPendingRequest.Started) pendingRequest);
-                } else if (pendingRequest instanceof VenmoPendingRequest.Failure) {
-                    handleError(((VenmoPendingRequest.Failure) pendingRequest).getError());
-                }
+        venmoButton.setVenmoRequest(venmoRequest);
+        venmoButton.setVenmoLaunchCallback(request -> {
+            if (request instanceof VenmoPendingRequest.Started) {
+                storeVenmoPendingRequest((VenmoPendingRequest.Started) request);
+            } else if (request instanceof  VenmoPendingRequest.Failure) {
+                handleError(((VenmoPendingRequest.Failure) request).getError());
             }
         });
+
+        return view;
     }
 
-    private void completeVenmoFlow(VenmoPaymentAuthResult.Success paymentAuthResult) {
-        venmoClient.tokenize(paymentAuthResult, this::handleVenmoResult);
+    @Override
+    public void onResume() {
+        super.onResume();
+        VenmoPendingRequest.Started venmoPendingRequest = getVenmoPendingRequest();
+        if (venmoPendingRequest != null) {
+            venmoButton.handleReturnToApp(venmoPendingRequest, requireActivity().getIntent(), venmoResult -> {
+                if (venmoResult instanceof VenmoResult.Success) {
+                    handleVenmoAccountNonce(((VenmoResult.Success) venmoResult).getNonce());
+                } else if (venmoResult instanceof VenmoResult.Failure) {
+                    handleError(((VenmoResult.Failure) venmoResult).getError());
+                } else if (venmoResult instanceof VenmoResult.Cancel) {
+                    handleError(new Exception("User did not complete payment flow"));
+                }
+            });
+            clearVenmoPendingRequest();
+        }
+    }
+
+    private void handleVenmoAccountNonce(VenmoAccountNonce venmoAccountNonce) {
+        super.onPaymentMethodNonceCreated(venmoAccountNonce);
+
+        NavDirections action = PaymentButtonsFragmentDirections
+                .actionPaymentButtonsFragmentToDisplayNonceFragment(venmoAccountNonce)
+                .setVenmoNonce(venmoAccountNonce.getString());
+        NavHostFragment.findNavController(this).navigate(action);
     }
 
     private void storeVenmoPendingRequest(VenmoPendingRequest.Started request) {
--- UIComponents/src/main/java/com/braintreepayments/api/uicomponents/VenmoButton.kt
+++ UIComponents/src/main/java/com/braintreepayments/api/uicomponents/VenmoButton.kt
@@ -1,15 +1,28 @@
 package com.braintreepayments.api.uicomponents
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.GradientDrawable
+import android.net.Uri
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatButton
 import androidx.core.content.ContextCompat
+import com.braintreepayments.api.venmo.VenmoClient
+import com.braintreepayments.api.venmo.VenmoLauncher
+import com.braintreepayments.api.venmo.VenmoPaymentAuthRequest
+import com.braintreepayments.api.venmo.VenmoPaymentAuthResult
+import com.braintreepayments.api.venmo.VenmoPendingRequest
+import com.braintreepayments.api.venmo.VenmoRequest
+import com.braintreepayments.api.venmo.VenmoResult
+import com.braintreepayments.api.venmo.VenmoTokenizeCallback
 
 /**
- * A customizable Venmo branded button to initiate the Venmo flow
+ * A customizable Venmo branded button to initiate the Venmo flow.
+ *
+ * This button provides a pre-styled Venmo button with configurable colors and handles
+ * the complete Venmo payment flow.
  */
 class VenmoButton @JvmOverloads constructor(
     context: Context,
@@ -25,30 +38,25 @@ class VenmoButton @JvmOverloads constructor(
     private val desiredHeight = resources.getDimension(R.dimen.pay_button_height).toInt()
     private val minDesiredWidth = resources.getDimension(R.dimen.pay_button_min_width).toInt()
 
-    private var venmoClient: VenmoClient? = null
-    private var venmoLauncher: VenmoLauncher? = null
+    lateinit var venmoClient: VenmoClient
+    private var venmoLauncher: VenmoLauncher
 
     private var venmoRequest: VenmoRequest? = null
-    private var activityRef: WeakReference<FragmentActivity>? = null
-
-    private var resultCallback: ((VenmoResult) -> Unit)? = null
-    private var pendingRequestCreatedCallback: ((VenmoPendingRequest.Started) -> Unit)? = null
-    private var pendingRequestCompleteCallback: (() -> Unit)? = null
-
+    var venmoLaunchCallback: VenmoLaunchCallback? = null
 
     init {
         context.theme.obtainStyledAttributes(attrs, R.styleable.VenmoButton, 0, 0).apply {
             try {
-                currentStyle = VenmoButtonColor.fromId(getInt(R.styleable.VenmoButton_paymentButtonColor, 0))
+                currentStyle =
+                    VenmoButtonColor.fromId(getInt(R.styleable.VenmoButton_paymentButtonColor, 0))
             } finally {
                 recycle()
             }
         }
         setupBackground()
         applyStyle()
-        setOnClickListener {
-            handleClick()
-        }
+
+        venmoLauncher = VenmoLauncher()
     }
 
     private fun setupBackground() {
@@ -94,60 +102,123 @@ class VenmoButton @JvmOverloads constructor(
         applyStyle()
     }
 
+    /**
+     * Initializes the Venmo button with the required parameters needed to start the payment flow.
+     *
+     * Call this before setting a Venmo request or handling any payment flows.
+     *
+     * @param authorization             a Tokenization Key or Client Token used to authenticate
+     * @param appLinkReturnUrl          a [Uri] containing the Android App Link website associated with
+     * merchant's application to be used to return to merchant's app from the Venmo payment flows.
+     * @param deepLinkFallbackUrlScheme a return url scheme that will be used as a deep link fallback when returning to
+     * merchant's app via App Link is not available (buyer unchecks the "Open supported links" setting).
+     */
+
     fun initialize(
-        activity: FragmentActivity,
         authorization: String,
-        request: VenmoRequest,
-        onPendingRequestCreated: (VenmoPendingRequest.Started) -> Unit,
-        onPendingRequestComplete: () -> Unit,
-        onResult: (VenmoResult) -> Unit
-    ){
-        this.activityRef = WeakReference(activity)
-        this.venmoRequest = request
-        this.pendingRequestCreatedCallback = onPendingRequestCreated
-        this.pendingRequestCompleteCallback = onPendingRequestComplete
-        this.resultCallback = onResult
-
-        this.venmoClient = VenmoClient(context, authorization)
-        this.venmoLauncher = VenmoLauncher()
+        appLinkReturnUrl: Uri,
+        deepLinkFallbackUrlScheme: String? = null
+    ) {
+        venmoClient = VenmoClient(
+            context = context,
+            authorization = authorization,
+            appLinkReturnUrl = appLinkReturnUrl,
+            deepLinkFallbackUrlScheme = deepLinkFallbackUrlScheme
+        )
     }
 
-    fun setPaymentRequest(request: VenmoRequest) {
-        this.venmoRequest = request
-    }
-
-
-
-    private fun handleClick() {
-        startVenmoFlow(activity, request)
-    }
-
-    private fun startVenmoFlow(activity: FragmentActivity, request: VenmoRequest) {
-        venmoClient?.createPaymentAuthRequest(activity, request) { paymentAuthRequest ->
+    /**
+     * Sets the Venmo request configuration and wires up the click handler.
+     *
+     * When the button is clicked, it:
+     * 1. Creates a payment auth request using the provided Venmo request
+     * 2. Launches the Venmo authentication flow
+     * 3. Notifies the [venmoLaunchCallback] with the result
+     *
+     * @param venmoRequest the Venmo request containing payment details and configuration
+     */
+    fun setVenmoRequest(venmoRequest: VenmoRequest) {
+        setOnClickListener {
+            venmoClient.createPaymentAuthRequest(
+                context = context,
+                request = venmoRequest
+            ) { venmoPaymentAuthRequest: VenmoPaymentAuthRequest ->
+                when (venmoPaymentAuthRequest) {
+                    is VenmoPaymentAuthRequest.ReadyToLaunch -> {
+                        getActivity()?.let { activity ->
+                            val venmoPendingRequest = venmoLauncher.launch(
+                                activity = activity,
+                                paymentAuthRequest = venmoPaymentAuthRequest
+                            )
+                            when (venmoPendingRequest) {
+                                is VenmoPendingRequest.Started -> {
+                                    venmoLaunchCallback?.onVenmoPaymentAuthRequest(
+                                        VenmoPendingRequest.Started(venmoPendingRequest.pendingRequestString)
+                                    )
+                                }
+                                is VenmoPendingRequest.Failure -> {
+                                    venmoLaunchCallback?.onVenmoPaymentAuthRequest(
+                                        VenmoPendingRequest.Failure(venmoPendingRequest.error)
+                                    )
+                                }
+                            }
+                        } ?: run {
+                            venmoLaunchCallback?.onVenmoPaymentAuthRequest(
+                                VenmoPendingRequest.Failure(NullPointerException("Activity is null"))
+                            )
+                        }
+                    }
+                    is VenmoPaymentAuthRequest.Failure -> {
+                        venmoLaunchCallback?.onVenmoPaymentAuthRequest(
+                            VenmoPendingRequest.Failure(venmoPaymentAuthRequest.error)
+                        )
+                    }
+                }
+            }
         }
     }
 
+    /**
+     * Handles the return from the Venmo authentication flow and tokenizes the result.
+     *
+     * Call this method on [onResume] after the user completes the Venmo authentication and is returned to app.
+     *
+     * If the Activity used to launch the Venmo flow has is configured with
+     * android:launchMode="singleTop", this method should be invoked in the onNewIntent method of the Activity.
+     *
+     * This method:
+     * 1. Extracts the authentication result from the intent
+     * 2. Tokenizes the successful authentication
+     * 3. Returns the final Venmo result via the callback
+     *
+     * @param pendingRequest the [VenmoPendingRequest.Started] stored after successfully
+     * invoking [VenmoLauncher.launch]
+     * @param intent         the intent to return to merchant's application containing a deep link result
+     * from the Venmo browser flow
+     * @param callback       callback to receive the final Venmo result (success, cancel, or failure)
+     */
 
-
-    fun handleReturnToApp(pendingRequest: VenmoPendingRequest.Started, intent: Intent) {
-        val paymentAuthResult = venmoLauncher?.handleReturnToApp(pendingRequest, intent)
-
-        pendingRequestCompleteCallback?.invoke()
-
+    fun handleReturnToApp(
+        pendingRequest: VenmoPendingRequest.Started,
+        intent: Intent,
+        callback: VenmoTokenizeCallback
+    ) {
+        val paymentAuthResult = venmoLauncher.handleReturnToApp(
+            pendingRequest = pendingRequest,
+            intent = intent
+        )
         when (paymentAuthResult) {
             is VenmoPaymentAuthResult.Success -> {
-                completeVenmoFlow(paymentAuthResult)
+                venmoClient.tokenize(paymentAuthResult) { venmoResult ->
+                    callback.onVenmoResult(venmoResult)
+                }
             }
-            else -> {
-                resultCallback?.invoke(
-                    VenmoResult.Failure(Exception("User did not complete payment flow"))
-                )
+            is VenmoPaymentAuthResult.Failure -> {
+                callback.onVenmoResult(VenmoResult.Failure(paymentAuthResult.error))
+            }
+            is VenmoPaymentAuthResult.NoResult -> {
+                callback.onVenmoResult(VenmoResult.Cancel)
             }
         }
     }
-
-    private fun completeVenmoFlow(paymentAuthResult: VenmoPaymentAuthResult.Success) {
-        venmoClient?.tokenize(paymentAuthResult) { result ->
-            resultCallback?.invoke(result)
-        }
-
+}
