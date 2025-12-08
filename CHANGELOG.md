7ccd47016e4586b9cd4ce6c90f655965b29dd6b8
anoguier <anoguier@paypal.com>
2025-12-08 09:58:04 -0600

    address PR comments

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,10 +9,6 @@
 * UIComponents
     * Add `PayPalButton` class to generate a PayPal-branded button for launching PayPal flow
     * Add `VenmoButton` class to generate a Venmo-branded button for launching Venmo flow
-    * Add FTPI tags `paypal-button:shown` and `paypal-button:clicked` to track button events
-    * Add FTPI tags `venmo-button:shown` and `venmo-button:clicked` to track button events
-  
-
 
 ## 5.19.0 (2025-11-18)
 
--- UIComponents/src/main/java/com/braintreepayments/api/uicomponents/ButtonsAnalytics.kt
+++ UIComponents/src/main/java/com/braintreepayments/api/uicomponents/ButtonsAnalytics.kt
@@ -2,9 +2,9 @@ package com.braintreepayments.api.uicomponents
 
 internal object ButtonsAnalytics {
 
-    const val PAYPAL_BUTTON_SHOWN = "ui-components:paypal-button:shown"
-    const val PAYPAL_BUTTON_CLICKED = "ui-components:paypal-button:clicked"
+    const val PAYPAL_BUTTON_SHOWN = "ui-components:paypal-button:presented"
+    const val PAYPAL_BUTTON_CLICKED = "ui-components:paypal-button:selected"
 
-    const val VENMO_BUTTON_SHOWN = "ui-components:venmo-button:shown"
-    const val VENMO_BUTTON_CLICKED = "ui-components:venmo-button:clicked"
+    const val VENMO_BUTTON_SHOWN = "ui-components:venmo-button:presented"
+    const val VENMO_BUTTON_CLICKED = "ui-components:venmo-button:selected"
 }
