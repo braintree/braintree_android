f0821179a58c230ea78692e29c6bdab007478895
Sai <saperi@paypal.com>
2025-12-11 13:14:08 -0800

    move more colors to companion object

--- UIComponents/src/main/java/com/braintreepayments/api/uicomponents/PayPalButtonColor.kt
+++ UIComponents/src/main/java/com/braintreepayments/api/uicomponents/PayPalButtonColor.kt
@@ -2,7 +2,6 @@ package com.braintreepayments.api.uicomponents
 
 import android.graphics.Color
 import androidx.annotation.ColorInt
-import androidx.core.content.ContextCompat
 import androidx.core.graphics.toColorInt
 
 /**
@@ -32,23 +31,21 @@ sealed class PayPalButtonColor(val key: Int) {
     object Black : PayPalButtonColor(key = 1) {
         override val logoId: Int get() = R.drawable.paypal_logo_white
         override val spinnerId: Int get() = R.drawable.avd_spinner_white
-        override val default = ButtonColors("#000000".toColorInt(), "#000000".toColorInt())
-        override val hover = ButtonColors("#333333".toColorInt(), "#333333".toColorInt())
-        override val focus = ButtonColors("#000000".toColorInt(), "#000000".toColorInt(), "#0066F5".toColorInt())
-        override val focusHover =
-            ButtonColors("#333333".toColorInt(), "#333333".toColorInt(), "#0066F5".toColorInt())
-        override val pressed = ButtonColors("#696969".toColorInt(), "#696969".toColorInt())
+        override val default = ButtonColors(PAYPAL_TERTIARY_MAIN, PAYPAL_TERTIARY_MAIN)
+        override val hover = ButtonColors(PAYPAL_TERTIARY_HOVER, PAYPAL_TERTIARY_HOVER)
+        override val focus = ButtonColors(PAYPAL_TERTIARY_MAIN, PAYPAL_TERTIARY_MAIN, PAYPAL_FOCUS_INDICATOR)
+        override val focusHover = ButtonColors(PAYPAL_TERTIARY_HOVER, PAYPAL_TERTIARY_HOVER, PAYPAL_FOCUS_INDICATOR)
+        override val pressed = ButtonColors(PAYPAL_TERTIARY_MAIN_ACTIVE, PAYPAL_TERTIARY_MAIN_ACTIVE)
     }
 
     object White : PayPalButtonColor(key = 2) {
         override val logoId: Int get() = R.drawable.paypal_logo_black
         override val spinnerId: Int get() = R.drawable.avd_spinner_black
-        override val default = ButtonColors("#FFFFFF".toColorInt(), "#555555".toColorInt())
-        override val hover = ButtonColors("#F2F2F2".toColorInt(), "#555555".toColorInt())
-        override val focus = ButtonColors("#FFFFFF".toColorInt(), "#555555".toColorInt(), "#0066F5".toColorInt())
-        override val focusHover =
-            ButtonColors("#F2F2F2".toColorInt(), "#555555".toColorInt(), "#0066F5".toColorInt())
-        override val pressed = ButtonColors("#E9E9E9".toColorInt(), "#555555".toColorInt())
+        override val default = ButtonColors(PAYPAL_WHITE_MAIN, PAYPAL_WHITE_BORDER)
+        override val hover = ButtonColors(PAYPAL_WHITE_HOVER, PAYPAL_WHITE_BORDER)
+        override val focus = ButtonColors(PAYPAL_WHITE_MAIN, PAYPAL_WHITE_BORDER, PAYPAL_FOCUS_INDICATOR)
+        override val focusHover = ButtonColors(PAYPAL_WHITE_HOVER, PAYPAL_WHITE_BORDER, PAYPAL_FOCUS_INDICATOR)
+        override val pressed = ButtonColors(PAYPAL_WHITE_MAIN_ACTIVE, PAYPAL_WHITE_BORDER)
     }
 
     data class ButtonColors(
@@ -69,5 +66,12 @@ sealed class PayPalButtonColor(val key: Int) {
         private val PAYPAL_FEATURED_HOVER = "#54B4E0".toColorInt()
         private val PAYPAL_FEATURED_MAIN_ACTIVE = "#3DB5FF".toColorInt()
         private val PAYPAL_FOCUS_INDICATOR = "#0066F5".toColorInt()
+        private val PAYPAL_TERTIARY_MAIN = "#000000".toColorInt()
+        private val PAYPAL_TERTIARY_HOVER = "#333333".toColorInt()
+        private val PAYPAL_TERTIARY_MAIN_ACTIVE = "#696969".toColorInt()
+        private val PAYPAL_WHITE_MAIN = "#FFFFFF".toColorInt()
+        private val PAYPAL_WHITE_HOVER = "#F2F2F2".toColorInt()
+        private val PAYPAL_WHITE_MAIN_ACTIVE = "#E9E9E9".toColorInt()
+        private val PAYPAL_WHITE_BORDER = "#555555".toColorInt()
     }
 }
