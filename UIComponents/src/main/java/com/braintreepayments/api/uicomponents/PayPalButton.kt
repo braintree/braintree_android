cc7e6c312bfafb1f9f6e13c77f050adcc5fe4ad5
Sai <saperi@paypal.com>
2025-12-22 17:06:22 -0800

    Use logoOffset to visually center PP logo. No offset is to be used when loading indicator is shown.

--- UIComponents/src/main/java/com/braintreepayments/api/uicomponents/PayPalButton.kt
+++ UIComponents/src/main/java/com/braintreepayments/api/uicomponents/PayPalButton.kt
@@ -42,7 +42,8 @@ class PayPalButton @JvmOverloads constructor(
     private var logo: Drawable? = null
     private var spinner: ProgressBar? = null
 
-    private val logoOffset = resources.getDimension(R.dimen.pp_logo_offset).toInt()
+    private val ppLogoOffset = resources.getDimension(R.dimen.pp_logo_offset).toInt()
+    private var logoOffset = ppLogoOffset
     private val desiredWidth = resources.getDimension(R.dimen.pay_button_width).toInt()
     private val desiredHeight = resources.getDimension(R.dimen.pay_button_height).toInt()
     private val minDesiredWidth = resources.getDimension(R.dimen.pay_button_min_width).toInt()
@@ -227,6 +228,7 @@ class PayPalButton @JvmOverloads constructor(
         this.isEnabled = false
         logo = ContextCompat.getDrawable(context, currentStyle.spinnerId)
         (logo as? android.graphics.drawable.Animatable)?.start()
+        logoOffset = 0
         invalidate()
     }
 
@@ -234,6 +236,7 @@ class PayPalButton @JvmOverloads constructor(
         this.isEnabled = true
         (logo as? android.graphics.drawable.Animatable)?.stop()
         logo = ContextCompat.getDrawable(context, currentStyle.logoId)
+        logoOffset = ppLogoOffset
         invalidate()
     }
 
@@ -305,13 +308,14 @@ class PayPalButton @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        val d = logo ?: return
-        val w = d.intrinsicWidth
-        val h = d.intrinsicHeight
-        val left = (width - w) / 2
-        val top = (height - h) / 2 + logoOffset
-        d.setBounds(left, top, left + w, top + h)
-        d.draw(canvas)
+        logo?.let { logoDrawable ->
+            val w = logoDrawable.intrinsicWidth
+            val h = logoDrawable.intrinsicHeight
+            val left = (width - w) / 2
+            val top = (height - h) / 2 + logoOffset
+            logoDrawable.setBounds(left, top, left + w, top + h)
+            logoDrawable.draw(canvas)
+        }
     }
 
     override fun setEnabled(enabled: Boolean) {
