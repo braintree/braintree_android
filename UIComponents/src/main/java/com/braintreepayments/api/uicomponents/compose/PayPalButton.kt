d3ae4b57713bc443c5f1004344d846bfbd31074f
Sai <saperi@paypal.com>
2026-02-04 12:47:45 -0800

    linter

--- UIComponents/src/main/java/com/braintreepayments/api/uicomponents/compose/PayPalButton.kt
+++ UIComponents/src/main/java/com/braintreepayments/api/uicomponents/compose/PayPalButton.kt
@@ -18,13 +18,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
