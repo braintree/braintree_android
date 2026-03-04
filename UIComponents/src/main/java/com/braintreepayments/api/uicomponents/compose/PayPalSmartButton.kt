dca8f06550d84840737cec56440caf0b0cfb7a0e
Sai <saperi@paypal.com>
2026-03-04 12:19:11 -0800

    make function private

--- UIComponents/src/main/java/com/braintreepayments/api/uicomponents/compose/PayPalSmartButton.kt
+++ UIComponents/src/main/java/com/braintreepayments/api/uicomponents/compose/PayPalSmartButton.kt
@@ -143,7 +143,7 @@ private fun handleReturnToApp(
     }
 }
 
-fun Context.findActivity(): Activity? = when (this) {
+private fun Context.findActivity(): Activity? = when (this) {
     is Activity -> this
     is ContextWrapper -> baseContext.findActivity()
     else -> null
