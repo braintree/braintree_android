e8cdb91bbadeff152cc539510790969debbfb0a2
Sai <saperi@paypal.com>
2026-03-09 17:09:33 -0700

    Venmo compose button

--- Demo/src/main/java/com/braintreepayments/demo/ComposeButtonsFragment.kt
+++ Demo/src/main/java/com/braintreepayments/demo/ComposeButtonsFragment.kt
@@ -1,5 +1,6 @@
 package com.braintreepayments.demo
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,7 +13,13 @@ import com.braintreepayments.api.core.PaymentMethodNonce
 import com.braintreepayments.api.paypal.PayPalResult
 import com.braintreepayments.api.paypal.PayPalTokenizeCallback
 import com.braintreepayments.api.uicomponents.PayPalButtonColor
+import com.braintreepayments.api.uicomponents.VenmoButtonColor
 import com.braintreepayments.api.uicomponents.compose.PayPalSmartButton
+import com.braintreepayments.api.uicomponents.compose.VenmoSmartButton
+import com.braintreepayments.api.venmo.VenmoPaymentMethodUsage
+import com.braintreepayments.api.venmo.VenmoRequest
+import com.braintreepayments.api.venmo.VenmoResult
+import com.braintreepayments.api.venmo.VenmoTokenizeCallback
 
 class ComposeButtonsFragment : BaseFragment() {
 
@@ -22,47 +29,28 @@ class ComposeButtonsFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
-
-        // PayPal flow setup
-        val payPalRequest = PayPalRequestFactory.createPayPalCheckoutRequest(
-            requireContext(),
-            "10.0",
-            null,
-            null,
-            null,
-            false,
-            null,
-            false,
-            false,
-            false
-        )
-
+        val paypalRequest = paypalRequest(requireContext())
         return ComposeView(requireContext()).apply {
-            val paypalTokenizeCallback = PayPalTokenizeCallback { payPalResult ->
-                when (payPalResult) {
-                    is PayPalResult.Success -> {
-                        handlePayPalResult(payPalResult.nonce)
-                    }
-
-                    is PayPalResult.Cancel -> {
-                        handleError(Exception("User did not complete payment flow"))
-                    }
-
-                    is PayPalResult.Failure -> {
-                        handleError(payPalResult.error)
-                    }
-                }
-            }
             setContent {
                 Column {
                     PayPalSmartButton(
                         style = PayPalButtonColor.Blue,
-                        payPalRequest = payPalRequest,
+                        payPalRequest = paypalRequest,
                         authorization = authStringArg,
                         appLinkReturnUrl =
                             "https://mobile-sdk-demo-site-838cead5d3ab.herokuapp.com/braintree-payments".toUri(),
                         deepLinkFallbackUrlScheme = "com.braintreepayments.demo.braintree",
                         paypalTokenizeCallback = paypalTokenizeCallback
+                    )
+
+                    VenmoSmartButton(
+                        style = VenmoButtonColor.Blue,
+                        venmoRequest = venmoRequest,
+                        authorization = authStringArg,
+                        appLinkReturnUrl =
+                            "https://mobile-sdk-demo-site-838cead5d3ab.herokuapp.com/braintree-payments".toUri(),
+                        deepLinkFallbackUrlScheme = "com.braintreepayments.demo.braintree",
+                        venmoTokenizeCallback = venmoTokenizeCallback
                     )
                 }
             }
@@ -78,6 +66,74 @@ class ComposeButtonsFragment : BaseFragment() {
                     paymentMethodNonce
                 )
             NavHostFragment.findNavController(this).navigate(action)
+        }
+    }
+
+    private fun handleVenmoResult(paymentMethodNonce: PaymentMethodNonce?) {
+        if (paymentMethodNonce != null) {
+            super.onPaymentMethodNonceCreated(paymentMethodNonce)
+
+            val action =
+                ComposeButtonsFragmentDirections.actionComposePaymentButtonsFragmentToDisplayNonceFragment(
+                    paymentMethodNonce
+                )
+            NavHostFragment.findNavController(this).navigate(action)
+        }
+    }
+
+    private fun paypalRequest(context: Context) = PayPalRequestFactory.createPayPalCheckoutRequest(
+        context,
+        "10.0",
+        null,
+        null,
+        null,
+        false,
+        null,
+        false,
+        false,
+        false
+    )
+
+    private val paypalTokenizeCallback = PayPalTokenizeCallback { payPalResult ->
+        when (payPalResult) {
+            is PayPalResult.Success -> {
+                handlePayPalResult(payPalResult.nonce)
+            }
+
+            is PayPalResult.Cancel -> {
+                handleError(Exception("User did not complete PayPal payment flow"))
+            }
+
+            is PayPalResult.Failure -> {
+                handleError(payPalResult.error)
+            }
+        }
+    }
+
+    private val venmoRequest = VenmoRequest(VenmoPaymentMethodUsage.SINGLE_USE).apply {
+        profileId = null
+        shouldVault = shouldVault
+        collectCustomerBillingAddress = true
+        collectCustomerShippingAddress = true
+        totalAmount = "20"
+        subTotalAmount = "18"
+        taxAmount = "1"
+        shippingAmount = "1"
+    }
+
+    private val venmoTokenizeCallback = VenmoTokenizeCallback { venmoResult ->
+        when (venmoResult) {
+            is VenmoResult.Success -> {
+                handleVenmoResult(venmoResult.nonce)
+            }
+
+            is VenmoResult.Cancel -> {
+                handleError(Exception("User did not complete Venmo payment flow"))
+            }
+
+            is VenmoResult.Failure -> {
+                handleError(venmoResult.error)
+            }
         }
     }
 }
---
+++ UIComponents/src/main/java/com/braintreepayments/api/uicomponents/compose/VenmoSmartButton.kt
@@ -0,0 +1,195 @@
+package com.braintreepayments.api.uicomponents.compose
+
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivityResultRegistryOwner
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.compose.LifecycleResumeEffect
+import androidx.lifecycle.coroutineScope
+import com.braintreepayments.api.core.AnalyticsClient
+import com.braintreepayments.api.core.AnalyticsEventParams
+import com.braintreepayments.api.uicomponents.UIComponentsAnalytics
+import com.braintreepayments.api.uicomponents.UIComponentsAnalytics.UI_TYPE_COMPOSE
+import com.braintreepayments.api.uicomponents.VenmoButtonColor
+import com.braintreepayments.api.venmo.VenmoClient
+import com.braintreepayments.api.venmo.VenmoLauncher
+import com.braintreepayments.api.venmo.VenmoPaymentAuthRequest
+import com.braintreepayments.api.venmo.VenmoPaymentAuthResult
+import com.braintreepayments.api.venmo.VenmoPendingRequest
+import com.braintreepayments.api.venmo.VenmoRequest
+import com.braintreepayments.api.venmo.VenmoResult
+import com.braintreepayments.api.venmo.VenmoTokenizeCallback
+import kotlinx.coroutines.launch
+
+@Composable
+fun VenmoSmartButton(
+    style: VenmoButtonColor,
+    venmoRequest: VenmoRequest,
+    authorization: String,
+    appLinkReturnUrl: Uri,
+    deepLinkFallbackUrlScheme: String? = null,
+    pendingRequestRepository: PayPalPendingRequestRepository = PayPalPendingRequestRepository(LocalContext.current),
+    venmoTokenizeCallback: VenmoTokenizeCallback
+) {
+    val context = LocalContext.current
+    val activity = context.findActivity()
+    val coroutineScope = rememberCoroutineScope()
+
+    var enabled by remember { mutableStateOf(true) }
+    var flowLaunched by remember { mutableStateOf(false) }
+    var shouldLogButtonPresentment by rememberSaveable { mutableStateOf(true) }
+
+    val registry = LocalActivityResultRegistryOwner.current?.activityResultRegistry
+
+    val venmoLauncher = remember { VenmoLauncher(registry) }
+    val venmoClient = remember {
+        VenmoClient(
+            context = context,
+            authorization = authorization,
+            appLinkReturnUrl = appLinkReturnUrl,
+            deepLinkFallbackUrlScheme = deepLinkFallbackUrlScheme
+        )
+    }
+    val analyticsClient = remember { AnalyticsClient.lazyInstance.value }
+
+    VenmoButton(style = style, enabled = enabled) {
+        enabled = false
+        logButtonSelected(analyticsClient)
+        venmoClient.createPaymentAuthRequest(
+            context = context,
+            request = venmoRequest
+        ) { paymentAuthRequest: VenmoPaymentAuthRequest ->
+            when (paymentAuthRequest) {
+                is VenmoPaymentAuthRequest.ReadyToLaunch -> {
+                    activity?.let {
+                        coroutineScope.launch {
+                            completeVenmoFlow(
+                                venmoLauncher,
+                                pendingRequestRepository,
+                                it,
+                                paymentAuthRequest,
+                                venmoTokenizeCallback
+                            )
+                            flowLaunched = true
+                        }
+                    }
+                }
+
+                is VenmoPaymentAuthRequest.Failure -> {
+                    venmoTokenizeCallback.onVenmoResult(VenmoResult.Failure(paymentAuthRequest.error))
+                }
+            }
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        if (shouldLogButtonPresentment) {
+            shouldLogButtonPresentment = false
+            logButtonPresented(analyticsClient)
+        }
+    }
+
+    LifecycleResumeEffect(Unit) {
+        if (flowLaunched) {
+            flowLaunched = false
+            lifecycle.coroutineScope.launch {
+                val pendingRequest = pendingRequestRepository.getPendingRequest()
+
+                activity?.intent?.let { intent ->
+                    handleReturnToApp(
+                        venmoLauncher,
+                        venmoClient,
+                        VenmoPendingRequest.Started(pendingRequest ?: ""),
+                        intent,
+                        venmoTokenizeCallback
+                    )
+                    enabled = true
+                    pendingRequestRepository.clearPendingRequest()
+                    activity.intent.data = null
+                }
+            }
+        }
+
+        onPauseOrDispose { lifecycle }
+    }
+}
+
+private suspend fun completeVenmoFlow(
+    venmoLauncher: VenmoLauncher,
+    pendingRequestRepository: PayPalPendingRequestRepository,
+    activity: Activity,
+    paymentAuthRequest: VenmoPaymentAuthRequest.ReadyToLaunch,
+    venmoTokenizeCallback: VenmoTokenizeCallback
+) {
+    val venmoPendingRequest = venmoLauncher.launch(
+        activity = activity as ComponentActivity,
+        paymentAuthRequest = paymentAuthRequest
+    )
+    when (venmoPendingRequest) {
+        is VenmoPendingRequest.Started -> {
+            pendingRequestRepository.storePendingRequest(venmoPendingRequest.pendingRequestString)
+        }
+
+        is VenmoPendingRequest.Failure -> {
+            venmoTokenizeCallback.onVenmoResult(VenmoResult.Failure(venmoPendingRequest.error))
+        }
+    }
+}
+
+private fun handleReturnToApp(
+    venmoLauncher: VenmoLauncher,
+    venmoClient: VenmoClient,
+    pendingRequest: VenmoPendingRequest.Started,
+    intent: Intent,
+    callback: VenmoTokenizeCallback
+) {
+    val paymentAuthResult = venmoLauncher.handleReturnToApp(
+        pendingRequest = pendingRequest,
+        intent = intent
+    )
+    when (paymentAuthResult) {
+        is VenmoPaymentAuthResult.Success -> {
+            venmoClient.tokenize(paymentAuthResult) { venmoResult ->
+                callback.onVenmoResult(venmoResult)
+            }
+        }
+        is VenmoPaymentAuthResult.Failure -> {
+            callback.onVenmoResult(VenmoResult.Failure(paymentAuthResult.error))
+        }
+        is VenmoPaymentAuthResult.NoResult -> {
+            callback.onVenmoResult(VenmoResult.Cancel)
+        }
+    }
+}
+
+private fun logButtonPresented(analyticsClient: AnalyticsClient) {
+    analyticsClient.sendEvent(
+        UIComponentsAnalytics.VENMO_BUTTON_PRESENTED,
+        AnalyticsEventParams(uiType = UI_TYPE_COMPOSE)
+    )
+}
+
+private fun logButtonSelected(analyticsClient: AnalyticsClient) {
+    analyticsClient.sendEvent(
+        UIComponentsAnalytics.VENMO_BUTTON_SELECTED,
+        AnalyticsEventParams(uiType = UI_TYPE_COMPOSE)
+    )
+}
+
+private fun Context.findActivity(): Activity? = when (this) {
+    is Activity -> this
+    is ContextWrapper -> baseContext.findActivity()
+    else -> null
+}
