1a5f21a416a00dfa974af791432f2045346f884f
anoguier <anoguier@paypal.com>
2026-03-17 10:50:49 -0500

    PR suggestions

--- LocalPayment/src/main/java/com/braintreepayments/api/localpayment/LocalPaymentApi.kt
+++ LocalPayment/src/main/java/com/braintreepayments/api/localpayment/LocalPaymentApi.kt
@@ -4,7 +4,6 @@ import com.braintreepayments.api.core.AnalyticsParamRepository
 import com.braintreepayments.api.core.BraintreeClient
 import com.braintreepayments.api.core.MerchantRepository
 import com.braintreepayments.api.localpayment.LocalPaymentNonce.Companion.fromJSON
-import kotlinx.coroutines.CancellationException
 import org.json.JSONObject
 
 internal class LocalPaymentApi(
@@ -13,68 +12,55 @@ internal class LocalPaymentApi(
     private val merchantRepository: MerchantRepository = MerchantRepository.instance
 ) {
 
-    @Suppress("TooGenericExceptionCaught")
     suspend fun createPaymentMethod(
         request: LocalPaymentRequest
     ): LocalPaymentAuthRequestParams {
-        try {
-            val returnUrlScheme = braintreeClient.getReturnUrlScheme()
-            val returnUrl = "$returnUrlScheme://${LocalPaymentClient.LOCAL_PAYMENT_SUCCESS}"
-            val cancel = "$returnUrlScheme://${LocalPaymentClient.LOCAL_PAYMENT_CANCEL}"
+        val returnUrlScheme = braintreeClient.getReturnUrlScheme()
+        val returnUrl = "$returnUrlScheme://${LocalPaymentClient.LOCAL_PAYMENT_SUCCESS}"
+        val cancel = "$returnUrlScheme://${LocalPaymentClient.LOCAL_PAYMENT_CANCEL}"
 
-            val url = "/v1/local_payments/create"
-            val responseBody = braintreeClient.sendPOST(
-                url,
-                request.build(returnUrl, cancel)
-            )
+        val url = "/v1/local_payments/create"
+        val responseBody = braintreeClient.sendPOST(
+            url,
+            request.build(returnUrl, cancel)
+        )
 
-            val responseJson = JSONObject(responseBody)
-            val redirectUrl = responseJson.getJSONObject("paymentResource")
-                .getString("redirectUrl")
-            val paymentToken = responseJson.getJSONObject("paymentResource")
-                .getString("paymentToken")
+        val responseJson = JSONObject(responseBody)
+        val redirectUrl = responseJson.getJSONObject("paymentResource")
+            .getString("redirectUrl")
+        val paymentToken = responseJson.getJSONObject("paymentResource")
+            .getString("paymentToken")
 
-            return LocalPaymentAuthRequestParams(request, redirectUrl, paymentToken)
-        } catch (error: Exception) {
-            if (error is CancellationException) throw error
-            throw error
-        }
+        return LocalPaymentAuthRequestParams(request, redirectUrl, paymentToken)
     }
 
-    @Suppress("TooGenericExceptionCaught")
     suspend fun tokenize(
         merchantAccountId: String?,
         responseString: String?,
         clientMetadataID: String?
     ): LocalPaymentNonce {
-        try {
-            val payload = JSONObject()
-            payload.put("merchant_account_id", merchantAccountId)
+        val payload = JSONObject()
+        payload.put("merchant_account_id", merchantAccountId)
 
-            val paypalAccount = JSONObject()
-                .put("intent", "sale")
-                .put("response", JSONObject().put("webURL", responseString))
-                .put("options", JSONObject().put("validate", false))
-                .put("response_type", "web")
-                .put("correlation_id", clientMetadataID)
-            payload.put("paypal_account", paypalAccount)
+        val paypalAccount = JSONObject()
+            .put("intent", "sale")
+            .put("response", JSONObject().put("webURL", responseString))
+            .put("options", JSONObject().put("validate", false))
+            .put("response_type", "web")
+            .put("correlation_id", clientMetadataID)
+        payload.put("paypal_account", paypalAccount)
 
-            val metaData = JSONObject()
-                .put("source", "client")
-                .put("integration", merchantRepository.integrationType.stringValue)
-                .put("sessionId", analyticsParamRepository.sessionId)
-            payload.put("_meta", metaData)
+        val metaData = JSONObject()
+            .put("source", "client")
+            .put("integration", merchantRepository.integrationType.stringValue)
+            .put("sessionId", analyticsParamRepository.sessionId)
+        payload.put("_meta", metaData)
 
-            val url = "/v1/payment_methods/paypal_accounts"
-            val responseBody = braintreeClient.sendPOST(
-                url = url,
-                data = payload.toString()
-            )
-
-            return fromJSON(JSONObject(responseBody))
-        } catch (error: Exception) {
-            if (error is CancellationException) throw error
-            throw error
-        }
+        val url = "/v1/payment_methods/paypal_accounts"
+        val responseBody = braintreeClient.sendPOST(
+            url = url,
+            data = payload.toString()
+        )
+        return fromJSON(JSONObject(responseBody))
     }
 }
