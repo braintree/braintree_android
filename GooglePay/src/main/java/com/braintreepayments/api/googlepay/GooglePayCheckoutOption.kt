8cf30de0e916ec83a20417c923078307f62020f9
Phil Andrews <phil@frontside.com.au>
2026-04-17 10:44:06 +1000

    lint fix

--- GooglePay/src/main/java/com/braintreepayments/api/googlepay/GooglePayCheckoutOption.kt
+++ GooglePay/src/main/java/com/braintreepayments/api/googlepay/GooglePayCheckoutOption.kt
@@ -13,11 +13,11 @@ enum class GooglePayCheckoutOption(val stringValue: String) {
 
     /**
      * The payment sheet button is labeled "Review Order".
-     */    
+     */
     CONTINUE_TO_REVIEW("CONTINUE_TO_REVIEW"),
 
     /**
-     * The payment sheet button is labeled "Pay now.
+     * The payment sheet button is labeled "Pay now".
      */
     COMPLETE_IMMEDIATE_PURCHASE("COMPLETE_IMMEDIATE_PURCHASE")
 }
