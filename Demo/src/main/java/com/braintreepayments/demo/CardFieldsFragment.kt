5c35d3ef5a5a082e204dd0d0e55266eaaa82b93e
anoguier <anoguier@paypal.com>
2026-06-05 16:43:18 -0500

    feat: create Card Fields class and use it in the demo

--- Demo/src/main/java/com/braintreepayments/demo/CardFieldsFragment.kt
+++ Demo/src/main/java/com/braintreepayments/demo/CardFieldsFragment.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.braintreepayments.api.uicomponents.cardfields.CardNumberTextInputView
-import com.braintreepayments.api.uicomponents.cardfields.ExpirationTextInputView
-import com.braintreepayments.api.uicomponents.cardfields.CvvTextInputView
+import com.braintreepayments.api.uicomponents.cardfields.CardFields
 
 class CardFieldsFragment : Fragment() {
 
@@ -18,9 +18,19 @@ class CardFieldsFragment : Fragment() {
     ): View {
         val view = inflater.inflate(R.layout.fragment_card_fields, container, false)
 
-        val cardNumberInput = view.findViewById<CardNumberTextInputView>(R.id.card_number_input)
-        view.findViewById<ExpirationTextInputView>(R.id.expiration_input)
-        view.findViewById<CvvTextInputView>(R.id.cvv_input).linkTo(cardNumberInput)
+        val cardFields = view.findViewById<CardFields>(R.id.card_fields)
+        val payButton = view.findViewById<Button>(R.id.pay_button)
+
+        //Card fields will tell the merchant if the card is valid, and they can enable the button
+        cardFields.setOnValidationChangedListener { isFormValid ->
+            payButton.isEnabled = isFormValid
+        }
+
+        // placeholder for tokenization
+        payButton.setOnClickListener {
+            Toast.makeText(requireContext(), R.string.card_fields_valid_toast, Toast.LENGTH_SHORT)
+                .show()
+        }
 
         return view
     }
