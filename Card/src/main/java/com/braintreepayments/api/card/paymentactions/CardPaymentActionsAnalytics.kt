6108fb4e8b886eeb51b0c73239551c230b24c4d4
mzlangreder <mlangreder@paypal.com>
2026-07-28 15:04:25 -0500

    fix: Align analytics event keys with iOS

--- Card/src/main/java/com/braintreepayments/api/card/paymentactions/CardPaymentActionsAnalytics.kt
+++ Card/src/main/java/com/braintreepayments/api/card/paymentactions/CardPaymentActionsAnalytics.kt
@@ -9,8 +9,7 @@ import androidx.annotation.RestrictTo
  */
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
 object CardPaymentActionsAnalytics {
-    const val SET_PAYMENT_METHOD_STARTED = "payment-actions:set-payment-method:started"
-    const val SET_PAYMENT_METHOD_SUCCEEDED = "payment-actions:set-payment-method:succeeded"
-    const val SET_PAYMENT_METHOD_FAILED = "payment-actions:set-payment-method:failed"
-    const val READY_FOR_CONFIRMATION = "payment-actions:ready-for-confirmation"
+    const val SET_PAYMENT_METHOD_STARTED = "card:payment-actions:set-payment-method:started"
+    const val SET_PAYMENT_METHOD_SUCCEEDED = "card:payment-actions:set-payment-method:succeeded"
+    const val SET_PAYMENT_METHOD_FAILED = "card:payment-actions:set-payment-method:failed"
 }
