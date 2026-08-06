677c8206062ff2c51a4ac09f66ab87e673f2af98
mzlangreder <mlangreder@paypal.com>
2026-08-06 10:23:56 -0500

    feat: create payment actions module and relocate clasess

--- PaymentActions/src/main/java/com/braintreepayments/api/paymentactions/PaymentActionsAnalytics.kt
+++ PaymentActions/src/main/java/com/braintreepayments/api/paymentactions/PaymentActionsAnalytics.kt
@@ -1,4 +1,4 @@
-package com.braintreepayments.api.card.paymentactions
+package com.braintreepayments.api.paymentactions
 
 import androidx.annotation.RestrictTo
 
@@ -8,7 +8,7 @@ import androidx.annotation.RestrictTo
  * TODO: These keys are currently placeholder values that must be changed when we get the official values.
  */
 @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
-object CardPaymentActionsAnalytics {
+object PaymentActionsAnalytics {
     const val SET_PAYMENT_METHOD_STARTED = "card:payment-actions:set-payment-method:started"
     const val SET_PAYMENT_METHOD_SUCCEEDED = "card:payment-actions:set-payment-method:succeeded"
     const val SET_PAYMENT_METHOD_FAILED = "card:payment-actions:set-payment-method:failed"
