e001a0f1a76719bc0283670b4c8e2222686b4b18
Sara Vasquez <98496950+saralvasquez@users.noreply.github.com>
2026-08-25 15:27:46 -0700

    feat: add card fields compose expiration field (#1678)
    * feat: add compose card fields expiration date field
    
    * fix: lint issues
    
    * pr-feedback: consolidate test classes
    
    * fix: add contentDescription
    
    * fix: lint issue
    
    * fix: rename controller
    
    * fix: rename state in unit tests

--- UIComponents/src/main/java/com/braintreepayments/api/uicomponents/compose/CardFieldsVisualTransformations.kt
+++ UIComponents/src/main/java/com/braintreepayments/api/uicomponents/compose/CardFieldsVisualTransformations.kt
@@ -5,11 +5,19 @@ import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.input.VisualTransformation
 import com.braintreepayments.api.uicomponents.cardfields.CardNumberFormatter
+import com.braintreepayments.api.uicomponents.cardfields.ExpirationDateFormatter
 
 internal class CardNumberVisualTransformation(private val formatGaps: IntArray) :
     VisualTransformation {
     override fun filter(text: AnnotatedString): TransformedText {
         val formatted = CardNumberFormatter.formatCardNumber(text.text, formatGaps)
+        return TransformedText(AnnotatedString(formatted), digitOffsetMapping(formatted))
+    }
+}
+
+internal class ExpirationDateVisualTransformation : VisualTransformation {
+    override fun filter(text: AnnotatedString): TransformedText {
+        val formatted = ExpirationDateFormatter.formatExpiration(text.text)
         return TransformedText(AnnotatedString(formatted), digitOffsetMapping(formatted))
     }
 }
