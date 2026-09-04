02647fccfdc6f6d9a84d5383eaddd669d43b9392
Sara Vasquez <98496950+saralvasquez@users.noreply.github.com>
2026-09-04 15:56:08 -0700

    feat: add compose pay button and processing (#1683)
    * feat: add compose pay button and processing
    
    * pr-feedback: update unit tests, add demo comment, fix process death bug, and remove initialize

--- Demo/src/main/java/com/braintreepayments/demo/ComposeCardFieldsFragment.kt
+++ Demo/src/main/java/com/braintreepayments/demo/ComposeCardFieldsFragment.kt
@@ -5,11 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.unit.dp
+import androidx.navigation.fragment.NavHostFragment
+import com.braintreepayments.api.card.Card
+import com.braintreepayments.api.uicomponents.cardfields.CardFieldsResult
 import com.braintreepayments.api.uicomponents.compose.CardFields
 import com.braintreepayments.api.uicomponents.compose.rememberCardFieldsController
 
@@ -23,10 +35,48 @@ class ComposeCardFieldsFragment : BaseFragment() {
         super.onCreateView(inflater, container, savedInstanceState)
         return ComposeView(requireContext()).apply {
             setContent {
-                val cardFieldsController = rememberCardFieldsController()
+                val cardFieldsController = rememberCardFieldsController(
+                    authorization = authStringArg,
+                    // optional customer data
+                    request = Card(
+                        cardholderName = "John Doe",
+                        postalCode = "12345"
+                    )
+                )
+                val isFormValid by cardFieldsController.isFormValid.collectAsState()
 
                 Column(modifier = Modifier.padding(16.dp)) {
                     CardFields(controller = cardFieldsController)
+                    Button(
+                        modifier = Modifier
+                            .height(68.dp)
+                            .fillMaxWidth()
+                            .padding(top = 16.dp),
+                        shape = RoundedCornerShape(12.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Black,
+                            contentColor = Color.White,
+                            disabledContainerColor = Color.Gray,
+                            disabledContentColor = Color.White
+                        ),
+                        enabled = isFormValid,
+                        onClick = {
+                            cardFieldsController.submit { result ->
+                                when (result) {
+                                    is CardFieldsResult.Success -> {
+                                        onPaymentMethodNonceCreated(result.nonce)
+                                        val action = ComposeCardFieldsFragmentDirections
+                                            .actionComposeCardFieldsFragmentToDisplayNonceFragment(result.nonce)
+                                        NavHostFragment.findNavController(this@ComposeCardFieldsFragment)
+                                            .navigate(action)
+                                    }
+                                    is CardFieldsResult.Failure -> handleError(result.error)
+                                }
+                            }
+                        }
+                    ) {
+                        Text(getString(R.string.card_fields_pay))
+                    }
                 }
             }
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
