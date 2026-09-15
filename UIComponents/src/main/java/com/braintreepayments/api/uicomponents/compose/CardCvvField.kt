4b33820a9fda49110fa54efc1c343ad083b198ee
anoguier <anoguier@paypal.com>
2026-09-15 13:44:12 -0500

    fix: revert back to using KeyboardType.Number to allow pasting an input

--- UIComponents/src/main/java/com/braintreepayments/api/uicomponents/compose/CardCvvField.kt
+++ UIComponents/src/main/java/com/braintreepayments/api/uicomponents/compose/CardCvvField.kt
@@ -59,7 +59,7 @@ internal fun CardCvvField(
         modifier = modifier,
         errorText = errorText,
         visualTransformation = remember(revealedIndex) { CvvVisualTransformation(revealedIndex) },
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         focusRequester = focusRequester,
         onFocusChanged = onFocusChanged,
         contentDescription = stringResource(R.string.cvv_accessibility),
