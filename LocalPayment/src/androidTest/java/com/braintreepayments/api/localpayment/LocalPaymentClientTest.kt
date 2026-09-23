cfd4ab8cda24faba3a519dc61561afdbfa8b1f6b
Sara Vasquez <saravasquez@paypal.com>
2026-09-23 15:46:52 -0700

    fix: resolve lint issue

--- LocalPayment/src/androidTest/java/com/braintreepayments/api/localpayment/LocalPaymentClientTest.kt
+++ LocalPayment/src/androidTest/java/com/braintreepayments/api/localpayment/LocalPaymentClientTest.kt
@@ -17,7 +17,6 @@ import org.json.JSONObject
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.util.concurrent.CountDownLatch
